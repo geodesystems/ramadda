d2461bb07a791f050c5e1bd61e0d8111a3defdf4
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2018-09-11 10:46:01 -0600

    Start of text to the future entry type

---
+++ src/org/ramadda/plugins/phone/TTTFTypeHandler.java
@@ -0,0 +1,59 @@
+/*
+* Copyright (c) 2008-2018 Geode Systems LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.ramadda.plugins.phone;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.type.*;
+
+import org.ramadda.util.HtmlUtils;
+
+import org.w3c.dom.*;
+
+import ucar.unidata.util.StringUtil;
+
+import java.util.List;
+
+
+/**
+ *
+ *
+ */
+public class TTTFTypeHandler extends GenericTypeHandler {
+
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public TTTFTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+@Override
+    public boolean getForUser() {
+    return true;
+    }
+
+}
