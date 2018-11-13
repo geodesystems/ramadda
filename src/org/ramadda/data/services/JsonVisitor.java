70c3e15f5ef573efa476d28ea94fb8f1006d06d6
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2018-11-13 08:53:52 -0700

    handle no data

--- src/org/ramadda/data/services/JsonVisitor.java
+++ src/org/ramadda/data/services/JsonVisitor.java
@@ -199,6 +199,13 @@ public class JsonVisitor extends BridgeRecordVisitor {
         super.finished(file, visitInfo);
         if (pw != null) {
             RecordField.addJsonFooter(pw);
+        } else if (fields == null) {
+            pw     = getThePrintWriter();
+            String       code = "warning";
+            StringBuffer json = new StringBuffer();
+            pw.append(Json.map("error", Json.quote("No data available"),
+                               "errorcode", Json.quote(code)));
+
         }
     }
 
