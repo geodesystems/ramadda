c81052ef353a13bed8b19b40c3170cb1cd4fd7e1
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2018-12-05 18:49:39 -0700

    new minified

--- src/org/ramadda/repository/htdocs/build.sh
+++ src/org/ramadda/repository/htdocs/build.sh
@@ -1,8 +1,8 @@
-yui="java -jar /Users/jeffmc/bin/yuicompressor-2.4.8.jar"
-cd display
-cat utils.js display.js pointdata.js control.js displaychart.js displayd3.js displayentry.js displayext.js displaymanager.js displaymap.js displaytable.js > display_all.js
-${yui} display_all.js > display_all_mini.js
-cd ../
+dir=`dirname $0`
+yui="java -jar ${dir}/../../../../../lib/yuicompressor-2.4.8.jar"
+
+cat display/utils.js display/display.js display/pointdata.js display/control.js display/displaychart.js display/displayd3.js display/displayentry.js display/displayext.js display/displaymanager.js display/displaymap.js display/displaytable.js > display/display_all.js
+${yui} display/display_all.js > display/display_all_mini.js
 ${yui} ramadda.js > ramadda.mini.js
 ${yui} ramaddamap.js > ramaddamap.mini.js
 ${yui} utils.js > utils.mini.js
@@ -10,4 +10,6 @@ ${yui} wiki.js > wiki.mini.js
 ${yui} repositories.js > repositories.mini.js
 ${yui} selectform.js > selectform.mini.js
 ${yui} entry.js > entry.mini.js
+${yui} style.css > style.mini.css
+${yui} display.css > display.mini.css
 
