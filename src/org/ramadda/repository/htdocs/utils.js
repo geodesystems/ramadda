009c10138d39cefa8510369764e10eed3932db90
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2018-12-12 11:54:14 -0700

    Add a makeDownloadFile func

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -62,6 +62,15 @@ var Utils = {
         }
         return date;
     },
+    makeDownloadFile:    function(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    },
     isDefined: function(v) {
         return   !(typeof v=== 'undefined');
     },
