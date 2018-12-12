566ce48fb9ac8791b2037aa405c1f36beb4b824f
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2018-12-12 07:30:57 -0700

    add pretty print functionality to main

--- src/org/ramadda/util/Json.java
+++ src/org/ramadda/util/Json.java
@@ -591,6 +591,11 @@ public class Json {
      * @throws Exception _more_
      */
     public static void main(String[] args) throws Exception {
+        String json = IOUtil.readContents(args[0],Json.class);
+        JSONObject   obj      = new JSONObject(json.toString());
+        System.out.println(obj.toString(3));
+        if(true) return;
+
         toCsv(args[0], System.out, (args.length > 1)
                                    ? args[1]
                                    : null);
