08b17d0daf8545a51671f50781b76f614e22f2ba
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-01-05 08:54:24 -0700

    update

--- src/org/ramadda/plugins/community/10000cities/processMembers.tcl
+++ src/org/ramadda/plugins/community/10000cities/processMembers.tcl
@@ -12,13 +12,28 @@ proc getText {textNode} {
     return [$textNode nodeValue]
 }
 
-puts "map.fields=party,member,url,memberimage"
+puts "map.fields=party,state,member,url,memberimage,town,phone,address"
 puts "map.key=geoid"
 
 foreach member [$root selectNodes //member] {
     set district [getText [$member selectNodes {statedistrict/text()}]]
     set infoNode [$member selectNodes {member-info}]
+    set state [getText [$infoNode selectNodes {state/state-fullname/text()}]]
     set name [getText [$infoNode selectNodes official-name/text()]]
+    if {$name == ""} {
+        set name "[getText [$infoNode selectNodes firstname/text()]] [getText [$infoNode selectNodes lastname/text()]]"
+    }
+    set town [getText [$infoNode selectNodes townname/text()]]
+    set office [getText [$infoNode selectNodes office-building/text()]]
+    set room [getText [$infoNode selectNodes office-room/text()]]
+    set zip "[getText [$infoNode selectNodes office-zip/text()]]-[getText [$infoNode selectNodes office-zip-suffix/text()]]"
+    set phone [getText [$infoNode selectNodes phone/text()]]
+
+    switch ${office}  {
+        LHOB {set office  "Longworth House Office Building" }
+        CHOB {set office "Cannon House Office Building "}
+        RHOB {set office "Rayburn House Office Building"}
+    }
     set id [getText [$infoNode selectNodes bioguideID/text()]]
     set party [getText [$infoNode selectNodes party/text()]]
     regexp {(..)(..)} $district match abbr num
@@ -29,11 +44,16 @@ foreach member [$root selectNodes //member] {
     set fips $::abbrToFips($abbr)
     puts "map.${fips}$num.party=$party"
     puts "map.${fips}$num.member=$name"
+    puts "map.${fips}$num.state=$state"
     set img "https://clerkpreview.house.gov/content/assets/img/members/$id.jpg"
     set url "https://clerkpreview.house.gov/members/$id"
     puts "map.${fips}$num.memberimage=$img"
     puts "map.${fips}$num.url=$url"
+    puts "map.${fips}$num.town=$town"
+    puts "map.${fips}$num.phone=$phone"
+    puts "map.${fips}$num.address=$room $office<br>Washington, DC, $zip"
 }
+
 
 
 
