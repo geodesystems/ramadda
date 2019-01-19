752efd9662bd8ea819ae97edb7dc98e09f2cc4ec
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-01-19 00:32:13 -0700

    update

--- src/org/ramadda/repository/htdocs/display/displayentry.js
+++ src/org/ramadda/repository/htdocs/display/displayentry.js
@@ -1257,16 +1257,15 @@ function RamaddaEntrydategridDisplay(displayManager, id, properties) {
                 var hlines = "";
                 var vlines = "";
                 var dayOffset = 15;
-                var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31,31];
-                var monthNums = [0,1,2,3,4,5,6,7,8,9,10,11,12];
-                var months = ["Jan","Feb","Mar","Apr","May","Jun", "Jul","Aug", "Sep","Oct", "Nov","Dec",null];
+                var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+                var monthNums = [0,1,2,3,4,5,6,7,8,9,10,11];
+                var months = ["Jan","Feb","Mar","Apr","May","Jun", "Jul","Aug", "Sep","Oct", "Nov","Dec"];
                                            var days = dayOffset;
                 var daysInYAxis = 365+dayOffset;
                 var leftAxis = "";
                 var bottomAxis = "";
                 for(var month=0;month<months.length;month++) {
                     var yPercent  =Math.round(100*(days/daysInYAxis));
-                    //                    console.log(months[month] +" " + yPercent +" days:" + days);
                     days+=monthDays[month];
                     var bottom = yPercent +"%";
                     var style = "bottom:" + bottom+";";
