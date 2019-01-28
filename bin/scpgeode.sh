0fc6851464fd5a8b5299b0aba0681729c4780b93
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-01-28 16:02:57 -0700

    snapshot

--- bin/scpgeode.sh
+++ bin/scpgeode.sh
@@ -1,8 +1,6 @@
+#!/bin/sh
 export dest_ip=$1
-
-#52.88.245.74
-
 echo "scping $2 to $dest_ip"
-scp -i /Users/jeffmc/work/amazon/geodesystems.pem $2 ec2-user@${dest_ip}:
+scp -i ${GEODESYSTEMS_PEM} $2 ec2-user@${dest_ip}:
 
 
