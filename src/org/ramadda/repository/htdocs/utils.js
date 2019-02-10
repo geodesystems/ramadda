c5012b2a1b314ee5ef117184ca72a1cfb89060ae
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-02-10 12:58:10 -0700

    update

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -164,7 +164,7 @@ var Utils = {
     articles: "that,this,they,those,with,them,their,have,than,there,when,more,much,many,will,were".split(","),
     tokenizeWords:function(s,stopWords, extraStopWords, removeArticles) {
         if(!stopWords) {
-            stopWords = ['a','i','the','if','1','2','3','4','5','6','7','8','9','0','to','of','or','and','but','it','by','as','in','for'];
+            stopWords =  [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ];
         }
         var words = [];
         var toks = s.split(/[ ,\(\)\.:\;\n\?\-!\*]/);
