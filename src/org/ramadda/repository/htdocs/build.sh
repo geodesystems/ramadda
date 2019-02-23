e86fc3c8b93cbde29bb98f040533a017a0ab938a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-02-22 18:54:54 -0700

    use new minifier since yui fails with newer js

--- src/org/ramadda/repository/htdocs/build.sh
+++ src/org/ramadda/repository/htdocs/build.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
+#
+#This uses jsmin for minification
+#to install do:
+#pip install jsmin
+#
+
 dir=`dirname $0`
-yui="java -jar ${dir}/../../../../../lib/yuicompressor-2.4.8.jar"
+minify="python -m jsmin "
+#yui="java -jar ${dir}/../../../../../lib/yuicompressor-2.4.8.jar"
 dest="${dir}/min"
 
 cat ${dir}/display/widgets.js ${dir}/display/display.js ${dir}/display/pointdata.js ${dir}/display/control.js ${dir}/display/notebook.js ${dir}/display/displaychart.js ${dir}/display/displayd3.js ${dir}/display/displaytext.js ${dir}/display/displayentry.js ${dir}/display/displayext.js ${dir}/display/displaymanager.js ${dir}/display/displaymap.js ${dir}/display/displaytable.js ${dir}/display/displayplotly.js > ${dest}/display_all.js
-${yui} ${dest}/display_all.js > ${dest}/display_all.min.js
-${yui} ${dest}/display_all.js > ${dest}/display_all.min.js
-#${yui} ${dir}/display/displayplotly.js > ${dest}/displayplotly.min.js
-${yui} ${dir}/ramadda.js > ${dest}/ramadda.min.js
-${yui} ${dir}/ramaddamap.js > ${dest}/ramaddamap.min.js
-${yui} ${dir}/utils.js > ${dest}/utils.min.js
-${yui} ${dir}/wiki.js > ${dest}/wiki.min.js
-${yui} ${dir}/repositories.js > ${dest}/repositories.min.js
-${yui} ${dir}/selectform.js > ${dest}/selectform.min.js
-${yui} ${dir}/entry.js > ${dest}/entry.min.js
-${yui} ${dir}/style.css > ${dest}/style.min.css
-${yui} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
-${yui} ${dir}/display/display.css > ${dest}/display.min.css
+${minify} ${dest}/display_all.js > ${dest}/display_all.min.js
+${minify} ${dest}/display_all.js > ${dest}/display_all.min.js
+#${minify} ${dir}/display/displayplotly.js > ${dest}/displayplotly.min.js
+${minify} ${dir}/ramadda.js > ${dest}/ramadda.min.js
+${minify} ${dir}/ramaddamap.js > ${dest}/ramaddamap.min.js
+${minify} ${dir}/utils.js > ${dest}/utils.min.js
+${minify} ${dir}/wiki.js > ${dest}/wiki.min.js
+${minify} ${dir}/repositories.js > ${dest}/repositories.min.js
+${minify} ${dir}/selectform.js > ${dest}/selectform.min.js
+${minify} ${dir}/entry.js > ${dest}/entry.min.js
+${minify} ${dir}/style.css > ${dest}/style.min.css
+${minify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
+${minify} ${dir}/display/display.css > ${dest}/display.min.css
 
