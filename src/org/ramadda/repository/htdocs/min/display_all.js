d72bc40c24a92b9e820d46ad2e3ac42131943619
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-04-08 19:14:40 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -5315,8 +5315,6 @@ function RamaddaLabelDisplay(displayManager, id, properties) {
 Copyright 2008-2019 Geode Systems LLC
 */
 
-
-
 var DISPLAY_NOTEBOOK = "notebook";
 addGlobalDisplayType({
     type: DISPLAY_NOTEBOOK,
