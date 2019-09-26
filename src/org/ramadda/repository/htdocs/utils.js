271be445fe0aaf82fae49aec7addb6bca429a72f
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-09-26 04:26:59 -0600

    add a new color table

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -855,7 +855,10 @@ var Utils = {
         rainbow: {
             colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
         },
-        nice: {
+        cats: {
+            colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf","#12649f","#ffd800","#DFA25A"]
+        },
+	nice: {
             colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
         },
         nice2: {
