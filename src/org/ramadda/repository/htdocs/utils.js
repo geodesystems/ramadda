f4898c61fd1fd58e3854b9c6525ae534242d2186
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2019-09-26 04:34:24 -0600

    more colors

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -856,7 +856,7 @@ var Utils = {
             colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
         },
         cats: {
-            colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf","#12649f","#ffd800","#DFA25A"]
+            colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf","#12649f","#ffd800","#DFA25A","#4B0082","#7BCCC4","#ADDD8E"]
         },
 	nice: {
             colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
