a832478dce67915d5b7fec14b5baa4b9221bd42a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2020-03-12 13:00:11 -0600

    fix display container width

--- test/runuitest.tcl
+++ test/runuitest.tcl
@@ -15,7 +15,7 @@ set ::tcnt 0
 
 
 
-set ::limit 10000
+set ::limit 3
 set csv [getUrl https://geodesystems.com/repository/entry/show?ascending=true&orderby=name&entryid=11ff9695-7b5e-4b5c-b6df-3f058bbea5dc&output=default.csv&fields=name,id&showheader=false]
 foreach line [split $csv "\n"] {
     set line [string trim $line]
