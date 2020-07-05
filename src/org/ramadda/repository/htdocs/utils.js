1b6fa7e8996324a78dca4aaaa4230a413588a580
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2020-07-04 20:23:21 -0600

    new split method

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -78,12 +78,15 @@ var Utils =  {
 	var ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
 	return {x:nx, y:ny};
     },
-    split: function(s) {
+    split: function(s,trim,excludeEmpty) {
 	if(!s) return null;
 	s = String(s).replace(/\\,/g,"_comma_");
 	let l = [];
 	s.split(",").forEach((tok)=>{
-	    l.push(tok.replace(/_comma_/g,","));
+	    tok = tok.replace(/_comma_/g,",");
+	    if(trim) tok = tok.trim();
+	    if(excludeEmpty && tok == "") return;
+	    l.push(tok);
 	});
 	return l;
     },
@@ -464,6 +467,12 @@ var Utils =  {
             b.push(a[i]);
         return b;
     },
+    //If not an array then make it one
+    makeArray: function(v) {
+	if(!Array.isArray(v)) return [v];
+	return v;
+    },
+
     join: function(l, delimiter, offset) {
         if ((typeof offset) == "undefined") offset = 0;
         var s = "";
@@ -796,6 +805,11 @@ var Utils =  {
         s = s.trim();
         return this.camelCase(s.replace(/_/g," "));
     },
+    makeId: function(s) {
+        s  = String(s);
+        s = s.trim().toLowerCase().replace(/ /g,"_");
+        return s;
+    },    
     camelCase: function(s,firstLower) {
         if (!s) return s;
         var r = "";
@@ -1496,16 +1510,6 @@ var Utils =  {
 	if(!this.foregroundColors[c] && c.startsWith("#")) return this.getContrastYIQ(c);
 	return this.foregroundColors[c] ||"#000";
     },
-    colorToRgb: {
-	"red":"rgb(255,0,0)",
-	"green":"rgb(0,255,0)",
-	"blue":"rgb(0,0,255)",
-	"yellow":"rgb(255,255,0)",
-	"aqua":"#00FFFF",
-	"black":"#000000",
-	"brown":"A52A2A",
-	"":"#00FFFF",
-    },
     //From  https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
     pSBC: function(p,c0,c1,l) {
 	c0 = this.colorToRgb[c0] || c0;
@@ -1536,6 +1540,31 @@ var Utils =  {
 	if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
 	else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
     },
+    colorToRgb:   {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+     "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+				   "yellow":"#ffff00","yellowgreen":"#9acd32"},
+
     addAlphaToColor: function(c, alpha) {
 	c = this.colorToRgb[c] || c;
 	if(!alpha) alpha = "0.5";
@@ -2574,6 +2603,9 @@ var HU = HtmlUtils = window.HtmlUtils  = window.HtmlUtil = {
         });
     },
     join: function(items, separator) {
+        if(!Array.isArray(items)) {
+	    items=[items];
+	}
         var html = "";
         for (var i = 0; i < items.length; i++) {
             if (i > 0 & separator != null) html += separator;
@@ -2655,9 +2687,9 @@ var HU = HtmlUtils = window.HtmlUtils  = window.HtmlUtil = {
     leftCenterRight: function(left, center, right, leftWidth, centerWidth, rightWidth, attrs) {
         if (!attrs) attrs = {};
         if (!attrs.valign) attrs.valign = "top";
-        if (leftWidth == null) leftWidth = "33%";
-        if (centerWidth == null) centerWidth = "33%";
-        if (rightWidth == null) rightWidth = "33%";
+//        if (leftWidth == null) leftWidth = "33%";
+//        if (centerWidth == null) centerWidth = "33%";
+//        if (rightWidth == null) rightWidth = "33%";
 
         return this.tag("table", ["border", 0, "width", "100%", "cellspacing", "0", "cellpadding", "0"],
 			this.tr(["valign", attrs.valign],
