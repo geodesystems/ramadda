28208512a43a10c33b08e50193ce2084bd23059a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-03-29 00:28:49 -0600

    better emdedd display of html

--- src/org/ramadda/plugins/media/HtmlDocTypeHandler.java
+++ src/org/ramadda/plugins/media/HtmlDocTypeHandler.java
@@ -28,12 +28,12 @@ import org.w3c.dom.*;
 
 import ucar.unidata.util.IOUtil;
 
-import ucar.unidata.util.Misc;
 import ucar.unidata.util.StringUtil;
 
 
 import ucar.unidata.xml.XmlUtil;
 
+import java.util.List;
 import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -44,8 +44,12 @@ import java.io.OutputStream;
  *
  *
  */
-public class HtmlDocTypeHandler extends GenericTypeHandler {
+public class HtmlDocTypeHandler extends ExtensibleGroupTypeHandler {
 
+
+    private static int IDX=0;
+
+    public static final int IDX_STYLE=IDX++;
 
     /**
      * _more_
@@ -103,9 +107,10 @@ public class HtmlDocTypeHandler extends GenericTypeHandler {
      * @throws Exception _more_
      */
     @Override
-    public Result getHtmlDisplay(Request request, Entry entry)
+    public Result getHtmlDisplay(Request request, Entry entry,
+                                 List<Entry> subGroups, List<Entry> entries)
             throws Exception {
-        String style = entry.getValue(0, "none");
+        String style = entry.getValue(IDX_STYLE, "none");
         if (style.equals("none")) {
             return null;
         }
@@ -126,7 +131,7 @@ public class HtmlDocTypeHandler extends GenericTypeHandler {
                     HtmlUtils.attr(HtmlUtils.ATTR_SRC, url)
                     + HtmlUtils.attr(HtmlUtils.ATTR_WIDTH, "100%")
                     + HtmlUtils.attr(
-                        HtmlUtils.ATTR_HEIGHT, "300"), "Need frames"));
+                        HtmlUtils.ATTR_HEIGHT, "800px"), "Need frames"));
 
             return new Result("", sb);
         }
@@ -135,11 +140,24 @@ public class HtmlDocTypeHandler extends GenericTypeHandler {
             return null;
         }
 
-        if (style.equals("full")) {
+        if (style.equals("embed") || style.equals("full")) {
+	    String content = getContent(request, entry);
+	    String head = StringUtil.findPattern(content,"(?s)<head>(.*?)</head>");
+	    //	    System.err.println("head:" + head);
+	    if(head!=null) {
+		content = content.replaceAll("(?s)<head>(.*?)</head>","");
+		head = head.replaceAll("(?s)<title>(.*?)</title>","");
+		request.appendHead(head);
+	    }
+	    String body = StringUtil.findPattern(content,"(?s)<body>(.*?)</body>");
+	    if(body!=null) content = body;
+            String title = HU.href(getEntryManager().getEntryUrl(request,
+								 entry), entry.getName());
+	    content  =content.replaceAll("(?s)<div *class *= *\"ramadda-page-title\"[^>]*>(.*?)</div>","<div class=\"ramadda-page-title\">" + title +"</div>");
+	    if(true) return new Result("",new StringBuilder(content));
             return getEntryManager().addHeaderToAncillaryPage(request,
                     new Result(BLANK,
-                               new StringBuilder(getContent(request,
-                                   entry))));
+                               new StringBuilder(content)));
         }
 
         return null;
@@ -162,7 +180,7 @@ public class HtmlDocTypeHandler extends GenericTypeHandler {
     public String getInnerWikiContent(Request request, Entry entry,
                                       String wikiTemplate)
             throws Exception {
-        if ( !((entry.getValue(0) + "").equals("partial"))) {
+        if ( !((entry.getValue(IDX_STYLE) + "").equals("partial"))) {
             return null;
         }
 
@@ -191,7 +209,6 @@ public class HtmlDocTypeHandler extends GenericTypeHandler {
         html = html.replace("${urlroot}",
                             getRepository().getUrlBase()).replace("${root}",
                                 getRepository().getUrlBase());
-
         return html;
     }
 
