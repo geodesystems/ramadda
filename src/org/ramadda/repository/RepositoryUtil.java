f89d0ca677f0a2f8d0f4fbc1eb496395eeefb877
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-04-01 22:44:39 -0600

    bump minor

--- src/org/ramadda/repository/RepositoryUtil.java
+++ src/org/ramadda/repository/RepositoryUtil.java
@@ -50,7 +50,7 @@ public class RepositoryUtil {
     private static final double MAJOR_VERSION = 5.0;
 
     /** _more_ */
-    private static final int MINOR_VERSION = 5;
+    private static final int MINOR_VERSION = 6;
 
 
     //When we make any real change to the css or javascript change this version
