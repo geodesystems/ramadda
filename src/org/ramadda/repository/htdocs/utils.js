ac5722b711d86f1646b9934378f69f75fadf39f9
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-04-05 00:09:00 -0600

    add a copyToClipboard method

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -127,6 +127,13 @@ var Utils =  {
 	});
 	return b3;
     },
+    copyToClipboard:function(text) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val(text).select();
+	document.execCommand("copy");
+	$temp.remove();
+    },
     isAnonymous: function() {
 	return ramaddaUser =="anonymous";
     },
