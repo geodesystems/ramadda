17cf47bd306876c3e9f287b3ee757b522f67a3b0
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-05-01 12:09:36 -0600

    add ramaddaThisEntry

--- src/org/ramadda/repository/htdocs/base.js
+++ src/org/ramadda/repository/htdocs/base.js
@@ -7,3 +7,5 @@ var ramaddaBaseUrl = "${ramadda.root}";
 var ramaddaBaseEntry = "${ramadda.baseentry}";
 var ramaddaBaseHtdocs = "${ramadda.htdocs}";
 var ramaddaUser = "${ramadda.user}";
+var ramaddaTreeSearchEnabled = ${ramadda.search.tree};
+var ramaddaThisEntry = null;
