7d7f0b374ab058569a25429fee2b41a5628268ca
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-08-07 14:41:50 -0600

    handle directory argument

--- bin/scpgeode.sh
+++ bin/scpgeode.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 export dest_ip=$1
 echo "scping $2 to $dest_ip"
-scp -r -i ${GEODESYSTEMS_PEM} $2 ec2-user@${dest_ip}:
+if [ -z "$3" ]; then
+    scp -r -i ${GEODESYSTEMS_PEM} $2 ec2-user@${dest_ip}:$3/
+else
+    scp -r -i ${GEODESYSTEMS_PEM} $2 ec2-user@${dest_ip}:$3
+fi
 
 
