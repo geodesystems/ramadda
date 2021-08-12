4113fcdccd232f4edc84dea6d17e38c2d6866734
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-08-11 21:27:42 -0600

    update

--- projects/bocovoters/contributions.sh
+++ projects/bocovoters/contributions.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 export csv=~/bin/csv.sh 
 
+
+#wget -O source/new.csv --post-data="exportType=Contribution&electionID=20&committeeID=-1&filingDateStart=&filingDateStop=&transactionDateStart=&transactionDateStop=" https://election.bouldercolorado.gov/electionContributions.php
+
+
 dots=2000
 do_convert() {
     ${csv} -set Match 0 MatchAmount -set CommitteeNum 0 CommitteeNumber  \
 	   -change filingdate,amendeddate,transactiondate "(....)/(..)/(..).*" "\$1/\$2/\$3" \
+	   -case  FromCandidate lower \
+	   -case  anonymous lower \
 	   -p source/old.csv > oldtmp.csv
     ${csv} -notcolumns "YTDAmount,AmendsContributionID,ContributionID" \
 	   -change filingdate,amendeddate,transactiondate "(..)/(..)/(....)" "\$3/\$1/\$2" \
+	   -case  FromCandidate lower \
+	   -case  anonymous lower \
 	   -p source/new.csv > newtmp.csv
 }
 
@@ -52,7 +60,7 @@ do_contributions() {
 echo "converting"
 do_convert
 echo "making old"
-do_contributions oldtmp.csv    contributions_old.csv
+#do_contributions oldtmp.csv    contributions_old.csv
 echo "making new"
 do_contributions newtmp.csv    contributions_new.csv
 
@@ -60,6 +68,7 @@ cp contributions_old.csv contributions_final.csv
 tail -n+2 contributions_new.csv >>contributions_final.csv
 echo "making db"
 ${csv} -db " table.id boulder_campaign_contributions table.label {Boulder Campaign Contributions} \
+table.include file:source/include.txt \
 committee.numberOfSearchWidgets 5 \
 committee.type enumeration type.type enumeration candidate.type enumeration \
 official_filing.type enumeration \
@@ -73,10 +82,11 @@ committee.canlist true  candidate.canlist true  filing_date.canlist true  full_n
 street.canlist true  city.canlist true  contribution.canlist true  \
 state.type enumeration \
 filing_date.type date amended_date.type date transaction_date.type date 
-city.type enumeration zip.type string contribution_type.type enumeration anonymous.type enumeration \
+city.type enumeration zip.type string contribution_type.type enumeration contribution_type.addnot true \
+anonymous.type enumeration \
 " contributions_old.csv > boulder_campaign_contributionsdb.xml
 
-#exit
+exit
 sh /Users/jeffmc/source/ramadda/bin/scpgeode.sh 50.112.99.202 contributions_final.csv staging
 sh /Users/jeffmc/source/ramadda/bin/scpgeode.sh 50.112.99.202  boulder_campaign_contributionsdb.xml plugins
 
