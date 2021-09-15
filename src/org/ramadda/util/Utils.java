a45178a8f1ab3e0cf132a7375caaa4868b9e5836
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-09-15 13:14:52 -0600

    Add whitespace_chars

--- src/org/ramadda/util/Utils.java
+++ src/org/ramadda/util/Utils.java
@@ -96,6 +96,40 @@ public class Utils extends IO {
         new DecimalFormat("#0.0000"), new DecimalFormat("#0.00000"),
     };
 
+    //From https://stackoverflow.com/questions/4731055/whitespace-matching-regex-java
+    public static final String WHITESPACE_CHARS =  ""  
+	/* dummy empty string for homogeneity */
+	+ "\\u0009" // CHARACTER TABULATION
+	+ "\\u000A" // LINE FEED (LF)
+	+ "\\u000B" // LINE TABULATION
+	+ "\\u000C" // FORM FEED (FF)
+	+ "\\u000D" // CARRIAGE RETURN (CR)
+	+ "\\u0020" // SPACE
+	+ "\\u0085" // NEXT LINE (NEL) 
+	+ "\\u00A0" // NO-BREAK SPACE
+	+ "\\u1680" // OGHAM SPACE MARK
+	+ "\\u180E" // MONGOLIAN VOWEL SEPARATOR
+	+ "\\u2000" // EN QUAD 
+	+ "\\u2001" // EM QUAD 
+	+ "\\u2002" // EN SPACE
+	+ "\\u2003" // EM SPACE
+	+ "\\u2004" // THREE-PER-EM SPACE
+	+ "\\u2005" // FOUR-PER-EM SPACE
+	+ "\\u2006" // SIX-PER-EM SPACE
+	+ "\\u2007" // FIGURE SPACE
+	+ "\\u2008" // PUNCTUATION SPACE
+	+ "\\u2009" // THIN SPACE
+	+ "\\u200A" // HAIR SPACE
+	+ "\\u2028" // LINE SEPARATOR
+	+ "\\u2029" // PARAGRAPH SEPARATOR
+	+ "\\u202F" // NARROW NO-BREAK SPACE
+	+ "\\u205F" // MEDIUM MATHEMATICAL SPACE
+	+ "\\u3000" // IDEOGRAPHIC SPACE
+	;        
+
+    public static final String     WHITESPACE_CHARCLASS = "["  + WHITESPACE_CHARS + "]"; 
+
+
     /** _more_ */
     private static DecimalFormat[] COMMA_FORMATS = {
         new DecimalFormat("#,##0"), new DecimalFormat("#,##0.0"),
