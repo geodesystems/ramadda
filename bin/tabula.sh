cbb8c8c667d6fd759f0a16f665274f452cbc4424
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-11-10 21:00:17 -0700

    snapshot

--- bin/tabula.sh
+++ bin/tabula.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 #
 #This script is used to call the tabular.jar as a command line for extracting
-#tabular data from PDF files.
-#It is used by RAMADDA's CSV processing to support using PDFs as an input
-#data source. To use copy this file along with the /lib/tabula.jar
-#to some directory (e.g., ~/.ramadda/bin). Then set the environment variable:
-#export ramadda_tabula=<ramadda home>/bin/tabula.sh
+#tabular data from PDF files. It is used by RAMADDA's CSV processing to support
+#using PDFs as an input data source.
+#To use set the environment variable:
+#export RAMADDA_TABULA=/path/to/tabula.sh
 #
 myDir=`dirname $0`
-java -jar ${myDir}/tabula.jar $1
+if [ -e ${myDir}/lib/tabula.jar ]
+then
+    java -jar ${myDir}/lib/tabula.jar $1
+elif [ -e ${myDir}/tabula.jar ]
+then
+    java -jar ${myDir}/tabula.jar $1
+else
+    echo "Cannot find tabula.jar"
+fi
