93f4937e60c29c6ad1df004bf9e7e8c1854f3aba
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-11-13 11:06:51 -0700

    new captureDisplays.scpt

---
+++ test/captureDisplays.scpt
@@ -0,0 +1,20 @@
+set ok to 0
+set tries to 0
+
+repeat while ok = 0
+       tell application "Safari"
+           activate
+	   set winID to id of window 1
+	   set tries to tries + 1
+	   if tries > 10
+	        log "Too many tries: " & tries
+		exit repeat
+	   end if
+           set ok to   do JavaScript "Utils.areDisplaysReady();" in document 1 
+	   if ok = 0
+	        delay 1
+	   end if
+       end tell
+end repeat
+delay 1
+do shell script "screencapture   -a -x -l " & winID & " capture.png" 
--- test/runuitest.tcl
+++ test/runuitest.tcl
@@ -1,3 +1,11 @@
+set ::loc [file dirname [file normalize [info script]]]
+set ::cnt 0
+set  ::cnt2 0
+set ::tcnt 0
+set ::limit 500
+set ::root https://geodesystems.com/repository
+
+
 proc getUrl {url} {
     catch {exec rm tmp.csv}
     catch {exec curl -k $url > tmp.csv} err
@@ -7,12 +15,6 @@ proc getUrl {url} {
     set c
 }
 
-
-
-set ::loc [file dirname [file normalize [info script]]]
-set ::cnt 0
-set ::tcnt 0
-set ::limit 500
 
 proc finish {} {
     write  "</div>"
@@ -26,9 +28,48 @@ proc write {html {mode a}} {
 }
 
 
+
+
+proc capture {_group name id} {
+    regsub -all {[/ .'\",]+} $name _ clean
+    set image image_${clean}.png
+    set thumb thumb_${_group}_${clean}.png
+    set url $id
+    if {![regexp {^http} $url]} {
+	set url "$::root/entry/show?entryid=${id}#fortest"
+    }
+    set sleep 1
+    if {![file exists $thumb]} {
+	if {$::cnt2>$::limit} break
+	incr ::cnt2
+	puts stderr "\tprocessing $name"
+	#Bring Firefox to the front and tell it to reload the main page
+	set cmd "tell application \"Safari\" to set the URL of the front document to \"$url\""    
+	if {[catch {
+	    exec osascript -e {activate application "Safari"}
+	    exec osascript -e $cmd
+	    exec sleep $sleep
+	    exec osascript $::loc/captureDisplays.scpt
+	    puts stderr "Captured"
+	    exec cp capture.png $thumb
+	    exit
+	} err]} {
+	    puts stderr "Error: $err"
+	    write "Error: $err<hr>"
+	    write "</div>"
+	    exit
+	}
+    }
+    incr ::cnt
+    set line  "<a href=\"$url\"><div>#$::cnt $name</div>\n<img width=75% border=0 src=${thumb}>\n</a>\n"
+    write $line
+    finish
+}    
+
+
 proc runit {group id {groupLimit 10000}} {
     write "<h2>$group</h2>"
-    set url "https://geodesystems.com/repository/entry/show?ascending=true&orderby=name&entryid=${id}&output=default.csv&fields=name,id&showheader=false&showheader=false"
+    set url "$::root/entry/show?ascending=true&orderby=name&entryid=${id}&output=default.csv&fields=name,id&showheader=false&showheader=false"
     puts "group: $group"
     set csv [getUrl $url]
     set  ::cnt2 0
@@ -38,36 +79,7 @@ proc runit {group id {groupLimit 10000}} {
 	set line2 [string trim $line2]
 	if {$line2==""} continue;
 	foreach     {name id} [split $line2 ,] break
-	regsub -all {[/ .'\",]+} $name _ clean
-
-	set image image_${clean}.png
-	set thumb thumb_${_group}_${clean}.png
-	set url "https://geodesystems.com/repository/entry/show?entryid=${id}#fortest"
-	set sleep 10
-	if {![file exists $thumb]} {
-	    if {$::cnt2>$::limit} break
-	    incr ::cnt2
-	    puts stderr "\tprocessing $name"
-	    #Bring Firefox to the front and tell it to reload the main page
-	    set cmd "tell application \"Safari\" to set the URL of the front document to \"$url\""    
-	    if {[catch {
-		exec osascript -e {activate application "Safari"}
-		exec osascript -e $cmd
-		exec sleep $sleep
-		exec osascript $::loc/capture.scpt
-		exec cp capture.png $thumb
-
-	    } err]} {
-		puts stderr "Error: $err"
-		write "Error: $err<hr>"
-		write "</div>"
-		exit
-	    }
-	}
-	incr ::cnt
-	set line  "<a href=\"$url\"><div>#$::cnt $name</div>\n<img width=75% border=0 src=${thumb}>\n</a>\n"
-	write $line
-	finish
+	capture $_group $name $id
     }
 }
 
@@ -90,19 +102,25 @@ proc processGroup {root} {
     
 
 write "" w
-runit "Charts" 3ebcb4f4-fa4d-4fb3-9ede-d42ec7e0aa9d
-runit "Maps" 1d0fa3f5-407e-4a39-a3da-9a5ed7e1e687
-runit "Data tables" b36bb6fc-b2c4-4d12-8c31-1f4dcff6881e
-runit "Science Data" 1012d4bb-5e57-460a-95f5-07c997bd04e8
-#runit Covid 52644ac1-f6d6-45ea-88af-b5d2ec75742e
-runit "Text" 23847d93-4bca-4d54-a6db-f96a19be250b
-runit "Boulder and Colorado" 4624f63d-cd71-43e8-a558-83835c6b5541
-runit Dashboards eb4102f8-720f-4ef3-9211-0ce5940da04d
-runit "Media" bca6228e-3f8e-49d4-a20e-b5a0ea8a6441
-runit Cards e4b6667d-d640-4048-a756-b06e4c352a62 3
-#runit Features 26fff0d9-3de7-4bbd-8a6f-a26d8a287f4a
-runit Misc a7fe0b5c-5c1b-4005-afce-e92b082fa335
-runit Notebooks 38bb6bb4-3f51-4625-9acd-c79c4f445c78
+if {[llength $argv] != 0} {
+    foreach id $argv {
+	capture "Local"  "Page" $id
+    }
+} else {
+    runit "Charts" 3ebcb4f4-fa4d-4fb3-9ede-d42ec7e0aa9d
+    runit "Maps" 1d0fa3f5-407e-4a39-a3da-9a5ed7e1e687
+    runit "Data tables" b36bb6fc-b2c4-4d12-8c31-1f4dcff6881e
+    runit "Science Data" 1012d4bb-5e57-460a-95f5-07c997bd04e8
+    #runit Covid 52644ac1-f6d6-45ea-88af-b5d2ec75742e
+    runit "Text" 23847d93-4bca-4d54-a6db-f96a19be250b
+    runit "Boulder and Colorado" 4624f63d-cd71-43e8-a558-83835c6b5541
+    runit Dashboards eb4102f8-720f-4ef3-9211-0ce5940da04d
+    runit "Media" bca6228e-3f8e-49d4-a20e-b5a0ea8a6441
+    runit Cards e4b6667d-d640-4048-a756-b06e4c352a62 3
+    #runit Features 26fff0d9-3de7-4bbd-8a6f-a26d8a287f4a
+    runit Misc a7fe0b5c-5c1b-4005-afce-e92b082fa335
+    runit Notebooks 38bb6bb4-3f51-4625-9acd-c79c4f445c78
+}
 finish
 exit
 
