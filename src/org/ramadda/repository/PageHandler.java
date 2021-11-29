8c0948d3c1936055f5f4d2833f1958f03b5f0dbc
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-11-28 21:55:28 -0700

    update

--- src/org/ramadda/repository/PageHandler.java
+++ src/org/ramadda/repository/PageHandler.java
@@ -393,11 +393,14 @@ public class PageHandler extends RepositoryManager {
 
             StringBuilder cssImports = new StringBuilder();
             for (String file : readWebResources("/org/ramadda/repository/resources/web/cssimports.html")) {
+		HU.cssPreloadLink(cssImports, file.substring("preload:".length()));
+		/*
 		if(file.startsWith("preload:")) {
 		    HU.cssPreloadLink(cssImports, file.substring("preload:".length()));
 		} else {
 		    cssImports.append(HU.cssLink(file).trim() + "\n");
 		}
+		*/
             }
             webImports = applyBaseMacros(cssImports.toString().trim() + "\n"
                                          + jsImports.toString().trim() + "\n");
