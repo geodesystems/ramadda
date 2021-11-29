32d4a694026c017be9dfa979f5a1dc523907fb41
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-11-28 21:06:30 -0700

    change the js minify to use terser

--- src/org/ramadda/repository/htdocs/build.sh
+++ src/org/ramadda/repository/htdocs/build.sh
@@ -1,39 +1,45 @@
 #!/bin/sh
 
 #
-#This uses jsmin for minification
+#This uses jsmin and terser for minification
 #to install do:
 #pip install jsmin
 #
+#terser is a Node/NPM based package - https://github.com/terser/terser
+#
 
 dir=`dirname $0`
-minify="python -m jsmin "
-#yui="java -jar ${dir}/../../../../../lib/yuicompressor-2.4.8.jar"
+cssminify="python -m jsmin "
+
+jsminify="terser --compress --mangle -- "
+#If you don't want to install terser then set jsminify to
+#jsminify="${cssminify}"
+
 dest="${dir}/min"
 
 now=`date`
 echo "var build_date=\"RAMADDA build date: $now\";\n" > ${dir}/now.txt
 cat ${dir}/now.txt ${dir}/display/widgets.js ${dir}/display/display.js ${dir}/display/displaymanager.js ${dir}/display/pointdata.js   ${dir}/display/displaychart.js ${dir}/display/displayimages.js ${dir}/display/control.js ${dir}/display/notebook.js ${dir}/display/displayd3.js ${dir}/display/displaytext.js  ${dir}/display/displayentry.js ${dir}/display/displayext.js  ${dir}/display/displaymap.js ${dir}/display/editablemap.js ${dir}/display/displaymisc.js ${dir}/display/displaytable.js ${dir}/display/displayplotly.js ${dir}/display/displaythree.js > ${dest}/display_all.js
-${minify} ${dest}/display_all.js > ${dest}/display_all.min.js
-${minify} ${dest}/display_all.js > ${dest}/display_all.min.js
-#${minify} ${dir}/display/displayplotly.js > ${dest}/displayplotly.min.js
-${minify} ${dir}/ramadda.js > ${dest}/ramadda.min.js
-${minify} ${dir}/ramaddamap.js > ${dest}/ramaddamap.min.js
-${minify} ${dir}/utils.js > ${dest}/utils.min.js
-${minify} ${dir}/wiki.js > ${dest}/wiki.min.js
-${minify} ${dir}/repositories.js > ${dest}/repositories.min.js
-${minify} ${dir}/selectform.js > ${dest}/selectform.min.js
-${minify} ${dir}/entry.js > ${dest}/entry.min.js
-${minify} ${dir}/lib/openlayers/v2/OpenLayers.debug.js > ${dir}/lib/openlayers/v2/OpenLayers.mini.js
+${jsminify} ${dest}/display_all.js > ${dest}/display_all.min.js
+${jsminify} ${dest}/display_all.js > ${dest}/display_all.min.js
+#${jsminify} ${dir}/display/displayplotly.js > ${dest}/displayplotly.min.js
+${jsminify} ${dir}/ramadda.js > ${dest}/ramadda.min.js
+${jsminify} ${dir}/ramaddamap.js > ${dest}/ramaddamap.min.js
+${jsminify} ${dir}/utils.js > ${dest}/utils.min.js
+${jsminify} ${dir}/wiki.js > ${dest}/wiki.min.js
+${jsminify} ${dir}/repositories.js > ${dest}/repositories.min.js
+${jsminify} ${dir}/selectform.js > ${dest}/selectform.min.js
+${jsminify} ${dir}/entry.js > ${dest}/entry.min.js
+${jsminify} ${dir}/lib/openlayers/v2/OpenLayers.debug.js > ${dir}/lib/openlayers/v2/OpenLayers.mini.js
 cp  ${dir}/style.css  ${dir}/style.min.css
-${minify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
+${cssminify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
 ##don't minify the display.css as it screws up how jquery styles are overridden
-#${minify} ${dir}/display/display.css > ${dest}/display.min.css
+#${cssminify} ${dir}/display/display.css > ${dest}/display.min.css
 cp ${dir}/display/display.css  ${dest}/display.min.css
 
 
 cat ${dir}/now.txt ${dir}/utils.js ${dir}/ramadda.js ${dir}/entry.js ${dir}/wiki.js > ${dir}/tmp.js
-${minify} ${dir}/tmp.js > ${dest}/ramadda_all.min.js
+${jsminify} ${dir}/tmp.js > ${dest}/ramadda_all.min.js
 rm ${dir}/tmp.js
 
 
