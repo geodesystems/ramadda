42a5d82b9f89c8df396f23f12665e617142bef2a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-11-30 12:43:26 -0700

    more work on the bootstrap-reduced

--- src/org/ramadda/repository/htdocs/build.sh
+++ src/org/ramadda/repository/htdocs/build.sh
@@ -17,14 +17,18 @@ jsminify="terser --compress --mangle -- "
 
 dest="${dir}/min"
 
-${cssminify} ${dir}/lib/bootstrap.reduced.css > ${dir}/lib/bootstrap.reduced.min.css
+${cssminify} ${dir}/lib/bootstrap-reduced/css/bootstrap.css > ${dir}/lib/bootstrap-reduced/css/bootstrap.min.css
+
 #exit
 
 now=`date`
+echo "minifying display_all.js"
 echo "var build_date=\"RAMADDA build date: $now\";\n" > ${dir}/now.txt
 cat ${dir}/now.txt ${dir}/display/widgets.js ${dir}/display/display.js ${dir}/display/displaymanager.js ${dir}/display/pointdata.js   ${dir}/display/displaychart.js ${dir}/display/displayimages.js ${dir}/display/control.js ${dir}/display/notebook.js ${dir}/display/displayd3.js ${dir}/display/displaytext.js  ${dir}/display/displayentry.js ${dir}/display/displayext.js  ${dir}/display/displaymap.js ${dir}/display/editablemap.js ${dir}/display/displaymisc.js  ${dir}/display/displayplotly.js ${dir}/display/displaythree.js ${dir}/display/displaytable.js > ${dest}/display_all.js
 ${jsminify} ${dest}/display_all.js > ${dest}/display_all.min.js
-${jsminify} ${dest}/display_all.js > ${dest}/display_all.min.js
+
+
+echo "minifying the other files"
 #${jsminify} ${dir}/display/displayplotly.js > ${dest}/displayplotly.min.js
 ${jsminify} ${dir}/ramadda.js > ${dest}/ramadda.min.js
 ${jsminify} ${dir}/ramaddamap.js > ${dest}/ramaddamap.min.js
@@ -33,7 +37,12 @@ ${jsminify} ${dir}/wiki.js > ${dest}/wiki.min.js
 ${jsminify} ${dir}/repositories.js > ${dest}/repositories.min.js
 ${jsminify} ${dir}/selectform.js > ${dest}/selectform.min.js
 ${jsminify} ${dir}/entry.js > ${dest}/entry.min.js
+
+echo "minifying openlayers"
 ${jsminify} ${dir}/lib/openlayers/v2/OpenLayers.debug.js > ${dir}/lib/openlayers/v2/OpenLayers.mini.js
+
+
+
 cp  ${dir}/style.css  ${dir}/style.min.css
 ${cssminify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
 ##don't minify the display.css as it screws up how jquery styles are overridden
@@ -41,6 +50,7 @@ ${cssminify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
 cp ${dir}/display/display.css  ${dest}/display.min.css
 
 
+echo "minifying ramadda_all"
 cat ${dir}/now.txt ${dir}/utils.js ${dir}/ramadda.js ${dir}/entry.js ${dir}/wiki.js > ${dir}/tmp.js
 ${jsminify} ${dir}/tmp.js > ${dest}/ramadda_all.min.js
 rm ${dir}/tmp.js
