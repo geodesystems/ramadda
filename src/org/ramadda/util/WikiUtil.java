731839102e596c2fa960b93af4bb499eb30f9735
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-12-03 11:01:48 -0700

    tweak the +tabs

--- src/org/ramadda/util/WikiUtil.java
+++ src/org/ramadda/util/WikiUtil.java
@@ -1594,11 +1594,12 @@ public class WikiUtil {
 		    Hashtable props = getProps.apply(tline);
 		    if (props.get("min") != null) {
 			divClass = "ramadda-tabs-min";
-		    } else if (props.get("center") != null) {
-			divClass = "ramadda-tabs-center";
 		    } else if (props.get("minarrow") != null) {
 			divClass =
 			    "ramadda-tabs-min ramadda-tabs-minarrow";
+		    }
+		    if (props.get("center") != null) {
+			divClass += " ramadda-tabs-center ";
 		    }
 		    if (props.get("transparent") != null) {
 			divClass += " ramadda-tabs-transparent ";
