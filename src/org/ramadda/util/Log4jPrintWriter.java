ddb59c3bd46f98ecfffbf6714a81b8157088b95a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-12-13 23:45:58 -0700

    move to new log4j

--- src/org/ramadda/util/Log4jPrintWriter.java
+++ src/org/ramadda/util/Log4jPrintWriter.java
@@ -6,9 +6,8 @@ SPDX-License-Identifier: Apache-2.0
 package org.ramadda.util;
 
 
-import org.apache.log4j.Logger;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+//import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 
@@ -19,6 +18,7 @@ import java.io.*;
 
 
 public class Log4jPrintWriter extends PrintWriter {
+
 
     /** _more_ */
     private Logger log;
