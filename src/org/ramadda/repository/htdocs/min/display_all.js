2b732b8df75fdbddb96250da855af2964a94c36d
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-12-15 20:24:23 -0700

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Wed Dec 15 20:23:42 MST 2021";
+var build_date="RAMADDA build date: Wed Dec 15 20:24:09 MST 2021";
 
 /**
    Copyright 2008-2021 Geode Systems LLC
