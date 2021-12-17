38f9a789a4b747ab967b91de25203c0ee983f670
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2021-12-17 07:42:25 -0700

    snapshot

--- bin/tagandbag.sh
+++ bin/tagandbag.sh
@@ -1,6 +1,5 @@
-ANT=~/software/ant/bin/ant
 sh ${RAMADDA_GIT_ROOT}/bin/doall.sh
 tclsh ${RAMADDA_GIT_ROOT}/bin/changeminor.tcl
 cd ${RAMADDA_GIT_ROOT} 
-${ANT} -q tag  "$1"
+sh ${ANT_HOME} -q tag  "$1"
 sh ${RAMADDA_BIN}/makerelease.sh ${GEODESYSTEMS_IP}
