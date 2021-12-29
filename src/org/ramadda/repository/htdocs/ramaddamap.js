3a152d60a51a89eaab68773b420899e3690544e6
dmurray <don.murray@noaa.gov>
2021-12-29 12:18:04 -0700

    get rid of greek letters

--- src/org/ramadda/repository/htdocs/ramaddamap.js
+++ src/org/ramadda/repository/htdocs/ramaddamap.js
@@ -85,13 +85,13 @@ var MapUtils =  {
     distance: function(lat1,lon1,lat2,lon2) {
 	//From: https://www.movable-type.co.uk/scripts/latlong.html
 	const R = 6371e3; // metres
-	const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-	const φ2 = lat2 * Math.PI/180;
-	const Δφ = (lat2-lat1) * Math.PI/180;
-	const Δλ = (lon2-lon1) * Math.PI/180;
-	const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	const o1 = lat1 * Math.PI/180; // o, l in radians
+	const o2 = lat2 * Math.PI/180;
+	const deo = (lat2-lat1) * Math.PI/180;
+	const dl = (lon2-lon1) * Math.PI/180;
+	const a = Math.sin(deo/2) * Math.sin(deo/2) +
+              Math.cos(o1) * Math.cos(o2) *
+              Math.sin(dl/2) * Math.sin(dl/2);
 	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 	const d = R * c; // in metres
 	return MapUtils.metersToFeet(d);
