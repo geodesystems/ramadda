6011124e4b1a8faf1a873b862885e1923122f3b6
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-01-03 09:43:17 -0700

    add in display template docs

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {6.72.0}
+gen::defineMacro {<%ramadda.version%>} {7.9.0}
 
 
 namespace eval wiki {}
