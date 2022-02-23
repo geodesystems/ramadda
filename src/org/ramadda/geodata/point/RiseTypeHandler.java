5bcd2060d3e40f06829b2c20cec22fe3331eaf6b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-02-23 13:46:29 -0700

    new Rise point type

---
+++ src/org/ramadda/geodata/point/RiseTypeHandler.java
@@ -0,0 +1,112 @@
+/**
+Copyright (c) 2008-2021 Geode Systems LLC
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.ramadda.geodata.point;
+import org.json.*;
+import org.ramadda.data.record.*;
+import org.ramadda.data.services.PointTypeHandler;
+import org.ramadda.repository.*;
+
+import org.ramadda.repository.type.*;
+import org.ramadda.util.IO;
+import org.ramadda.util.Json;
+import org.ramadda.util.Utils;
+
+import org.w3c.dom.*;
+import java.io.*;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+
+
+/**
+ */
+public class RiseTypeHandler extends PointTypeHandler {
+
+
+    /** _more_ */
+    private static int IDX = PointTypeHandler.IDX_LAST + 1;
+
+    /**  */
+    public static final int IDX_RISE_ID = IDX++;
+    public static final int IDX_LOCATION_TYPE = IDX++;
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param node _more_
+     * @throws Exception _more_
+     */
+    public RiseTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param entry _more_
+     * @param fromImport _more_
+     *
+     * @throws Exception _more_
+     */
+    @Override
+    public void initializeNewEntry(Request request, Entry entry,
+                                   boolean fromImport)
+            throws Exception {
+	System.err.println("init");
+        super.initializeNewEntry(request, entry, fromImport);
+        if (fromImport) {
+            return;
+        }
+        String id = (String) entry.getValue(IDX_RISE_ID, "");
+	System.err.println("ID:" + id);
+	if(!Utils.stringDefined(id)) return;
+        String url =      "https://data.usbr.gov/rise/api/location/" +id;
+        try {
+	    System.err.println("URL: " + url);
+            String json = IO.doGet(new URL(url),"accept","application/vnd.api+json");
+	    System.err.println("json:" + json);
+            JSONObject obj           = new JSONObject(json);
+	    JSONObject data   =    obj.getJSONObject("data");
+	    JSONObject attrs   =    data.getJSONObject("attributes");	    
+	    JSONArray coords = attrs.getJSONObject("locationCoordinates").getJSONArray("coordinates");
+	    double lon = coords.getDouble(0);
+	    double lat = coords.getDouble(1);
+	    entry.setLocation(lat,lon);
+	    entry.setAltitude(attrs.getDouble("elevation"));
+	    entry.setValue(IDX_LOCATION_TYPE,attrs.getString("locationTypeName"));
+	    if(Utils.stringDefined(entry.getName())) entry.setName(attrs.getString("locationName"));
+	    if(attrs.has("locationRegionNames")) {
+		JSONArray a = attrs.getJSONArray("locationRegionNames");
+		for (int i = 0; i < a.length(); i++) {
+		    getMetadataManager().addMetadataTag(entry,"Region: " + a.getString(i));
+		}
+	    }
+	    if(attrs.has("locationTags")) {
+		JSONArray a = attrs.getJSONArray("locationTags");
+		for (int i = 0; i < a.length(); i++) {
+		    getMetadataManager().addMetadataTag(entry,"Location: " + a.getJSONObject(i).getString("tag"));
+		}
+
+	    }
+        } catch (Exception exc) {
+            getLogManager().logError("Error reading RISE URL:" + url,
+                                     exc);
+
+            throw new RuntimeException(
+                "Error accessing RISE API for site:" + id);
+        }
+    }
+
+
+}
