8e7e682bb2a7a9c7e6ced42953f80ca10308b7e1
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-03-14 11:58:42 -0600

    better role insert

--- src/org/ramadda/repository/htdocs/accessform.js
+++ src/org/ramadda/repository/htdocs/accessform.js
@@ -14,16 +14,29 @@ Ramadda.initAccessForm = function() {
 	roles.appendTo(holder);
     });
     roles.change(function() {
-	let v = roles.val();
-	if(v=="") return;
+	let role = roles.val();
+	if(role=="") return;
 	roles.val("");
+	role="\n"+role+"\n";
 	let textarea = $("#textarea_" + index);
 	let val = (textarea.val()||"").trim();
-	if(val!="") val+="\n";
-	val+=v;
-	textarea.val(val);
-	
 
+        let start = textarea[0].selectionStart;
+        let end = textarea[0].selectionEnd;
+	if(!Utils.isDefined(start) || val=="") {
+	    val = val+role;
+	} else  {
+	    val = val.substring(0, start) + role +
+                val.substring(end);
+	}
+	let newVal = "";
+	val.split("\n").forEach(line=>{
+	    line = line.trim();
+	    if(line=="") return;
+	    line = line+"\n";
+	    newVal = newVal+line;
+	});
+	textarea.val(newVal);
     });
 
 
