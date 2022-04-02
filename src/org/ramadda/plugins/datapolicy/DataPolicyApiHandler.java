21f1056d966407d757409de734c5a31f10edf907
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-04-02 11:33:04 -0600

    snapshot

--- src/org/ramadda/plugins/datapolicy/DataPolicyApiHandler.java
+++ src/org/ramadda/plugins/datapolicy/DataPolicyApiHandler.java
@@ -93,7 +93,7 @@ public class DataPolicyApiHandler extends RepositoryManager implements RequestHa
 	Utils.add(policy,"citation",qt(entry.getValue(DataPolicyTypeHandler.IDX_CITATION)));
 	Utils.add(policy,"license",qt(entry.getValue(DataPolicyTypeHandler.IDX_LICENSE)));
 	Utils.add(policy,"license_description",qt(entry.getValue(DataPolicyTypeHandler.IDX_LICENSE_DESCRIPTION)));			
-	Utils.add(policy,"url",getEntryManager().getEntryURL(null, entry));
+	Utils.add(policy,"url",qt(getEntryManager().getEntryURL(null, entry)));
 
 	List<String> access = new ArrayList<String>();
 
