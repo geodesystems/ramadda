d90a9a1e76cb3aa1afca924520e7b03adcc136c3
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-04-08 11:46:26 -0600

    add lorem tag

--- src/org/ramadda/util/WikiUtil.java
+++ src/org/ramadda/util/WikiUtil.java
@@ -2645,6 +2645,12 @@ public class WikiUtil {
                     continue;
                 }
 
+
+		if(tline.startsWith(":lorem")) {
+		    buff.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+		    continue;
+		}
+
                 if (tline.equals(":br")) {
                     buff.append("<br>");
 
