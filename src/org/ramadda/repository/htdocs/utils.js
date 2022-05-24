cf67bbdd61d2f92087853e8851ee3f0d4f40339b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-05-24 11:35:48 -0600

    snapshot

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -269,7 +269,7 @@ var Utils =  {
         return {x:nx, y:ny};
     },
     split: function(s,delim,trim,excludeEmpty) {
-        if(!s) return null;
+        if(s===null) return null;
         let l = [];
         s.split(delim||",").forEach((tok)=>{
             tok = tok.replace(/_comma_/g,",");
