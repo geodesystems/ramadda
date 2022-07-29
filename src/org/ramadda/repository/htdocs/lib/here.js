d5f671d99b435d3aea423b1c51ab501a77a5774d
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-07-29 14:29:42 -0600

    snapshot

--- src/org/ramadda/repository/htdocs/lib/here.js
+++ src/org/ramadda/repository/htdocs/lib/here.js
@@ -8,28 +8,26 @@
 Originally from https://github.com/heremaps/flexible-polyline/blob/master/javascript/index.js
 changed to not be a module
 */
-const DEFAULT_PRECISION = 5;
+var DEFAULT_PRECISION = 5;
 
-const ENCODING_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+var ENCODING_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
-const DECODING_TABLE = [
+var DECODING_TABLE = [
     62, -1, -1, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1,
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
     36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
 ];
 
-const FORMAT_VERSION = 1;
-
-const ABSENT = 0;
-const LEVEL = 1;
-const ALTITUDE = 2;
-const ELEVATION = 3;
+var FORMAT_VERSION = 1;
+var ABSENT = 0;
+var LEVEL = 1;
+var ALTITUDE = 2;
+var ELEVATION = 3;
 // Reserved values 4 and 5 should not be selectable
-const CUSTOM1 = 6;
-const CUSTOM2 = 7;
-
-const Num = typeof BigInt !== "undefined" ? BigInt : Number;
+var CUSTOM1 = 6;
+var CUSTOM2 = 7;
+var Num = typeof BigInt !== "undefined" ? BigInt : Number;
 
 function hereDecode(encoded) {
     const decoder = decodeUnsignedValues(encoded);
