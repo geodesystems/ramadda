d186de30c6d9a61e49684be2ac6a3722201f5964
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-08-04 01:34:50 -0600

    rework to use the new MapLayer

--- src/org/ramadda/repository/resources/mapextra.js
+++ src/org/ramadda/repository/resources/mapextra.js
@@ -1,17 +1,20 @@
-//this gets wrapped in
-//This gets called when a new map is created
-map.addBaseLayer(map.createXYZLayer("Boulder Co. Open Space", "https://caltopo.com/tile/geoimage_571EF841944797FD7E2492988BD9901BFD781CC44B18A10E3C4158D7F9484FF5/${z}/${x}/${y}@2x.png","Map from Caltopo",true));
+
+new MapLayer('7_day_emodis_vegdri_index','7-Day eMODIS VegDRI Index',
+	     'https://dmsdata.cr.usgs.gov/geoserver/quickdri_vegdri_conus_week_data/vegdri_conus_week_data/ows',
+	     {type:'wms',layer:'vegdri_conus_week_data',isOverlay:true,isForMap:false});
+new MapLayer('7_day_emodis_quickdri_index','7-Day eMODIS QuickDRI Index',
+	     'https://dmsdata.cr.usgs.gov/geoserver/quickdri_quickdri_conus_week_data/quickdri_conus_week_data/ows',
+	     {type:'wms',layer:'quickdri_conus_week_data',isOverlay:true,isForMap:false});
+new MapLayer('bocoopenspace','Boulder Co. Open Space','https://caltopo.com/tile/geoimage_571EF841944797FD7E2492988BD9901BFD781CC44B18A10E3C4158D7F9484FF5/${z}/${x}/${y}@2x.png',
+	     {attribution:'Map from Caltopo',isOverlay:true,isForMap:false});
 
 let maxZoom =  8;
 let wlayers = [ 
-    {name:'GOES Infrared',maxZoom:maxZoom,		    id:'goes-ir-4km-900913', alias:'goes-ir'},
+    {name:'GOES Infrared',maxZoom:maxZoom,   id:'goes-ir-4km-900913', alias:'goes-ir'},
     {name:'GOES Water Vapor', maxZoom:maxZoom,id:'goes-wv-4km-900913', alias:'goes-wv'},
     {name:'GOES Visible', maxZoom:maxZoom,id:'goes-vis-1km-900913', alias:'goes-visible'},
     {name:'NWS Radar', maxZoom:maxZoom,id:'nexrad-n0q-900913',alias:'nexrad'},
     {name:'24 hr precip', maxZoom:maxZoom,id:'q2-p24h-900913',alias:'precipition'}];
-
-//wlayers = [ {name:'NWS Radar', maxZoom:maxZoom,id:'nexrad-n0q-900913',alias:'nexrad'}]
-
 
 let _this = map;
 let get_my_url = function(bounds) {
@@ -28,6 +31,32 @@ let get_my_url = function(bounds) {
 };
 
 
+let ctor = (mapLayer,map)=>{
+    return  new OpenLayers.Layer.TMS(
+        mapLayer.name,
+        'https://mesonet.agron.iastate.edu/cache/tile.py/', {
+            layername: mapLayer.id,
+            service: '1.0.0',
+            type: 'png',
+            visibility: false,
+            getURL: get_my_url,
+            isBaseLayer: false,
+	    maxZoomLevel: mapLayer.opts.maxZoom,
+	    minZoomLevel:mapLayer.minZoom,			    
+        });
+};
+
+wlayers.forEach(l=>{
+    new MapLayer(l.id,l.name,'',
+		 {isOverlay:true,
+		  refresh:300,
+		  minZoom:l.minZoom,
+		  maxZoom:l.maxZoom,
+		  creator:ctor
+		 });
+});
+
+/*
 wlayers.forEach(l=>{
     let layer = new OpenLayers.Layer.TMS(
         l.name,
@@ -50,8 +79,11 @@ wlayers.forEach(l=>{
     };
     setTimeout(redrawFunc,1000*60*5);
 
+
+
     map.baseLayers[l.id] = layer;
     if(l.alias) map.baseLayers[l.alias] = layer;		    
     layer.ramaddaId = l.id;
     map.addLayer(layer,true);
 });
+*/
