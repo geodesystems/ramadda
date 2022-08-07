64bd677db26419fb542fffbac28689cfe229dfa4
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-08-07 01:37:10 -0600

    dedebug

--- src/org/ramadda/repository/htdocs/lib/openlayers/v2/OpenLayers.debug.js
+++ src/org/ramadda/repository/htdocs/lib/openlayers/v2/OpenLayers.debug.js
@@ -28686,7 +28686,6 @@ OpenLayers.Map = OpenLayers.Class({
                 for (var i=0, len=this.popups.length; i<len; i++) {
                     this.popups[i].updatePosition();
                 }
-		console.log(Utils.getStack(12));
                 this.events.triggerEvent("zoomend");
             }
         }
