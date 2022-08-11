6206af0c60440b8f90056cffbda7e56f302ab4ee
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-08-11 15:42:28 -0600

    Add ANSI_RED, etc

--- src/org/ramadda/util/Utils.java
+++ src/org/ramadda/util/Utils.java
@@ -81,6 +81,17 @@ public class Utils extends IO {
         "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     };
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+
     /** _more_ */
     private static DecimalFormat[] FORMATS = {
         new DecimalFormat("#0"), new DecimalFormat("#0.0"),
