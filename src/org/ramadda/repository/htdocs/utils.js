aabc03bbdf64fedcb77a30d4de766a4e28a996d4
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-08-15 23:12:08 -0600

    add in RamaddaJsonUtil.init

--- src/org/ramadda/repository/htdocs/utils.js
+++ src/org/ramadda/repository/htdocs/utils.js
@@ -5854,6 +5854,33 @@ dateFormat.i18n = {
     ]
 };
 
+RamaddaJsonUtil = {
+    init:function(id) {
+	let brackets = jqid(id).find('.ramadda-json-openbracket');
+	brackets.each(function() {
+	    $(this).append(HU.span(['visible','true','class','ramadda-json-toggle'],HU.getIconImage(icon_folderopen)));
+	});
+	let toggles = 	brackets.find('.ramadda-json-toggle');
+	toggles.css('cursor','pointer');
+	toggles.click(function() {
+	    let parent = $(this).closest('.ramadda-json-openbracket');
+	    let next = parent.next('.ramadda-json-block');
+	    let visible = $(this).attr('visible')=='true';
+	    visible=!visible;
+	    $(this).attr('visible',visible);
+	    if(visible) {
+		next.show(500);
+		$(this).html(HU.getIconImage(icon_folderopen));
+	    } else {
+		next.hide(500);
+		$(this).html(HU.getIconImage(icon_folderclosed));
+	    }
+	});
+    }
+}
+
+
+
 function jqid(id) {
     return HtmlUtils.jqid(id);
 }
