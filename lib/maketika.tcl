9f06f40a44b5de9fea88306778aa54f19402238e
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-08-26 15:02:29 -0600

    get rid of anything called log4j in ramaddatika.jar

--- lib/maketika.tcl
+++ lib/maketika.tcl
@@ -4,10 +4,33 @@ cd tika
 puts "unzipping tika"
 set rc [catch {exec unzip -o ../tika-app-2.1.0.jar} msg]
 
-foreach del {com/google dods ucar thredds org/slf4j org/apache/logging org/apache/http  org/joda} {
+set delete {
+    com/google dods ucar thredds org/slf4j org/apache/logging org/apache/http  org/joda
+    log4j2.xml
+    log4j2_batch_process.properties
+    META-INF/org/apache/logging/log4j
+    META-INF/maven/org.apache.logging.log4j
+    META-INF/maven/org.apache.logging.log4j/log4j-api
+    META-INF/maven/org.apache.logging.log4j/log4j-core
+    META-INF/maven/org.apache.logging.log4j/log4j-slf4j-impl
+    META-INF/versions/9/org/apache/logging/log4j
+    META-INF/services/org.apache.logging.log4j.message.ThreadDumpMessage$ThreadInfoFactory
+    META-INF/services/org.apache.logging.log4j.spi.Provider
+    META-INF/services/org.apache.logging.log4j.core.util.ContextDataProvider
+    META-INF/services/org.apache.logging.log4j.util.PropertySource
+    ./pipes-fork-server-default-log4j2.xml
+}
+
+foreach del $delete {
     puts "deleting $del"
     file delete -force $del
 }
+
+
+
+
+#nuke any remaining log4j thing
+#exec {rm -r `find . -name "*log4j*"`}
 
 puts "Making ramaddatika.jar"
 puts "pwd: [pwd]"
