55ab7a8217d7481871dba857102fe2a28fb41063
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-12-08 20:51:43 -0700

    snapshot

--- src/org/ramadda/projects/scripts/nclcts.sh
+++ src/org/ramadda/projects/scripts/nclcts.sh
@@ -6,4 +6,6 @@ java  -Djava.awt.headless=true org.ramadda.util.text.Seesv -header "red,green,bl
       -if -find 0 "\." -scale 0,1,2 0 255 0 -decimals 0,1,2 0 -endif \
       -multifiles "processed/\${file_shortname}.csv" \
       -template "\${file_shortname}:{colors:[" "'rgb(\${0},\${1},\${2})'" "," "]},\n" \
-      *.rgb 
+      cb_9step.rgb
+
+#      *.rgb 
