5f4aa17657558d43706315ed40f698c27f13bbc5
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2022-12-28 13:55:24 -0700

    dedbug

--- src/org/ramadda/repository/htdocs/display/control.js
+++ src/org/ramadda/repository/htdocs/display/control.js
@@ -1089,7 +1089,6 @@ function RamaddaMenuDisplay(displayManager, id, properties) {
 		this.idToRecord = {};
 		let count = 0;
 		let maxPerRow  = this.getProperty('maxPerRow',-1);
-		console.log('max:'+ maxPerRow);
 		let html = '';
 		if(maxPerRow>=0) {
 		    html=HU.open('div',['style','text-align:center;']);
