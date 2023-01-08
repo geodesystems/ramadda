f3057dbe5d838dca52793a0bdcc6a6665b807ffa
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-01-08 16:40:06 -0700

    new script to minify css

--- src/org/ramadda/repository/htdocs/build.sh
+++ src/org/ramadda/repository/htdocs/build.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
+#To minify CSS this uses a tcl script in the /bin directory of the tree
+#Set the environment variable:
+#RAMADDA_ROOT=/.../ramadda
+#You will also need to have tclsh installed
+
 #
-#This uses jsmin and terser for minification
-#to install do:
-#pip install jsmin
-#
+#This uses terser for JS minification
 #terser is a Node/NPM based package - https://github.com/terser/terser
 #
 
 dir=`dirname $0`
-cssminify="python3 -m jsmin "
-jsminify="terser --compress --mangle -- "
-#If you don't want to install terser then set jsminify to
-#jsminify="${cssminify}"
-
-
 dest="${dir}/min"
+
+jsminify="terser --compress --mangle -- "
+
+cssminify() {
+    echo "minifying $1 to $2"
+    tclsh ${RAMADDA_ROOT}/bin/cssmin.tcl $1 $2
+}
+
 
 #echo "minifying bootstrap.css"
 #${cssminify} ${dir}/lib/bootstrap-reduced/css/bootstrap.css > ${dir}/lib/bootstrap-reduced/css/bootstrap.min.css
 
-#exit
 
 now=`date`
-echo "minifying display_all.js"
+echo "making and minifying display_all.js"
 echo "var build_date=\"RAMADDA build date: $now\";\n" > ${dir}/now.txt
 cat ${dir}/now.txt ${dir}/colortables.js ${dir}/display/widgets.js ${dir}/display/display.js ${dir}/display/displaymanager.js ${dir}/display/pointdata.js   ${dir}/display/displaychart.js ${dir}/display/displayimages.js ${dir}/display/control.js ${dir}/display/notebook.js ${dir}/display/displayd3.js ${dir}/display/displaytext.js  ${dir}/display/displayentry.js ${dir}/display/displayext.js  ${dir}/display/displaymap.js ${dir}/display/imdv.js ${dir}/display/displaymisc.js  ${dir}/display/displayplotly.js ${dir}/display/displaythree.js ${dir}/display/displaytable.js > ${dest}/display_all.js
 ${jsminify} ${dest}/display_all.js > ${dest}/display_all.min.js
@@ -43,15 +46,11 @@ echo "minifying openlayers"
 ${jsminify} ${dir}/lib/openlayers/v2/OpenLayers.debug.js > ${dir}/lib/openlayers/v2/OpenLayers.mini.js
 
 
-cp  ${dir}/style.css  ${dir}/style.min.css
 
-#Don't minify ramaddamap.css as it breaks some things
-#${cssminify} ${dir}/ramaddamap.css > ${dest}/ramaddamap.min.css
-cp  ${dir}/ramaddamap.css  ${dest}/ramaddamap.min.css
 
-##don't minify the display.css as it screws up how jquery styles are overridden
-#${cssminify} ${dir}/display/display.css > ${dest}/display.min.css
-cp ${dir}/display/display.css  ${dest}/display.min.css
+cssminify  ${dir}/style.css  ${dir}/style.min.css
+cssminify ${dir}/ramaddamap.css  ${dest}/ramaddamap.min.css
+cssminify ${dir}/display/display.css  ${dest}/display.min.css
 
 
 echo "minifying ramadda_all"
