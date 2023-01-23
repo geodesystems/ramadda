b4e202f067fd245d3247c970a5026e6d790bad39
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-01-22 18:03:50 -0700

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Sun Jan 22 17:34:22 MST 2023";
+var build_date="RAMADDA build date: Sun Jan 22 18:03:36 MST 2023";
 
 /*
  * Copyright (c) 2008-2023 Geode Systems LLC
@@ -44630,7 +44630,16 @@ MapGlyph.prototype = {
 	} else 	if(this.features.length) {
 	    this.display.getMap().centerOnFeatures(this.features);
 	} else if(this.mapLayer) {
-	    this.display.getMap().zoomToLayer(this.mapLayer);
+	    let bounds = this.mapLayer.getVisibility()?this.mapLayer.getDataExtent():null;
+	    if(this.imageLayers) {
+		this.imageLayers.forEach(obj=>{
+		    if(!obj.layer || !obj.layer.getVisibility()) return;
+		    bounds = MapUtils.extendBounds(bounds,
+						   this.getMap().getLayerVisbileExtent(obj.layer)||obj.layer.extent);
+		});
+	    }
+	    if(bounds)
+		this.display.getMap().zoomToBounds(bounds);
 	} else	if(this.displayInfo?.display) {
 	    if(this.displayInfo.display.myFeatureLayer && (
 		!Utils.isDefined(this.displayInfo.display.layerVisible) ||
