ad9a15c0d479b7bdbd08d620596d41189670d429
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-04-17 18:55:02 -0600

    update

--- bin/tagandbag.sh
+++ bin/tagandbag.sh
@@ -2,4 +2,5 @@ sh ${RAMADDA_GIT_ROOT}/bin/doall.sh
 tclsh ${RAMADDA_GIT_ROOT}/bin/changeminor.tcl
 cd ${RAMADDA_GIT_ROOT} 
 sh ${ANT_HOME} -q tag  "$1"
+echo "calling makerelease.sh"
 sh ${RAMADDA_BIN}/makerelease.sh ${GEODESYSTEMS_IP}
