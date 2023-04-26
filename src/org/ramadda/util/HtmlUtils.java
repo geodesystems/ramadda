69b9b13eaa3c0c824d0308246147adb1c81339ab
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-04-26 02:52:41 -0600

    snapshot

--- src/org/ramadda/util/HtmlUtils.java
+++ src/org/ramadda/util/HtmlUtils.java
@@ -2,8 +2,7 @@
 Copyright (c) 2008-2021 Geode Systems LLC
 SPDX-License-Identifier: Apache-2.0
 */
-// Copyright (c) 2008-2021 Geode Systems LLC
-// SPDX-License-Identifier: Apache-2.0
+
 
 package org.ramadda.util;
 
