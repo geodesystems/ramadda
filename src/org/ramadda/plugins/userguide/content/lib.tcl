3c80336a1c231967201d540f4b999695d1fd9d0a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-05-11 16:04:47 -0600

    update seesv docs

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {12.82.0}
+gen::defineMacro {<%ramadda.version%>} {12.98.0}
 
 
 namespace eval wiki {}
