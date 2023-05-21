07f3b84bc64f2192f579ed65c16af9b141152962
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-05-20 20:18:42 -0600

    updates

--- release/installer/updateimage.sh
+++ release/installer/updateimage.sh
@@ -6,20 +6,24 @@
 #
 
 
-INSTALLER_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source "${INSTALLER_DIR}/lib.sh"
+MYDIR=`dirname $0`
+source "${MYDIR}/lib.sh"
 
-host=`curl http://169.254.169.254/latest/meta-data/public-ipv4 2>/dev/null`
-printf "OK, the public IP of this machine is ${host}\n"
+export MYIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4 2>/dev/null`
 
+service ${SERVICE_NAME} stop
+
+##update with latest ramadda
+download_installer
+install_ramadda
 
 generate_keystore
 generate_install_password
 
-service ${SERVICE_NAME} restart
+service ${SERVICE_NAME} start
 
 header "Image update completed";
-printf "Finish the configuration at https://${host}/repository\n"
+printf "Finish the configuration at https://${MYIP}/repository\n"
 printf "The installation password is ${install_password}\n"
 
 
