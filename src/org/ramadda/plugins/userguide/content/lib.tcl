c0988d4b666a8e26c9228552e4b4663444544a9b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-08-18 11:00:05 -0600

    add /info section to the faq

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {13.73.0}
+gen::defineMacro {<%ramadda.version%>} {13.94.0}
 
 
 namespace eval wiki {}
