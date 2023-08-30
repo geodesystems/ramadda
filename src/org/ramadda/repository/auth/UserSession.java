f8b3425b94384fdb581758086156fff2bcaa6f8f
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-08-30 08:00:32 -0600

    code cleanup

--- src/org/ramadda/repository/auth/UserSession.java
+++ src/org/ramadda/repository/auth/UserSession.java
@@ -1,57 +1,14 @@
 /**
-Copyright (c) 2008-2023 Geode Systems LLC
-SPDX-License-Identifier: Apache-2.0
+   Copyright (c) 2008-2023 Geode Systems LLC
+   SPDX-License-Identifier: Apache-2.0
 */
 
 package org.ramadda.repository.auth;
-
-
 import org.ramadda.repository.*;
+import org.ramadda.util.Utils;
 
-
-import org.ramadda.repository.database.*;
-import org.ramadda.util.HtmlUtils;
-import org.ramadda.util.TTLCache;
-
-
-import org.ramadda.util.sql.Clause;
-
-
-import org.ramadda.util.sql.SqlUtil;
-
-import ucar.unidata.util.Cache;
-import ucar.unidata.util.DateUtil;
-
-import ucar.unidata.util.IOUtil;
-import ucar.unidata.util.LogUtil;
-import ucar.unidata.util.Misc;
-import ucar.unidata.util.StringUtil;
-import ucar.unidata.util.TwoFacedObject;
-import ucar.unidata.xml.XmlUtil;
-
-import java.io.File;
-
-
-import java.io.UnsupportedEncodingException;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.List;
-import java.util.Properties;
-
-
-
-
 
 
 /**
@@ -112,7 +69,7 @@ public class UserSession {
      * @return _more_
      */
     public String toString() {
-        return "session:" + user + " id:" + id.substring(0, 5);
+        return "session:" + user + " id:" + Utils.clip(id,10,"...");
     }
 
     /**
@@ -229,7 +186,6 @@ public class UserSession {
     public Date getLastActivity() {
         return lastActivity;
     }
-
 
 
 }
