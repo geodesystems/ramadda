be1513a1157b1616af1f0e3f7408b0de29201aec
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-08-30 11:01:09 -0600

    tweak embed entry dialog title

--- src/org/ramadda/repository/htdocs/wiki.js
+++ src/org/ramadda/repository/htdocs/wiki.js
@@ -406,7 +406,7 @@ WikiEditor.prototype = {
 	    SPACE3 +
 	    HU.div([STYLE,HU.css("display","inline-block"), CLASS,"ramadda-button",ID,this.domId("addcancel")],"Cancel");
 	html = HU.div([STYLE,HU.css('padding','10px','width','400px')],html);
-	let dialog = this.addDialog = HU.makeDialog({content:html,anchor:this.getScroller(),title:"New Entry",header:true,
+	let dialog = this.addDialog = HU.makeDialog({content:html,anchor:this.getScroller(),title:"Select Entry",header:true,
 						     sticky:true,draggable:true,modal:true,xmodalContentsCss:HU.css('left','50px')});
 	let menu = dialog.find("#"+this.domId("addtype"));
 	HU.initSelect(menu);
