98a43e4ebc95511bbde01c1a61e74f460839469b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-09-10 08:26:06 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Sat Sep  9 08:21:18 MDT 2023";
+var build_date="RAMADDA build date: Sun Sep 10 08:25:51 MDT 2023";
 
 /*
  * Copyright (c) 2008-2023 Geode Systems LLC
@@ -39896,7 +39896,7 @@ function RamaddaMapDisplay(displayManager, id, properties) {
 		    } else if(colorByColor) {
 			polygonProps.strokeColor=colorByColor;
 		    }
-		    let polys = this.map.createPolygonFromString(s, polygonProps,latlon,null);
+		    let polys = this.map.createPolygonFromString('',s, polygonProps,latlon,null);
 		    polys.forEach(poly=>{
 			poly.textGetter = textGetter;
 			poly.record = record;
