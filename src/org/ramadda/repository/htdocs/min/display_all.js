8c665873ccc1ce509fd72d3829fcf396a8cd0c8b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-09-14 17:33:51 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Thu Sep 14 14:18:23 MDT 2023";
+var build_date="RAMADDA build date: Thu Sep 14 17:33:36 MDT 2023";
 
 /*
  * Copyright (c) 2008-2023 Geode Systems LLC
@@ -46355,7 +46355,8 @@ HU.input('','',[ATTR_CLASS,'pathoutput','size','60',ATTR_STYLE,'margin-bottom:0.
 			   isMultiEntry:true,
 			   icon:Ramadda.getUrl("/icons/folder.png")});
 
-	    new GlyphType(this,GLYPH_DATA,"Data", {},
+	    new GlyphType(this,GLYPH_DATA,"Data",
+ 			  {externalGraphic: externalGraphic},
 			  MyEntryPoint,
 			  {isData:true, tooltip:'Select a map data entry to display',
 			   icon:Ramadda.getUrl("/icons/chart.png")});
