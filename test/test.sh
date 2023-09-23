ef1130b563e8c132eeec39d32d9c2c1065f5736c
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-09-23 09:02:22 -0600

    update

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,2 @@
-URL="https://localhost:8430/repository/entry/show?entryid=00d215ba-f3c8-4f09-a095-341f2b06eaac&output=points.product&product=points.json&max=5000"
-URL="https://localhost:8430/repository/entry/show?entryid=fbe4d7a9-1079-404b-a438-5b98b32a2a7a&output=points.product&product=points.json&max=30000"
-
-XURL="https://ramadda.org/repository/entry/show?entryid=66852dae-2ac3-4a49-a56d-f264c11d5c06&output=points.product&product=points.json&max=30000"
-echo $URL
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-time wget --no-check-certificate -q -O  tmp.csv "${URL}"
-
+#!/bin/sh
+java org.ramadda.util.Test "$@"
