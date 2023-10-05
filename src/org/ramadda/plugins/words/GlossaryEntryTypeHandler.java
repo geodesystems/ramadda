65fd1339cc94248e10da5f243f61ffb1f23e472c
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-10-04 19:10:53 -0600

    new dir

--- src/org/ramadda/plugins/words/GlossaryEntryTypeHandler.java
+++ src/org/ramadda/plugins/words/GlossaryEntryTypeHandler.java
@@ -3,7 +3,7 @@ Copyright (c) 2008-2023 Geode Systems LLC
 SPDX-License-Identifier: Apache-2.0
 */
 
-package org.ramadda.plugins.glossary;
+package org.ramadda.plugins.words;
 
 
 import org.ramadda.repository.*;
