9b370da4bd811bbeeedffdbf532af74ab4793eaa
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-10-20 08:24:41 -0600

    add a faq item for the ramadda.wiki.table.formopen property

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {15.11.0}
+gen::defineMacro {<%ramadda.version%>} {15.33.0}
 
 
 namespace eval wiki {}
