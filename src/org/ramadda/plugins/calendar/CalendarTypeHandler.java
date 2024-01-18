2f0f6783517c76d46e7542995a0975e1016e0879
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-01-18 06:06:53 -0700

    some cleanup

--- src/org/ramadda/plugins/calendar/CalendarTypeHandler.java
+++ src/org/ramadda/plugins/calendar/CalendarTypeHandler.java
@@ -5,45 +5,12 @@ SPDX-License-Identifier: Apache-2.0
 
 package org.ramadda.plugins.calendar;
 
-
 import org.ramadda.repository.*;
-import org.ramadda.repository.auth.*;
-import org.ramadda.repository.metadata.*;
 import org.ramadda.repository.output.*;
 import org.ramadda.repository.type.*;
 
-import org.ramadda.util.HtmlUtils;
-
-
-import org.ramadda.util.sql.Clause;
-
-
-import org.ramadda.util.sql.SqlUtil;
-import org.ramadda.util.sql.SqlUtil;
-
-
 import org.w3c.dom.*;
 
-import ucar.unidata.util.DateUtil;
-import ucar.unidata.util.HttpServer;
-import ucar.unidata.util.IOUtil;
-import ucar.unidata.util.LogUtil;
-import ucar.unidata.util.Misc;
-import ucar.unidata.util.StringUtil;
-import ucar.unidata.xml.XmlUtil;
-
-import java.sql.PreparedStatement;
-
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Properties;
 
 
 /**
