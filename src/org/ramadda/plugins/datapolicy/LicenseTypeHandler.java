0d65bfd85534fb81162ba8028a19230f0eeaa062
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-01-28 10:46:25 -0700

    new handler to implement a type_license wiki tag

---
+++ src/org/ramadda/plugins/datapolicy/LicenseTypeHandler.java
@@ -0,0 +1,85 @@
+/**
+Copyright (c) 2008-2023 Geode Systems LLC
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.ramadda.plugins.datapolicy;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.auth.*;
+import org.ramadda.repository.database.*;
+import org.ramadda.repository.metadata.*;
+import org.ramadda.repository.output.*;
+import org.ramadda.repository.type.*;
+import org.ramadda.repository.util.SelectInfo;
+import org.ramadda.util.HtmlUtils;
+import org.ramadda.util.Utils;
+import org.ramadda.util.WikiUtil;
+
+import org.w3c.dom.*;
+
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+
+/**
+ *
+ *
+ */
+@SuppressWarnings("unchecked")
+public class LicenseTypeHandler extends GenericTypeHandler {
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public LicenseTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+    /**
+     * _more_
+     *
+     * @param wikiUtil _more_
+     * @param request _more_
+     * @param originalEntry _more_
+     * @param entry _more_
+     * @param tag _more_
+     * @param props _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    @Override
+    public String getWikiInclude(WikiUtil wikiUtil, Request request,
+                                 Entry originalEntry, Entry entry,
+                                 String tag, Hashtable props)
+            throws Exception {
+
+        if ( !tag.equals("type_license")) {
+            return super.getWikiInclude(wikiUtil, request, originalEntry,
+                                        entry, tag, props);
+        }
+	String id = (String)entry.getValue("license_id");
+	String wiki = "{{license  license=\"" + id+"\"  includeName=\"true\" showDescription=\"true\" decorate=\"true\" }}";
+
+	return getWikiManager().wikifyEntry(request, entry, wiki);
+
+    }
+
+
+
+
+}
