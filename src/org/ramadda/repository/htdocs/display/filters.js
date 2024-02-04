320b94c18df1954cf6cc7f01c94837e28c3a2fe1
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-02-04 06:41:45 -0700

    tweak the filter suggest

--- src/org/ramadda/repository/htdocs/display/filters.js
+++ src/org/ramadda/repository/htdocs/display/filters.js
@@ -498,10 +498,13 @@ function RecordFilter(display,filterFieldId, properties) {
 			let input = $(this);
 			let v = input.val().toLowerCase();
 			let html = '';
+			let seen = {};
 			_this.records.forEach(r=>{
 			    let rv=r.getValueFromField(_this.getId());
 			    if(!rv) return;
 			    rv =String(rv);
+			    if(seen[rv]) return;
+			    seen[rv] = true;
 			    let _rv = rv.toLowerCase();
 			    if(_rv.indexOf(v)>0) {
 				html+=HU.div([ATTR_CLASS,'ramadda-clickable',
