6c3d871e929f60d2af304fbe13deb9339ba1da00
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-02-23 04:24:28 -0700

    support USER argument

--- bin/defineaws.sh
+++ bin/defineaws.sh
@@ -13,9 +13,13 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ID=$1
 IP=$2
 PEM=$3
+USER=ec2-user
+if [ $# -ge 4 ]; then
+    USER=$4
+fi
 
 
-alias go$ID="ssh -i $PEM ec2-user@$IP"
+alias go$ID="ssh -i $PEM ${USER}@$IP"
 alias put$ID="sh ${MYDIR}/scpaws.sh ${IP} ${PEM}"
 alias get$ID="sh ${MYDIR}/scpaws2.sh ${IP} ${PEM}"
 
