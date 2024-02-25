8fc314b9881757bdb244abd9fcc75527c2495156
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-02-25 04:23:44 -0700

    snapshot

--- bin/defineaws.sh
+++ bin/defineaws.sh
@@ -14,12 +14,19 @@ ID=$1
 IP=$2
 PEM=$3
 USER=ec2-user
+RAMADDAINSTALL=/mnt/ramadda/ramaddainstall
 if [ $# -ge 4 ]; then
     USER=$4
+fi
+
+
+if [ $# -ge 5 ]; then
+    RAMADDAINSTALL=$5
 fi
 
 
 alias go$ID="ssh -i $PEM ${USER}@$IP"
 alias put$ID="sh ${MYDIR}/scpaws.sh ${IP} ${PEM}"
 alias get$ID="sh ${MYDIR}/scpaws2.sh ${IP} ${PEM}"
+alias update$ID='ssh -i ${PEM} ${USER}@${IP} "sudo bash ramaddainstaller/awsupdate.sh -dir ${RAMADDAINSTALL}"'
 
