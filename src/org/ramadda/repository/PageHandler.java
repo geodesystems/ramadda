98ef31787ef78c34ea182f3600607dacc08b5be9
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-02 20:31:05 -0700

    styling

--- src/org/ramadda/repository/PageHandler.java
+++ src/org/ramadda/repository/PageHandler.java
@@ -86,7 +86,7 @@ public class PageHandler extends RepositoryManager {
 
     /** _more_ */
     private static final String ACK_MESSAGE =
-        "<div class='ramadda-acknowledgement'><a href='https://geodesystems.com'><br><img loading=lazy width=100px  src='${cdnpath}/images/poweredby.png'></a></div>";
+        "<div class='ramadda-acknowledgement'><a href='https://geodesystems.com'><img loading=lazy width=100px  src='${cdnpath}/images/poweredby.png'></a></div>";
 
     /**  */
     private String ackMessage;
