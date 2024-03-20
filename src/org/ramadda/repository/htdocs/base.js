79c1500cb53d5c9abf7ac936b178a61b8fc981d9
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-20 06:06:05 -0600

    add a isMobile flag to base.js

--- src/org/ramadda/repository/htdocs/base.js
+++ src/org/ramadda/repository/htdocs/base.js
@@ -1,6 +1,7 @@
 var ramaddaBaseUrl = "${ramadda.root}";
 var ramaddaBaseEntry = "${ramadda.baseentry}";
 var ramaddaBaseHtdocs = "${ramadda.htdocs}";
+var ramaddaIsMobile=${ramadda.ismobile};
 var ramaddaCdn = "${ramadda.cdn}";
 var ramaddaUser = "${ramadda.user}";
 var ramaddaTreeSearchEnabled = ${ramadda.search.tree};
