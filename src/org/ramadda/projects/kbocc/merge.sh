0d9036c416c450e5d1306b0a39259b05981ac8d2
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-27 13:05:10 -0600

    add some comments

--- src/org/ramadda/projects/kbocc/merge.sh
+++ src/org/ramadda/projects/kbocc/merge.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-#the SEESV environment variable needs to point to the directory holding the seesv.sh script in RAMADDA's SeeSV  release
-#
+#the SEESV environment variable should be set and needs to point to the directory
+#holding the seesv.sh script in RAMADDA's SeeSV  release 
 
+#usage:
+#sh merge.sh <any number of the kbocc csv files>   > merged.csv
 
+#Then add the merged.csv to your RAMADDA as a "RAMADDA CSV Data" entry type
+
+#merge function is called for each of the .csv files. It processes them, converting dates, stripping columns,
+#and generating the hours_in_year field
 merge() {
 sh ${SEESV}/seesv.sh "-delimiter" "?" "-skiplines" "1" "-set" "0" "0" "number" "-set" "1" "0" "Date Time" "-set" "2" "0" "Temperature" "-notcolumns" "0,3-10" "-indateformats" "MM/dd/yy hh:mm:ss a;MM/dd/yyyy HH:mm" "GMT-4" "-outdateformat" "iso8601" "GMT" "-convertdate" "date_time" "-outdateformat" "yyyy-MM-dd HH:mm Z" "UTC" "-indateformat" "iso8601" "GMT" "-extractdate" "date_time" "year" "-extractdate" "date_time" "hours_in_year" "-notcolumns" "date_time" "-lastcolumns" "0"  -print  $1
 }
@@ -10,10 +16,28 @@ sh ${SEESV}/seesv.sh "-delimiter" "?" "-skiplines" "1" "-set" "0" "0" "number" "
 
 
 
+#The tmp.csv will hold all of the processed source files
+#we want to skip the header that is written out by the merge call
 echo "Year,Hours in year,Temperature" > tmp.csv
 for arg in "$@"; do
     merge $arg| tail -n +2 >>tmp.csv
 done
+
+#Now, tmp.csv is of the form
+#year,hours_in_year, temperature
+#2018,4207,78.165
+#2018,4209,69.685
+#...
+
+#but we want to convert it to:
+#hours_in_year,2018,2019,2020,2021
+#3768,,,41.878,
+#...
+
+#the -makefields pivots up the year values and makes them new columns
+#then we sort by the hours_in_year
+#then we make a formatted field (.e.g, June 15)
+#then we add the RAMADDA CSV header, specifying the types of the columns
 sh ${SEESV}/seesv.sh \
    -makefields year temperature hours_in_year "" \
    -sortby hours_in_year up "" \
