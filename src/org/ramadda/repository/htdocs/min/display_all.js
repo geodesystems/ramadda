ac05dbea59b54a768492e4c5401b37fc33cd196b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-30 11:39:07 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Sat Mar 30 07:19:03 MDT 2024";
+var build_date="RAMADDA build date: Sat Mar 30 11:38:52 MDT 2024";
 
 /**
    Copyright (c) 2008-2023 Geode Systems LLC
@@ -23568,6 +23568,7 @@ function RamaddaImagesDisplay(displayManager, id, properties) {
 	    let columnMap = {};
 	    let class1= "display-images-image-outer display-images-block ";
 	    let class2 = "display-images-image-inner";
+	    let class3 = "display-images-image-wrapper";
 	    this.idToRecord = {};
 	    let baseStyle = "";
 	    if(!decorate) {
@@ -23609,10 +23610,10 @@ function RamaddaImagesDisplay(displayManager, id, properties) {
 		tooltipFields.forEach(l=>{tt += "\n" + l.getLabel()+": " + row[l.getIndex()]});
 		tt = tt.trim();
 		let style = baseStyle;
-		let imgAttrs = [STYLE,imageStyle,"alt",galleryLabel,ID,base+"image" + rowIdx,"loading","lazy"];
+		let imgAttrs = [ATTR_STYLE,imageStyle,"alt",galleryLabel,ATTR_ID,base+"image" + rowIdx,"loading","lazy"];
 		if(width) imgAttrs.push(WIDTH,width);
 		else if(height) imgAttrs.push(HEIGHT,height);		
-		let img = image==""?SPACE1:HU.image(image,imgAttrs);
+		let img = image==""?SPACE1:HU.div([ATTR_CLASS,class3],HU.image(image,imgAttrs));
 		let topLbl = (topLabel!=null?HU.div([CLASS,"ramadda-clickable display-images-toplabel"], topLabel):"");
 		let lbl = HU.div([CLASS,"ramadda-clickable display-images-label"], label.trim());
 		if(urlField) {
@@ -32795,6 +32796,7 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
 	{p:'startDateLabel'},
 	{p:'createDateLabel'},	
 	{p:'areaLabel'},
+	{p:'orderByTypes',d:'relevant,name,createdate,date,size'},
 	{p:'doWorkbench',d:false,ex:'true', tt:'Show the new, charts, etc links'},
 	];
 
@@ -32859,13 +32861,30 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
 	    }
 	    if(this.getShowOrderBy()) {
 		let settings = this.getSearchSettings();
-		let byList = [["Relevant","relevant"], ["A-Z","name_ascending"],["Z-A","name_descending"],
-			  ["Create date - newest first","createdate_descending"],
-			  ["Create date - oldest first","createdate_ascending"],
-			  ["From date - youngest first","fromdate_descending"],			  			  
-			  ["From date - oldest first","fromdate_ascending"],
-			  ["Size - largest first","size_descending"],
-			  ["Size - smallest first","size_ascending"]];			  
+		let byList = [];
+		let getLabel=(type,suffix,dflt)=>{
+		    let key ='orderByLabel_'+ type+(suffix?'_'+suffix:'');
+		    let label =this.getProperty(key);
+		    if(label) return label;
+		    return  dflt ?? Utils.camelCase(type);
+		}
+
+		Utils.split(this.getOrderByTypes(),',',true,true).forEach(type=>{
+		    if(type=='relevant')
+			byList.push([getLabel(type,null),type]);
+		    else if(type=='name')
+			byList.push([getLabel(type,'ascending',"Name A-Z"), type+'_ascending'],
+				    [getLabel(type,'descending',"Name Z-A"),type+'_descending']);
+		    else if(type=='createdate')
+			byList.push(["Record create date - newest first","createdate_descending"],
+				    ["Record create date - oldest first","createdate_ascending"]);
+		    else if(type=='date')
+			byList.push([getLabel(type,'descending',"From date - youngest first"),"fromdate_descending"],			  			  
+				    [getLabel(type,'ascending',"From date - oldest first"),"fromdate_ascending"]);
+		    else if(type=='size')
+			byList.push(["Size - largest first","size_descending"],
+				    ["Size - smallest first","size_ascending"]);
+		});
 		let options = "";
 		byList.forEach(tuple=>{
 		    let label = tuple[0];
@@ -34270,11 +34289,15 @@ function RamaddaEntrylistDisplay(displayManager, id, properties, theType) {
 		contents.push(HU.div([ATTR_CLASS,'display-entrylist-results'],c));
 	    }
 
+	    let makeExpandable= (html) =>{
+		html =HU.div([ATTR_STYLE,HU.css("max-height","100vh",'background','#fff','overflow-y','auto')],html);
+		return HU.div([ATTR_CLASS,'ramadda-expandable-wrapper',ATTR_STYLE,HU.css("position","relative")],html);
+	    }
 
 	    this.getDisplayTypes("list").split(",").forEach(type=>{
 		if(type=="list") {
 		    titles.push("List");
-		    addContents(this.getEntriesTree(entries));
+		    addContents(makeExpandable(this.getEntriesTree(entries)));
 		} else if(type=="images") {
 		    let defaultImage = this.getDefaultImage();
 		    let imageEntries = entries.filter(entry=>{
@@ -34284,13 +34307,9 @@ function RamaddaEntrylistDisplay(displayManager, id, properties, theType) {
 		    if(imageEntries.length>0) {
 			titles.push("Images");
 			let id = HU.getUniqueId(type +"_");
-			let outerId = HU.getUniqueId(type +"_");			
-			this.imagesId=outerId;
 			this.myDisplays.push({id:id,type:type});
 			let images =HU.div([ATTR_ID,id,ATTR_CLASS,'ramadda-expandable display-entrylist-images',ATTR_STYLE,HU.css("width","100%")]);
-			images =HU.div([ATTR_STYLE,HU.css("max-height","100vh",'background','#fff','overflow-y','auto')],images);
-			let outer = HU.div([ATTR_ID,outerId,ATTR_STYLE,HU.css("position","relative")],images); 
-			addContents(outer);
+			addContents(makeExpandable(images));
 		    }
 		} else if(type=="timeline") {
 		    titles.push("Timeline");
@@ -34477,9 +34496,10 @@ function RamaddaEntrylistDisplay(displayManager, id, properties, theType) {
 
 
 
-	    if(this.imagesId) {
-		HU.makeExpandable('#'+this.imagesId, false,{right:'20px'});
-	    }
+
+	    this.jq(ID_ENTRIES).find('.ramadda-expandable-wrapper').each(function() {
+		HU.makeExpandable($(this), false,{right:'5px',top:'0px'});
+	    });
 
 
 	    if(this.mapId && this.areaEntries && this.areaEntries.length>0) {
