5f439d62d9c792eddfadb113c784684d231a88ba
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-30 05:59:20 -0600

    more work on the missing person search display

--- src/org/ramadda/projects/missing/MissingPersonTypeHandler.java
+++ src/org/ramadda/projects/missing/MissingPersonTypeHandler.java
@@ -72,49 +72,53 @@ public class MissingPersonTypeHandler extends ExtensibleGroupTypeHandler {
 	boolean forSearch = request.get("forsearch",false);
 	StringBuilder sb = new StringBuilder();
 	linkCSS(request, sb, getRepository().getHtdocsUrl("/missing/missing.css"));
-	String url = request.entryUrl(getRepository().URL_ENTRY_SHOW, entry);
-	String nickname=(String)entry.getValue("nickname");
-	String status=(String)entry.getValue("status","");	
-	sb.append("<div class=missing-header>");
-	sb.append("<div class=missing-header1>");
-	sb.append(HU.href(url));
-	sb.append("Missing Person/#" + entry.getValue("case_number","")+"<br>");
-	sb.append(entry.getName().trim());
-	if(stringDefined(nickname)) sb.append(" - \"" + nickname.trim()+"\"");
-	sb.append(", ");
-	if(forSearch) {
-	    findColumn("status").formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),
-					     false);
-	    sb.append(", ");
-	}
-	//	String sex=(String)entry.getValue("biological_sex");
-	Column sexColumn = findColumn("biological_sex");
-        sexColumn.formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),
-                           false);
-	//	if(sex==null) sex = "unknown";
-	//	sb.append(sex.equals("m")?"Male":sex.equals("f")?"Female":sex);
-	sb.append(", ");
-	Column ethnicityColumn = findColumn("race_ethnicity");
-        ethnicityColumn.formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),
-                           false);
-
-	//	sb.append(entry.getValue("race_ethnicity"));
-	sb.append(HU.close("a"));
-	sb.append("</div>");
 	int years;
 	SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
 	Date birthDate = DateHandler.checkDate(new Date(entry.getStartDate()));
 	Date missingDate = DateHandler.checkDate((Date) entry.getValue("date_missing"));
 	Date foundDate = DateHandler.checkDate((Date) entry.getValue("date_found"));	
+	String status=(String)entry.getValue("status","");	
 	String clazz="missing-block missing-status-"+ status; 
-	StringBuilder blocks = new StringBuilder();
-	blocks.append("<div style='margin-top:5px;border-top:var(--basic-border);'>");
+	String image="";
 	if(Utils.getProperty(props,"includeImage",false)) {
 	    List<String> urls =    getMetadataManager().getThumbnailUrls(request, entry, null);
 	    if(urls!=null && urls.size()>0) {
-		HU.div(blocks,HU.image(urls.get(0),HU.attrs("width","100px")),HU.attrs("class","missing-block"));
+		image=HU.image(urls.get(0),HU.attrs("width","100px"));
 	    }
 	}
+
+
+	if(forSearch) {
+	    getHeaderLine(request, entry, sb,forSearch);
+	    sb.append("\n");
+	    HU.open(sb,"table",HU.cssClass("formtable"));
+	    sb.append("\n");
+	    HU.formEntry(sb,msgLabel("Missing From"),
+			 colValue(request,entry,"missing_city")+
+			 " "+
+			 colValue(request,entry,"missing_state"));
+	    if(missingDate!=null) {
+		HU.formEntry(sb,msgLabel("Date Missing"),colValue(request,entry,"date_missing"));
+	    }
+	    sb.append("</table>\n");	    
+	    return HU.div(HU.leftRight(sb.toString(),image),HU.clazz("missing-header"));
+	}
+
+
+	sb.append("\n<div class=missing-header>\n");
+	getHeaderLine(request, entry, sb,forSearch);
+
+
+
+
+
+	StringBuilder blocks = new StringBuilder();
+	blocks.append("<div style='margin-top:5px;border-top:var(--basic-border);'>");
+
+	if(image.length()>0)
+	    HU.div(blocks,image,HU.attrs("class","missing-block"));
+	
+
 
 	makeBlock(blocks,clazz,"Status",Utils.applyCase(Utils.CASE_PROPER,status));
 	if(missingDate!=null) {
@@ -122,12 +126,9 @@ public class MissingPersonTypeHandler extends ExtensibleGroupTypeHandler {
 	    if(birthDate!=null) {
 		years = DateHandler.getYearsBetween(birthDate,missingDate);
 		label+="<br>" +years+" years old";
-	    //		makeBlock(blocks,clazz,"Age when missing", 
 	    }
 	    makeBlock(blocks,clazz,"Date last seen", label);
 	    if(birthDate!=null) {
-		//		int years = DateHandler.getYearsBetween(birthDate,missingDate);
-		//		makeBlock(sb,clazz,"Age when missing", years+" years");
 		if(status.equals("missing")) {
 		    years = DateHandler.getYearsBetween(birthDate,new Date());
 		    makeBlock(blocks,clazz,"Current age",years+" years old");
@@ -146,16 +147,20 @@ public class MissingPersonTypeHandler extends ExtensibleGroupTypeHandler {
 			years = DateHandler.getYearsBetween(birthDate,foundDate);
 			makeBlock(blocks,clazz,label1,
 				  sdf.format(foundDate)+"<br>"+years+" years old");
-
-			//			makeBlock(blocks,clazz,label2,years+" years old");
 		    }
 		}
 	    }
 	}
-	blocks.append("</div></div>");
+	blocks.append("</div>");
 	if(forSearch) {
-	    sb.append(HU.makeShowHideBlock("Details",blocks.toString(),false));
+	    String line =  HU.leftRight(sb.toString(),image);
+	    line+="\n</div>";
+	    System.err.println(line);
+	    System.exit(0);
+	    return line;
+	    //	    sb.append(HU.makeShowHideBlock("Details",blocks.toString(),false));
 	} else {
+	    blocks.append("</div>");
 	    sb.append(blocks);
 	}
 
@@ -164,6 +169,33 @@ public class MissingPersonTypeHandler extends ExtensibleGroupTypeHandler {
 
 	return sb.toString();
     }
+
+    private void getHeaderLine(Request request, Entry entry, StringBuilder sb,boolean forSearch) throws Exception {
+	String url = request.entryUrl(getRepository().URL_ENTRY_SHOW, entry);
+	String nickname=(String)entry.getValue("nickname");
+	String status=(String)entry.getValue("status","");	
+	sb.append("<div class=missing-header1>\n");
+	sb.append(HU.href(url));
+	sb.append("Missing Person/#" + entry.getValue("case_number","")+"<br>");
+	sb.append(entry.getName().trim());
+	if(stringDefined(nickname)) sb.append(" - \"" + nickname.trim()+"\"");
+	sb.append(", ");
+	if(forSearch) {
+	    findColumn("status").formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),
+					     false);
+	    sb.append(", ");
+	}
+	Column sexColumn = findColumn("biological_sex");
+        sexColumn.formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),
+                           false);
+	sb.append(", ");
+	Column ethnicityColumn = findColumn("race_ethnicity");
+        ethnicityColumn.formatValue(request, entry, sb, Column.OUTPUT_HTML, entry.getValues(),false);
+
+	sb.append(HU.close("a"));
+	HU.close(sb,"div");
+	}
+
 
     public Result processEntryAction(Request request, Entry entry)
             throws Exception {
@@ -177,6 +209,10 @@ public class MissingPersonTypeHandler extends ExtensibleGroupTypeHandler {
 	return new Result("",sb);
     }
     
+    private String colValue(Request request, Entry entry, String column) throws Exception {
+	return findColumn(column).formatValue(request, entry, entry.getValues());
+    }
+
     public void makeFlyer(Request request, final Entry entry,StringBuilder sb)
 	throws Exception {
 	final Object[] values = entry.getValues();
