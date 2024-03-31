3ae82ad1c9b7e9d578a9c2a38205ef480129eae0
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-03-30 19:34:17 -0600

    call bash. hide and ignore any error with the pipefail. check if MYDIR is set correctly and use SEESV if not set. etc

--- src/org/ramadda/util/seesv/seesv.sh
+++ src/org/ramadda/util/seesv/seesv.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
-mydir=`dirname $0`
+#!/bin/bash
+MYDIR=`dirname $0`
 
 set -e
-set -o pipefail
+set -o pipefail 2>/dev/null || : 
+
+
+##In case this script was sources from another with . seesv.sh
+##check if 
+if [ ! -e "$MYDIR/lib/seesv.jar" ]; then
+    MYDIR="${SEESV}"
+fi
+
+if [ ! -e "$MYDIR/lib/seesv.jar" ]; then
+    printf "Error: cannot find $MYDIR/lib/seesv.jar\nIs the SEESV environment variable set?\n" >&2
+    false
+fi
+
 
 if [ -z "$JAVA" ]; then
     JAVA=java
 fi
 
-${JAVA} -Djava.awt.headless=true -jar  ${mydir}/lib/seesv.jar "$@"
+${JAVA} -Djava.awt.headless=true -jar  ${MYDIR}/lib/seesv.jar "$@"
 
