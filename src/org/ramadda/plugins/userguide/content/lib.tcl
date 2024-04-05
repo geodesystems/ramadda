457223b2f0643b7b3d3f6512ea842e4670f99cda
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-04-05 09:33:35 -0600

    add pageconfig.html

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {17.139.0}
+gen::defineMacro {<%ramadda.version%>} {18.2.0}
 
 
 namespace eval wiki {}
