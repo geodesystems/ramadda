3aaab1152f29cd981cb4f893267d115cd26af82d
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-04-11 16:02:11 -0600

    add olc.landknowledge.org

--- bin/makegeodecert.sh
+++ bin/makegeodecert.sh
@@ -2,6 +2,6 @@
 
 
 BIN_DIR="$(cd "$(dirname "$0")" && pwd)"
-OTHER_DOMAINS=www.geodesystems.com,boulderdata.org,www.boulderdata.org,communidata.org,www.communidata.org,10000cities.org,www.10000cities.org,ramadda.org,www.ramadda.org,asdi.ramadda.org,sdn.ramadda.org,landknowledge.ramadda.org,ditchproject.org,www.ditchproject.org,landknowledge.org,www.landknowledge.org
+OTHER_DOMAINS=www.geodesystems.com,boulderdata.org,www.boulderdata.org,communidata.org,www.communidata.org,10000cities.org,www.10000cities.org,ramadda.org,www.ramadda.org,asdi.ramadda.org,sdn.ramadda.org,landknowledge.ramadda.org,ditchproject.org,www.ditchproject.org,landknowledge.org,olc.landknowledge.org,www.landknowledge.org
 
 sh "${BIN_DIR}/letsencrypt.sh" -renew -home /mnt/ramadda/repository  -domain geodesystems.com -other "$OTHER_DOMAINS" "$@"
