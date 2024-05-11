896bd08e1bc37358f6a037be42ec518784efc121
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-05-10 23:02:49 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Fri May 10 19:48:02 MDT 2024";
+var build_date="RAMADDA build date: Fri May 10 23:02:33 MDT 2024";
 
 /**
    Copyright (c) 2008-2023 Geode Systems LLC
@@ -12969,7 +12969,7 @@ function DisplayGroup(argDisplayManager, argId, argProperties, type) {
                 } else {
                     let percent = Math.round((100 / this.columns))+"%";
                     let i = 0;
-		    html+=HU.open(TABLE,[WIDTH,'100%']);
+		    html+=HU.open(TABLE,[ATTR_WIDTH,'100%']);
 		    let colCnt = 100;
                     for (let i =0;i < displaysToLayout.length; i++) {
                         colCnt++;
@@ -12981,7 +12981,8 @@ function DisplayGroup(argDisplayManager, argId, argProperties, type) {
 			    html+="\n";
                             colCnt = 0; 
                         }
-                        html += HU.td(["width",percent], displaysToLayout[i].layoutDiv);
+			//Add in the max-width so the width of the td doesn't exceed the percent value
+                        html += HU.td([ATTR_STYLE,"max-width:300px;", ATTR_WIDTH,percent], displaysToLayout[i].layoutDiv);
 			html+="\n";
 		    }
                     if (i > 0) {
