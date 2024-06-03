7ef522277015e9ad70a6ba04ee8dc924e93cf2b3
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-06-02 23:16:18 -0600

    implementation of new types

---
+++ src/org/ramadda/geodata/geo/BoreholeLogTypeHandler.java
@@ -0,0 +1,64 @@
+/**
+Copyright (c) 2008-2024 Geode Systems LLC
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.ramadda.geodata.geo;
+
+import org.ramadda.data.point.text.*;
+import org.ramadda.data.record.*;
+import org.ramadda.data.services.PointTypeHandler;
+import org.ramadda.repository.*;
+import org.ramadda.repository.metadata.*;
+import org.ramadda.repository.type.*;
+import org.ramadda.util.WikiUtil;
+import org.ramadda.util.IO;
+import org.ramadda.util.Utils;
+import ucar.unidata.util.StringUtil;
+
+import org.w3c.dom.*;
+import org.json.*;
+import java.net.URL;
+import java.io.*;
+import org.json.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.HashSet;
+import java.util.List;
+
+
+public class BoreholeLogTypeHandler extends PointTypeHandler {
+    private JSONArray holes;
+    public BoreholeLogTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+
+    @Override
+    public void initializeNewEntry(Request request, Entry entry,NewType newType)
+            throws Exception {
+	super.initializeNewEntry(request, entry,newType);
+	if(!isNew(newType)) return;
+	String holeId = (String) entry.getValue("hole");
+	if(!stringDefined(holeId)) return;
+	if(holes==null) {
+	    holes = new JSONArray(getStorageManager().readUncheckedSystemResource("/org/ramadda/geodata/geo/resources/holes.json"));
+	}
+	holeId=holeId.trim();
+	for(int i=0;i<holes.length();i++) {
+	    JSONObject hole=holes.getJSONObject(i);
+	    String id = hole.getString("id").trim();
+	    if(id.equals(holeId)) {
+		entry.setLatitude(hole.getDouble("latitude"));
+		entry.setLongitude(hole.getDouble("longitude"));		
+	    }
+	}
+
+    }
+
+
+}
