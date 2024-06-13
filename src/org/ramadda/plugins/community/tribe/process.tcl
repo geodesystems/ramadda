3b86bf51733bfe7c597e0d087c80a8f650d2362c
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-06-13 07:54:07 -0700

    snapshot

--- src/org/ramadda/plugins/community/tribe/process.tcl
+++ src/org/ramadda/plugins/community/tribe/process.tcl
@@ -1,27 +1,25 @@
- package require csv
+package require csv
+package require http
 
 
-##@(https://en.wikipedia.org/wiki/Benton_County,_Arkansas imageWidth=100 addHeader=false ignoreError=true)
-
-proc biaIndex {key} {
-    for {set i 0} {$i<[llength $::key]} {incr i} {
-	if {[lindex $::key $i] == $key} {
+proc index {base key} {
+    set data "::${base}key"
+    for {set i 0} {$i<[llength [set $data]]} {incr i} {
+	if {[lindex [set $data] $i] == $key} {
 	    return $i
 	}
     }
     return -1
-
 }
-proc bia {line key} {
-    set idx [biaIndex $key]
+proc fetch {base line key} {
+    set idx [index $base $key]
     lindex $line $idx
 }
 
-proc biaFind {name} {
-    foreach line $::bia {
-	set tribe [bia $line tribe]
+proc find {base key name} {
+    foreach line [set "::$base"] {
+	set tribe [fetch $base $line $key]
 	if {[regexp -nocase $name $tribe]} {
-#	    puts stderr "GOT: $name $tribe"
 	    return $line
 	}
     }
@@ -29,16 +27,12 @@ proc biaFind {name} {
     return {}
 }
 
-set fp [open template.xml r]
-set ::template [read $fp]
-close $fp
-
-proc parseCsvFile {filename} {
+proc parseCsvFile {base filename} {
     set fp [open $filename r]
     set csvData [read $fp]
     close $fp
 
-    set ::bia {}
+    set "::$base" {}
     set cnt 0
     foreach row [split [string trim $csvData] \n] {
 	set line  [csv::split $row]
@@ -48,17 +42,29 @@ proc parseCsvFile {filename} {
 		set key [string tolower $key]
 		incr idx
 	    }
-	    set ::key $line
+	    set "::${base}key" $line
 	} else {
-	    lappend ::bia $line
+	    lappend "::$base" $line
 	}
 	incr cnt
     }
-    set ::bia
+    set "::$base"
 }
 
+
+
+set fp [open template.xml r]
+set ::template [read $fp]
+close $fp
+set fp [open toptemplate.xml r]
+set ::toptemplate [read $fp]
+close $fp
+
+
 # Main script execution
-set parsedData [parseCsvFile BIA_Tribal_Leadership_Directory.csv]
+parseCsvFile bia BIA_Tribal_Leadership_Directory.csv
+parseCsvFile terroritories territories.csv
+
 set ::list {kootenai colville jamestown elwha samish ronde siletz yakama gamble warm northwestern kasaan summit {duck valley} siuslaw umatilla}
 
 
@@ -107,11 +113,65 @@ proc attr {n v} {
     return " $n=\"$v\" "
 }
 
-proc tribe {state tribe wiki lat lon} {
-    set key [cleanTribe $tribe]
-    set biaInfo [biaFind $key]
+proc findSlug {tribe} {
+    if {[regexp -nocase {Cow Creek Band} $tribe]} {
+	return cow-creek-umpqua
+    }
+    if {[regexp -nocase {sauk} $tribe]} {
+	return sauk-suiattle
+    }
+    if {[regexp -nocase {burns paiute} $tribe]} {
+	return northern-paiute
+    }
+    if {[regexp -nocase {duck valley} $tribe]} {
+	return northern-paiute
+    }
+    if {[regexp -nocase {summit lake} $tribe]} {
+	return northern-paiute
+    }            
+    if {[regexp -nocase {Northwestern Band of Shoshone} $tribe]} {
+	return newe-western-shoshone
+    }        
 
-#    puts "$tribe $lat $lon"
+    if {[regexp -nocase {Chippewa Cree} $tribe]} {
+	return cree
+    }        
+    
+
+
+    if {[regexp -nocase klallam $tribe]} { set tribe klallam}        
+    set key [cleanTribe $tribe]
+    regsub -all upper $key {} key
+
+
+    if {[regexp tolowa $key]} { set key tolowa}
+    if {[regexp squaxin $key]} { set key squaxin}
+    if {[regexp haida $key]} { set key haida}
+    if {[regexp blackfeet $key]} { set key blackfoot}
+    set key [string trim $key]
+    if {[string length $key]<=3} {
+	puts stderr "NO $tribe key: $key"
+	return ""
+    }
+    set cnt 0
+    foreach line $::terroritories {
+	set name [lindex  $line 2]
+	if {[regexp -nocase $key $name]} {
+	    set slug [lindex  $line 0]
+	    return $slug
+	}
+    }
+    puts stderr "NO $tribe key: $key"
+
+    return ""
+}
+
+set ::tribecnt 0
+proc tribe {state tribe wiki lat lon} {
+    incr ::tribecnt
+#    if {$::tribecnt>10} return
+    set key [cleanTribe $tribe]
+    set biaInfo [find  bia  tribe $key]
     set file [string tolower $tribe]
     regsub -all { } $file _ file
     regsub -all {[\(\)]+} $file {} file    
@@ -125,15 +185,23 @@ proc tribe {state tribe wiki lat lon} {
     set metadata ""
     set attrs ""    
     if {[llength $biaInfo]>0} {
-	set lat [bia $biaInfo Y]
-	set lon [bia $biaInfo X]	
-	set url [string trim [bia $biaInfo website]]
+	set lat [fetch bia $biaInfo Y]
+	set lon [fetch bia $biaInfo X]	
+	set url [string trim [fetch bia $biaInfo website]]
 	if {$url!=""} {
 	    append metadata "<metadata [attr type content.url]>"
-	    append metadata "<attr [attr fileid $file] [attr index 1] [attr encoded false]>[cdata $url]</attr>"
-	    append metadata "<attr [attr fileid $file] [attr index 2] [attr encoded false]>$tribe Website</attr>"	    
+	    append metadata "<attr  [attr index 1] [attr encoded false]>[cdata $url]</attr>"
+	    append metadata "<attr  [attr index 2] [attr encoded false]>$tribe Website</attr>"	    
 	    append metadata "</metadata>"
 	}
+	append metadata "<metadata [attr type content.address]>"
+	set idx 0
+	append metadata "<attr  [attr index [incr idx]] [attr encoded false]>[cdata [fetch bia $biaInfo mailingaddress]]</attr>"
+	append metadata "<attr  [attr index [incr idx]] [attr encoded false]>[cdata [fetch bia $biaInfo mailingaddresscity]]</attr>"
+	append metadata "<attr  [attr index [incr idx]] [attr encoded false]>[cdata [fetch bia $biaInfo mailingaddressstate]]</attr>"
+	append metadata "<attr  [attr index [incr idx]] [attr encoded false]>[cdata [fetch bia $biaInfo mailingaddresszipcode]]</attr>"
+	append metadata "<attr  [attr index [incr idx]] [attr encoded false]>[cdata {United States}]</attr>"		
+	append metadata "</metadata>"
     }
 
 
@@ -142,17 +210,23 @@ proc tribe {state tribe wiki lat lon} {
     set west ""
     set south ""
     set east ""
+#    geojson $tribe $file
     if {[file exists $file]} {
 	set bounds [exec java org.ramadda.util.geo.GeoJson -bounds $file]
 	regexp {north:(.*) +west:(.*) +south:(.*) +east:(.*)} $bounds match north west south east
-#	set lat [expr $south+($north-$south)/2]
-#	set lon [expr $west+($east-$west)/2]
+	if {$lat=="N/A"} {
+	    set lat [expr $south+($north-$south)/2]
+	    set lon [expr $west+($east-$west)/2]
+	}
 	append metadata "<metadata [attr type map_displaymap_file] [attr inherited true]>\n"
 	append metadata "<attr [attr fileid $file] [attr index 1] [attr encoded false]>[cdata $file]</attr>\n"
+	append metadata "<attr  [attr index 4] [attr encoded false]>[cdata black]</attr>\n"	
 	append metadata "</metadata>\n"
 	append attrs "[attr north $north] [attr west $west] [attr south  $south] [attr east $east]"
     } else {
-	append attrs " [attr latitude $lat] [attr longitude $lon] "
+	if {$lat!="N/A"} {
+	    append attrs " [attr latitude $lat] [attr longitude $lon] "
+	}
     }
 
     puts $attrs
@@ -162,6 +236,12 @@ proc tribe {state tribe wiki lat lon} {
     puts "<state>[cdata $state]</state>\n"
     puts "<metadata [attr type content.alias]><attr [attr index 1] [attr encoded false]><!\[CDATA\[tribe_$id\]\]></attr></metadata>"
     set children ""
+    set template $::toptemplate
+    regsub -all {%parent%} $template $id template
+    append children $template
+    append children "\n"
+
+
     set template $::template
     set dataid ${id}_data
     append children "<entry [attr type group] [attr name Data] [attr parent $id] [attr id ${dataid}]/>\n"
@@ -170,11 +250,37 @@ proc tribe {state tribe wiki lat lon} {
     regsub -all {%longitude%} $template $lon template
     regsub -all {%parent%} $template $dataid template            
     append children $template
-
     append children "<entry [attr type type_document_collection] [attr name Documents] [attr parent $id]/>\n"    
-    if {$lat!="N/A"} {
-#	set cy [expr $south+($north-$south)/2]
-#	set cx [expr $west+($east-$west)/2]
+    set slug [findSlug $tribe]
+    if {$slug==""} {
+#	puts stderr "tribe: $tribe"
+    }
+
+    if {$slug!=""} {
+	set tmap "territories_${id}.geojson"
+	if {![file exists $tmap]} {
+	    set url  "https://native-land.ca/api/index.php?maps=territories&name=$slug"
+	    puts stderr "fetching $tmap"
+	    catch {exec wget -O dummy.txt $url}
+	    set fp [open dummy.txt r]
+	    set contents [read $fp]
+	    close $fp
+	    set fp [open $tmap w]
+	    puts $fp "{\"type\":\"FeatureCollection\",\"features\":$contents }"
+	    close $fp
+	}
+	set bounds [exec java org.ramadda.util.geo.GeoJson -bounds $tmap]
+	regexp {north:(.*) +west:(.*) +south:(.*) +east:(.*)} $bounds match north west south east
+	set mapsid ${id}_maps
+	append children "<entry [attr type type_map_folder] [attr name Maps] [attr parent $id] [attr id ${mapsid}]/>\n"
+	append children "<entry [attr north $north] [attr west $west] [attr south $south] [attr east $east] [attr type geo_geojson] [attr name {Traditional Territories}] [attr filename $tmap] [attr file $tmap] [attr parent $mapsid]>\n"	
+	set mapdesc "+callout-info\nTradiditional territories of the $tribe. Map courtesy of \[https://native-land.ca/ Native Land Digital\]\n-callout\n"
+	append children "<description>[cdata $mapdesc]</description>\n"
+	append children "</entry>\n"
+    }
+
+
+    if {$lat !="N/A"} {
 	set cy $lat
 	set cx $lon
 	set d 1
@@ -183,23 +289,17 @@ proc tribe {state tribe wiki lat lon} {
 	append children "<description>[cdata {{{map}}}]</description>"
 	append children "<entry_ids [attr encoded false]><!\[CDATA\[search.type=type_awc_metar\nsearch.bbox=$bbox\nsearch\]\]></entry_ids>\n"
 	append children "</entry>\n"
-
+	
 	append children "<entry [attr type type_virtual] [attr name {USGS Stream Gauges}] [attr parent ${dataid}]>\n"
 	append children "<description>[cdata {{{map}}}]</description>"
 	append children "<entry_ids [attr encoded false]><!\[CDATA\[search.type=type_usgs_gauge\nsearch.bbox=$bbox\nsearch\]\]></entry_ids>\n"
 	append children "</entry>\n"
-
-
 	set d 2
 	set bbox "[expr $cy+$d],[expr $cx-$d],[expr $cy-$d],[expr $cx+$d]"
 	append children "<entry [attr type type_virtual] [attr name {NOAA Sea-Level Trends}] [attr parent ${dataid}]>\n"
 	append children "<description>[cdata {{{map}}}]</description>"
 	append children "<entry_ids [attr encoded false]><!\[CDATA\[search.type=type_noaa_tides_trend\nsearch.bbox=$bbox\nsearch\]\]></entry_ids>\n"
 	append children "</entry>\n"
-
-    }
-
-    if {$lat!="N/A"} {
 	set name "$tribe Current Conditions"
 	append children "<entry [attr type nwsfeed] [attr name $name] [attr id ${id}_nws] [attr parent ${dataid}] [attr latitude $lat] [attr longitude $lon]/>\n"
 	set name "$tribe Historic Weather"
@@ -210,13 +310,10 @@ proc tribe {state tribe wiki lat lon} {
 	append desc "{{nws.current [attr entry ${id}_nws] [attr addHeader false] [attr orientation vertical]}}\n";
 	append desc ":br\n"
 	append desc "{{nws.forecast [attr entry ${id}_nws] showHeader=false showDetails=false count=1000}}\n";
-
-	
     }
     puts  "<description><!\[CDATA\[$desc\]\]></description>\n"
     puts "</entry>"    
     puts $children
-#    geojson $tribe $file
 }
 
 source tribes.tcl
