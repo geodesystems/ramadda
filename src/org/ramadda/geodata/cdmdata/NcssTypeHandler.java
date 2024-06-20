2883a1cdaa5739fa20fc6375c784617e0e003b62
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-06-20 05:35:45 -0600

    cleanup

--- src/org/ramadda/geodata/cdmdata/NcssTypeHandler.java
+++ src/org/ramadda/geodata/cdmdata/NcssTypeHandler.java
@@ -1,5 +1,5 @@
 /**
-Copyright (c) 2008-2023 Geode Systems LLC
+Copyright (c) 2008-2024 Geode Systems LLC
 SPDX-License-Identifier: Apache-2.0
 */
 
@@ -24,17 +24,13 @@ import ucar.unidata.util.StringUtil;
 import java.io.*;
 
 import java.text.SimpleDateFormat;
-
 import java.util.ArrayList;
-
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Hashtable;
 import java.util.List;
 
 
-/**
- */
 @SuppressWarnings("unchecked")
 public class NcssTypeHandler extends PointTypeHandler {
 
@@ -42,51 +38,20 @@ public class NcssTypeHandler extends PointTypeHandler {
     private static final String DATE_TYPE_RELATIVE = "relative";
     private static final String DATE_TYPE_ABSOLUTE = "absolute";        
 
-    /** _more_ */
     private static int IDX = PointTypeHandler.IDX_LAST + 1;
-
     private static int IDX_DATE_TYPE = IDX++;
-
-    /** _more_ */
     private static int IDX_END_TIME_OFFSET = IDX++;
-
-
-    /** _more_ */
     private static int IDX_DEFAULT_FIELDS = IDX++;
-
-
-
-    /**
-     * _more_
-     */
+    
     public NcssTypeHandler() {
         super(null, "", "");
     }
-
-
-
-
-    /**
-     * _more_
-     *
-     * @param repository _more_
-     * @param node _more_
-     * @throws Exception _more_
-     */
+    
     public NcssTypeHandler(Repository repository, Element node)
             throws Exception {
         super(repository, node);
     }
-
-    /**
-     * _more_
-     *
-     * @param request _more_
-     * @param entry _more_
-     * @param wikiText _more_
-     *
-     * @return _more_
-     */
+    
     @Override
     public String preProcessWikiText(Request request, Entry entry,
                                      String wikiText) {
@@ -108,18 +73,7 @@ public class NcssTypeHandler extends PointTypeHandler {
         return wikiText;
     }
 
-
-    /**
-     * _more_
-     *
-     * @param url _more_
-     * @param loc _more_
-     * @param vars _more_
-     *
-     * @return _more_
-     *
-     * @throws Exception _more_
-     */
+    
     private String parseUrl(String url, double[] loc, List<String> vars)
             throws Exception {
         String   lat = null,
@@ -146,7 +100,6 @@ public class NcssTypeHandler extends PointTypeHandler {
             loc[1] = Double.parseDouble(lon);
         }
 
-
         String rest = url;
         while (true) {
             toks = Utils.findPatterns(rest, "var=([^&]+)&?(.*)");
@@ -157,9 +110,6 @@ public class NcssTypeHandler extends PointTypeHandler {
             rest = toks[1];
 
         }
-
-
-
 
         //time_start=2019-01-07T00%3A00%3A00Z&time_end=2019-02-22T06%3A00%3A00Z&vertCoord=&accept=csv";
 	//latitude=${latitude}&longitude=${longitude}&time_start=${date  format=yyyy-MM-dd}&time_end=${date date=now offset="${endTimeOffset}" format=yyyy-MM-dd}&vertCoord=&accept=csv
@@ -192,24 +142,13 @@ public class NcssTypeHandler extends PointTypeHandler {
         return url;
     }
 
-
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     @Override
     public boolean okToSetNewNameDefault() {
         return false;
     }
 
-    /**
-     * _more_
-     *
-     * @param path _more_
-     *
-     * @return _more_
-     */
+    
     @Override
     public String getDefaultEntryName(String path) {
         return "";
@@ -225,12 +164,10 @@ public class NcssTypeHandler extends PointTypeHandler {
     }
 
 
-
     @Override
     public void initializeNewEntry(Request request, Entry entry,  NewType newType)
             throws Exception {
 	if(!isNew(newType)) return;
-
 	if(!checkLatLon(request, entry)) {
 	    return;
 	}
@@ -246,7 +183,7 @@ public class NcssTypeHandler extends PointTypeHandler {
             }
         }
 
-        //http://thredds.ucar.edu/thredds/ncss/grib/NCEP/GFS/Global_onedeg/Best?var=Pressure_surface&var=Snow_depth_surface&var=Temperature_surface&var=Visibility_surface&var=Wind_speed_gust_surface&latitude=40&longitude=-105.2&time_start=2019-01-07T00%3A00%3A00Z&time_end=2019-02-22T06%3A00%3A00Z&vertCoord=&accept=csv
+        //e.g. - http://thredds.ucar.edu/thredds/ncss/grib/NCEP/GFS/Global_onedeg/Best?var=Pressure_surface&var=Snow_depth_surface&var=Temperature_surface&var=Visibility_surface&var=Wind_speed_gust_surface&latitude=40&longitude=-105.2&time_start=2019-01-07T00%3A00%3A00Z&time_end=2019-02-22T06%3A00%3A00Z&vertCoord=&accept=csv
 
         String                    sample        = Utils.readUrl(url);
         int                       index         = sample.indexOf("\n");
@@ -255,7 +192,6 @@ public class NcssTypeHandler extends PointTypeHandler {
         String                    vertCoordUnit = "m";
         if (index >= 0) {
             String            header = sample.substring(0, index);
-	    System.err.println("header:" + header);
             List<RecordField> fields = new CsvFile().doMakeFields(header);
             for (RecordField field : fields) {
                 if (field.getName().equals("vertCoord")) {
@@ -302,7 +238,6 @@ public class NcssTypeHandler extends PointTypeHandler {
 	    }
             String label   = Utils.makeLabel(origVar);
             label = CdmOutputHandler.getPropertyWithSuffix(".label",label,alias,origVar,alias.toLowerCase(),origVar.toLowerCase());
-	    System.err.println(origVar+" label:" + label);
 	    defaultFields.append(alias);
 	    defaultFields.append(",");
             if (unit != null) {
@@ -356,16 +291,7 @@ public class NcssTypeHandler extends PointTypeHandler {
 
     }
 
-    /**
-     * _more_
-     *
-     * @param entry _more_
-     * @param requestProperties _more_
-     *
-     * @return _more_
-     *
-     * @throws Exception _more_
-     */
+    
     @Override
     public IO.Path getPathForRecordEntry(Request request, Entry entry,
 					 Hashtable requestProperties)
@@ -390,37 +316,13 @@ public class NcssTypeHandler extends PointTypeHandler {
         url = Utils.normalizeTemplateUrl(url);
 	return new IO.Path(url);
     }
-
-    /**
-     * _more_
-     *
-     * @param request _more_
-     * @param entry _more_
-     *
-     * @return _more_
-     *
-     * @throws Exception _more_
-     */
+    
     @Override
     public String getPathForEntry(Request request, Entry entry, boolean forRead)
             throws Exception {
         return getPathForRecordEntry(request, entry, request.getDefinedProperties()).getPath();
     }
-
-
-    /**
-     * _more_
-     *
-     *
-     * @param request _more_
-     * @param entry _more_
-     * @param pointProps _more_
-     * @param requestProperties _more_
-     *
-     * @return _more_
-     *
-     * @throws Exception _more_
-     */
+    
     @Override
     public RecordFile doMakeRecordFile(Request request, Entry entry,
                                        Hashtable pointProps,
@@ -429,15 +331,7 @@ public class NcssTypeHandler extends PointTypeHandler {
         Hashtable props = getRecordProperties(entry);
         return new CsvFile(getPathForRecordEntry(request, entry, requestProperties), props);
     }
-
-
-    /**
-     * _more_
-     *
-     * @param args _more_
-     *
-     * @throws Exception _more_
-     */
+    
     public static void main(String[] args) throws Exception {
         String url =
             "http://thredds.ucar.edu/thredds/ncss/grib/NCEP/GFS/Global_onedeg/Best?var=Pressure_surface&var=Snow_depth_surface&var=Temperature_surface&var=Visibility_surface&var=Wind_speed_gust_surface&latitude=40&longitude=-105.2&time_start=2019-01-07T00%3A00%3A00Z&time_end=2019-02-22T06%3A00%3A00Z&vertCoord=&accept=csv";
