38dad7d521ce0838af48855140465edcf915af17
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-06-20 21:47:26 -0600

    update

--- src/org/ramadda/projects/kbocc/flash.tcl
+++ src/org/ramadda/projects/kbocc/flash.tcl
@@ -25,12 +25,14 @@ proc download {days hours} {
 set days {20240601  20240602 20240603 20240604 20240605 20240606 20240607 20240608 20240609 20240610 20240611 20240612 20240613 20240614 20240615 20240616 20240617 20240618 20240619 20240620}
 set hours {000000 120000}
 
-set hours {}
+set hourlyHours {}
 set days {20240619 20240620}
 for {set hour 0} {$hour<24} {incr hour} {
     set h $hour
     if {$h<=9} {set  h "0$h"}
-    lappend  hours "${h}0000"
+    lappend  hourlyHours "${h}0000"
 }
 
-download $days $hours
+download $days $hourlyHours
+
+#download $days $hours
