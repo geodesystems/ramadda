d88c0e945a0a094c455e1abb1f9bc22a418232d5
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-06-25 04:51:43 -0600

    update

--- src/org/ramadda/repository/htdocs/min/display_all.js
+++ src/org/ramadda/repository/htdocs/min/display_all.js
@@ -1,4 +1,4 @@
-var build_date="RAMADDA build date: Mon Jun 24 18:08:31 MDT 2024";
+var build_date="RAMADDA build date: Tue Jun 25 04:51:23 MDT 2024";
 
 /**
    Copyright (c) 2008-2023 Geode Systems LLC
@@ -33610,7 +33610,7 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
         {p:'orderBy',ex: 'name_ascending|name_descending|fromdate_ascending|fromdate_descending|todate_|createdate_|size_'},
         {p:'orientation',ex:'horizontal|vertical',d:'horizontal'},
 	{p:'formHeight',d:'1000px'},
-        {p:'entriesHeight',d:'70vh'},	
+        {p:'entriesHeight',ex:'70vh'},	
         {p:'showEntries',d: true},
         {p:'showFooter',d: true},	
         {p:'showType',d: true},
@@ -33879,7 +33879,8 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
                 innerHeight = this.getEntriesHeight();
             }
             if (innerHeight != null) {
-//                style = "margin: 0px; padding: 0px; max-height:" + HU.getDimension(innerHeight) + "; overflow-y: auto;";
+                style = "margin: 0px; padding: 0px; max-height:" + HU.getDimension(innerHeight) + "; overflow-y: auto;";
+                style = HU.css('max-height', HU.getDimension(innerHeight),'overflow-y','auto');
             }
 	    style+= entriesStyle;
             entriesDivAttrs.push(ATTR_STYLE);
