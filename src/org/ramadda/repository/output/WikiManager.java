a68db2e52f98a1303edddf81099659caf0060590
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-07-05 21:22:46 -0600

    change data.status to data_status

--- src/org/ramadda/repository/output/WikiManager.java
+++ src/org/ramadda/repository/output/WikiManager.java
@@ -3139,7 +3139,7 @@ public class WikiManager extends RepositoryManager
 	    getAccessManager().formatDataPolicies(request, sb, dataPolicies,includeCollection, includePermissions);
 	    return sb.toString();
         } else if (theTag.equals(WIKI_TAG_DATA_STATUS)) {
-	    String text= "{{properties  message=\"\"  metadata.types=\"data.status\" inherited=\"true\" includeTitle=\"false\" center=\"true\" addLink=\"false\" stripe=\"false\"}}";             
+	    String text= "{{properties  message=\"\"  metadata.types=\"data_status\" inherited=\"true\" includeTitle=\"false\" center=\"true\" addLink=\"false\" stripe=\"false\"}}";             
 	    return   wikifyEntry(request, entry, text, false);
         } else if (theTag.equals(WIKI_TAG_PROPERTIES)) {
 
