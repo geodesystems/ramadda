110653ba1b4bee36aa0f71002277f722f456fdd5
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-09-19 16:58:37 -0600

    add documentation on how to build tesseract

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {19.153.0}
+gen::defineMacro {<%ramadda.version%>} {19.156.0}
 
 
 namespace eval wiki {}
