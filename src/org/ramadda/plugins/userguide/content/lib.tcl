9f019d201751c79b14d437210c23f6e4847ed1f6
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-09-23 21:40:31 -0600

    document gdal install with pixi

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {19.156.0}
+gen::defineMacro {<%ramadda.version%>} {19.160.0}
 
 
 namespace eval wiki {}
