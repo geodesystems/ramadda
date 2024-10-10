14edc14a66110a3dcd7fdc2021362949d239c35a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-10-10 05:28:50 -0600

    remove initTypeMenu

--- src/org/ramadda/repository/htdocs/harvester.js
+++ src/org/ramadda/repository/htdocs/harvester.js
@@ -1,22 +1,2 @@
-/**
-   Copyright (c) 2008-2023 Geode Systems LLC
-   SPDX-License-Identifier: Apache-2.0
-*/
 
 
-HtmlUtils.initTypeMenu = function InitTypeMenu(selectId, textAreaId) {
-
-    jqid(selectId).change(function() {
-	let v = $(this).val();
-	if(!Utils.stringDefined(v)) {
-	    return
-	}
-	$(this).val("");
-	let  t = jqid(textAreaId).val()??"";
-	t = t.trim();
-	if(t!="") t = t+"\n";
-	t+=v+":";
-	jqid(textAreaId).val(t);
-    });
-
-}
