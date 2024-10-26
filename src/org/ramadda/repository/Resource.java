ab8bd2caa75e025c2efe4d4433440522873e86e8
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-10-25 21:34:48 -0600

    clean up

--- src/org/ramadda/repository/Resource.java
+++ src/org/ramadda/repository/Resource.java
@@ -1,5 +1,5 @@
 /**
-Copyright (c) 2008-2023 Geode Systems LLC
+Copyright (c) 2008-2024 Geode Systems LLC
 SPDX-License-Identifier: Apache-2.0
 */
 
@@ -16,59 +16,26 @@ import java.net.URL;
  * @author RAMADDA Development Team
  */
 public class Resource {
-
-    /** _more_ */
     public static final String TYPE_FILE = "file";
-
     public static final String TYPE_S3 = "s3";
-
-    /** _more_ */
     public static final String TYPE_LOCAL_DIRECTORY = "localdirectory";
-
-    /** _more_ */
     public static final String TYPE_LOCAL_FILE = "localfile";
-
-    /** _more_ */
     public static final String TYPE_STOREDFILE = "storedfile";
-
-    /** _more_ */
     public static final String TYPE_URL = "url";
-
-    /** _more_ */
     public static final String TYPE_REMOTE_FILE = "remotefile";
-
-    /** _more_ */
     public static final String TYPE_UNKNOWN = "unknown";
-
-    /** _more_ */
     private String path;
-
-    /** _more_ */
     private String type = TYPE_UNKNOWN;
-
-    /** _more_ */
     private File file;
-
-    /** _more_ */
     private long fileSize;
-
-    /** _more_ */
     private String md5;
 
-    /**
-     * _more_
-     */
     public Resource() {
         path = "";
     }
 
 
-    /**
-     * _more_
-     *
-     * @param file _more_
-     * @param type _more_
-     */
+    
     public Resource(File file, String type) {
         this.file = file;
         this.path = file.toString();
@@ -77,11 +44,7 @@ public class Resource {
 
 
 
-    /**
-     * _more_
-     *
-     * @param path _more_
-     */
+    
     public Resource(String path) {
         this.path = path;
         if (new File(path).exists()) {
@@ -95,23 +58,14 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @param url _more_
-     */
+    
     public Resource(URL url) {
         type = TYPE_URL;
         path = url.toString();
     }
 
 
-    /**
-     * _more_
-     *
-     * @param path _more_
-     * @param type _more_
-     */
+    
     public Resource(String path, String type) {
         this(path, type, null, -1);
     }
@@ -121,14 +75,7 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @param path _more_
-     * @param type _more_
-     * @param md5 _more_
-     * @param fileSize _more_
-     */
+    
     public Resource(String path, String type, String md5, long fileSize) {
         this.path     = path;
         this.type     = type;
@@ -139,11 +86,7 @@ public class Resource {
         }
     }
 
-    /**
-     * _more_
-     *
-     * @param that _more_
-     */
+    
     public Resource(Resource that) {
         this.path     = that.path;
         this.type     = that.type;
@@ -156,40 +99,22 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isDefined() {
         return (path != null) && (path.length() > 0);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isImage() {
         return isImage(path);
     }
 
-    /**
-     * _more_
-     *
-     * @param path _more_
-     *
-     * @return _more_
-     */
+    
     public static boolean isImage(String path) {
         return Utils.isImage(path);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isEditableImage() {
         if (path == null) {
             return false;
@@ -200,11 +125,7 @@ public class Resource {
                || file.endsWith(".gif") || file.endsWith(".png");
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public File getTheFile() {
         if (file == null) {
             file = new File(path);
@@ -214,11 +135,7 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public long getFileSize() {
 	return getFileSize(false);
     }
@@ -236,19 +153,12 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public long getFileSizeRaw() {
         return fileSize;
     }
 
-    /**
-     *
-     * @return _more_
-     */
+    
     public boolean hasResource() {
         return isFile() || isUrl();
     }
@@ -258,11 +168,7 @@ public class Resource {
 	return type.equals(TYPE_S3);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isFile() {
         if (type.equals(TYPE_REMOTE_FILE)) {
             return true;
@@ -281,18 +187,12 @@ public class Resource {
         return false;
     }
 
-    /**
-      * @return _more_
-     */
+    
     public boolean isUnknown() {
         return type.equals(TYPE_UNKNOWN);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean fileExists() {
         if (type.equals(TYPE_FILE) || type.equals(TYPE_STOREDFILE)
                 || type.equals(TYPE_LOCAL_FILE)
@@ -303,11 +203,7 @@ public class Resource {
         return false;
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isFileType() {
         if (isS3() || type.equals(TYPE_FILE) || type.equals(TYPE_STOREDFILE)
                 || type.equals(TYPE_LOCAL_FILE)) {
@@ -318,67 +214,39 @@ public class Resource {
     }
 
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isServerSideFile() {
         return type.equals(TYPE_LOCAL_FILE);
     }
 
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isStoredFile() {
         return type.equals(TYPE_STOREDFILE);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isUrl() {
         return type.equals(TYPE_URL);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public boolean isRemoteFile() {
         return type.equals(TYPE_REMOTE_FILE);
     }
 
-    /**
-     * _more_
-     *
-     * @return _more_
-     */
+    
     public String toString() {
         return path;
     }
 
-    /**
-     * Set the Path property.
-     *
-     * @param value The new value for Path
-     */
+    
     public void setPath(String value) {
         path = value;
         file = null;
     }
 
-    /**
-     *
-     * @param file _more_
-     * @param type _more_
-     */
+    
     public void setFile(File file, String type) {
 	fileSize= file.length();
         setPath(file.toString());
@@ -386,11 +254,7 @@ public class Resource {
     }
 
 
-    /**
-     * Get the Path property.
-     *
-     * @return The Path
-     */
+    
     public String getPath() {
         return path;
     }
@@ -400,20 +264,12 @@ public class Resource {
     }
 
 
-    /**
-     * Set the Type property.
-     *
-     * @param value The new value for Type
-     */
+    
     public void setType(String value) {
         type = value;
     }
 
-    /**
-     * Get the Type property.
-     *
-     * @return The Type
-     */
+    
     public String getType() {
         return type;
     }
@@ -422,30 +278,18 @@ public class Resource {
 
 
 
-    /**
-     * Set the FileSize property.
-     *
-     * @param value The new value for FileSize
-     */
+    
     public void setFileSize(long value) {
         this.fileSize = value;
     }
 
 
-    /**
-     *  Set the Md5 property.
-     *
-     *  @param value The new value for Md5
-     */
+    
     public void setMd5(String value) {
         md5 = value;
     }
 
-    /**
-     *  Get the Md5 property.
-     *
-     *  @return The Md5
-     */
+    
     public String getMd5() {
         if (md5 == null) {
             //For now don't do this because big files take a long time
