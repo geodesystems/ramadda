173fd71ee949aef48469c7d665d01bf33201efc5
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-11-16 06:45:27 -0700

    snapshot

---
+++ src/org/ramadda/projects/soil/bin/grace/grace.tcl
@@ -0,0 +1,185 @@
+package require Tcl 8.6
+
+##source the lib
+source $env(RAMADDA_ROOT)/bin/ramadda.tcl
+
+set ::sites {}
+array set ::sitesMap {}
+
+set ::xml "<entries>\n"
+
+oo::class create Base {
+    variable props
+    # Constructor to initialize the citations list as empty
+    constructor {} {
+	array set props {}
+    }
+
+    
+    method setProp {key value} {
+	set props($key) $value
+    }
+    method getProp {key} {
+	return $props($key)
+    }    
+}
+
+
+
+oo::class create Site {
+    superclass Base
+    variable id
+    variable citations
+    variable persons
+    variable treatments
+    variable units        
+    constructor {siteId} {
+	set id $siteId
+        set citations {}
+        set persons {}
+        set treatments {}
+        set units {}				
+    }
+
+    method addPerson {person} {
+        lappend persons $person
+    }
+
+    method addCitation {citation} {
+        lappend citations $citation
+    }
+    method addTreatment {treatment} {
+        lappend treatments $treatment
+    }    
+
+    method addUnit {unit} {
+        lappend units $unit
+    }    
+    
+    method listCitations {} {
+        foreach citation $citations {
+            puts [$citation getCitationDetails]
+        }
+    }
+    method process {} {
+	set name "$id - [my getProp site_id_descriptor]"
+	set start [my getProp start_date]
+	set end [my getProp end_date]	
+
+	append ::xml [openEntry type_geo_site $id {} $name]
+	if {$start!=""} {
+	    append ::xml [col fromdate $start]
+	}
+	if {$end!=""} {
+	    append ::xml [col todate $end]
+	}
+	append ::xml [col latitude [my getProp latitude_decimal_deg]]
+	append ::xml [col longitude [my getProp longitude_decimal_deg]]
+	append ::xml [col altitude [my getProp elevation_m]]		
+	append ::xml [col description [my getProp site_history]]
+	set	alias "grace-[string tolower $id]"
+	regsub { } $alias _ alias
+	append ::xml [mtd1 content.alias $alias]
+	append ::xml [mtd5 content.address "" [my getProp city] [my getProp state_province] [my getProp postal_code] [my getProp country]   ]
+
+#country city postal_code
+	foreach prop {siteid research_unit project_name experiment_name funding_source mlra native_veg duration_of_study} {
+	    set label $prop
+	    regsub -all _ $label { } label
+	    set label [toProperCase $label]
+	    set v [string trim [my getProp $prop]]
+	    if {$v!=""} {
+		append ::xml [mtd2 property $label $v]
+	    }
+	}
+	foreach person $persons {
+	    set name "[$person getProp first_name] [$person getProp middle_name] [$person getProp last_name] [$person getProp suffix]"
+	    regsub -all {  +} $name { } name
+	    set name [string trim $name]
+	    puts stderr $name
+	}
+
+
+
+	append ::xml [closeEntry] 
+    }
+}
+
+
+set ::overviewArgs {siteid site_id_descriptor research_unit project_name experiment_name funding_source start_date end_date duration_of_study}
+proc overview $::overviewArgs {
+    set mySite [Site new $siteid]
+    set ::sitesMap($siteid) $mySite
+    lappend ::sites $mySite
+
+    foreach attr $::overviewArgs {
+	$mySite setProp $attr [set $attr]
+    }
+}
+
+
+set ::fieldsitesArgs {siteid date mlra field_id country state_province county city postal_code latitude_decimal_deg longitude_decimal_deg spatial_description elevation_m map_mm mat_degc native_veg site_history}
+proc fieldsites $::fieldsitesArgs {
+    set site $::sitesMap($siteid) 
+    foreach attr $::fieldsitesArgs {
+	$site setProp $attr [set $attr]
+    }
+}
+
+
+set ::personsArgs {siteid last_name first_name  middle_name  suffix  role_in_study  primary_contact  department  organization  date_created profession email telephone  web_site  note} 
+proc persons $::personsArgs {
+    set site $::sitesMap($siteid) 
+    set person [Base new]
+    $site addPerson $person
+    foreach arg $::personsArgs {
+	$person setProp $arg [set $arg]
+    }
+}
+
+set ::citationsArgs {siteid date_published type title is_part_of  author  correspond_author  identifier description citation url} 
+proc citations $::citationsArgs {
+    set site $::sitesMap($siteid) 
+    set citation [Base new]
+    $site addCitation $citation
+    foreach arg $::citationsArgs {
+	$citation setProp $arg [set $arg]
+    }
+
+
+}
+
+set ::treatmentsArgs  {    siteid treatment_id start_date  treatment_descriptor  rotation_descriptor  tillage_descriptor n_treatment_descriptor  project_scenario fertilizer_amendment_class  cover_crop residue_removal irrigation organic_management  grazing_rate animal_species  operation ars_projects}
+proc treatments $::treatmentsArgs {
+    set site $::sitesMap($siteid) 
+    set treatment [Base new]
+    $site addTreatment $treatment
+    foreach arg $::treatmentsArgs {
+	$treatment setProp $arg [set $arg]
+    }
+}
+
+set ::experunitsArgs {siteid exp_unit_id  treatment_id field_id  start_date  end_date  change_in_management soil_series  soil_classification  landscape_position  latitude longitude slope exp_unit_size_m2}
+
+proc experunits  $::experunitsArgs {
+    set site $::sitesMap($siteid) 
+    set unit [Base new]
+    $site addUnit $unit
+    foreach arg  $::experunitsArgs {
+	$unit setProp $arg [set $arg]
+    }
+}
+
+source tcl/overview.tcl
+source tcl/fieldsites.tcl
+source tcl/persons.tcl
+source tcl/citations.tcl
+source tcl/treatments.tcl
+source tcl/experunits.tcl
+
+
+foreach site  $::sites {
+    $site process
+}
+append ::xml "</entries>"
+puts $::xml
---
+++ src/org/ramadda/projects/soil/bin/grace/processgrace.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+export mydir=`dirname $0`
+
+
+#This calls the SeeSV command line
+#The environment variable needs to be set to the seesv.sh
+#e.g.
+#export SEESV=/Users/jeffmc/bin/seesv.sh
+
+#The -cleaninput says the input CSV is one line per record
+#the -dots say to print a progress message
+
+seesv() {
+    ${SEESV}   -dots  "tab5000"  "$@"
+}
+
+
+if [ ! -d "tcl" ]; then
+    mkdir  tcl
+fi
+
+
+for file in "$@"; do
+  filename=$(basename "$file")
+  echo "$filename"
+  basename="${filename%.*}"
+  seesv -trim 0-100 -min 1 -tcl $basename  $file > "tcl/$basename.tcl"
+done
+
