4871a1d65a53826f88685b44451995d2449eee43
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-11-24 10:18:54 -0700

    snapshot

--- src/org/ramadda/projects/lakota/bin/convertgw.sh
+++ src/org/ramadda/projects/lakota/bin/convertgw.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#the SEESV environment variable needs to point to the  seesv.sh script in RAMADDA's SeeSV  release
-#This converts the input groundwater file summarizing on the yearly average
+#the SEESV environment variable needs to point to the directory holding the seesv.sh script in RAMADDA's SeeSV  release
 #
-
 sh ${SEESV} \
-   -insert "" site "\${file_shortname}" -change site "^.*/" "" -change site "- U\.S\..*" "" \
+   -insert "" site "\${file_shortname}" \
+   -change site "^.*/" "" -change site "- U\.S\..*" "" \
+   -change site "_u_s_geological_survey" "" \
    -change site ".*\(RST" "RST"     -trim site -change site "\)$" "" \
-   "-extractdate" "time" "year" -gt year 1984 \
-   "-summary" "year" "original_value" "site,latitude,longitude" "average"  -print  "$1"
+   -extractdate "time" "year" -gt year 1984 \
+   -summary "year" "original_value" "site,latitude,longitude" "average"  -print  "$1"
 
 #sh ${SEESV}/seesv.sh "-extractdate" "time" "year" "-summary" "year" "original_value" "latitude,longitude" "average" "-increase" "original_value_average" "1"  -print  "$1"
--- src/org/ramadda/projects/lakota/bin/groundwater.tcl
+++ src/org/ramadda/projects/lakota/bin/groundwater.tcl
@@ -1,41 +1,32 @@
-#
-#This takes as a command line argument the CSV file of entry information
-#of the groundwater sites from https://sicangudata.org/repository/search/type/type_usgs_ngwm
-#and creates a groundwwater_yearly_averages.csv file
-#
+#This downloads groundwater data using the csvget.sh script
 
 set ::scriptDir [file dirname [info script]]
 
+#make the dir
 if {![file exists average]} {
     exec mkdir average
 }
 
 if {![file exists data]} {
+    puts stderr "downloading data"
     exec mkdir data
+    cd data
+    exec sh ../csvget.sh
+    cd ..
 }
 
-#pull out the id and name of the sites
-exec sh $env(SEESV) -columns "id,name" -unique name exact -tcl site [lindex $argv  0] > sites.tcl
+
 set ::cnt 0
-
-proc site {id name} {
-    set file "data/${name}.csv"
-    if {![file exists $file]} {
-	puts "fetching $file"
-	catch {exec wget -O $file https://sicangudata.org/repository/entry/show?entryid=$id&output=points.product&getdata=Get%20Data&product=points.csv&addlatlon=true}
-    }
+foreach csv [glob data/*.csv] {
     incr ::cnt
-    set file2 "average/${name}_average.csv"
-#Convert the file
-    exec sh $::scriptDir/convertgw.sh $file > "$file2"
-    puts "$file2"
+    set name [file tail $csv]
+    set file2 "average/$name"
+    puts "creating average file: $file2"
+    exec sh $::scriptDir/convertgw.sh $csv > "$file2"
     if {$::cnt==1} {
-	exec cat $file2 > groundwwater_yearly_averages.csv
+        exec cat $file2 > groundwater_yearly_averages.csv
     } else {
-	exec tail -n +2 $file2 >> groundwwater_yearly_averages.csv
+        exec tail -n +2 $file2 >> groundwater_yearly_averages.csv
     }
 }
-
-source sites.tcl
-
 
