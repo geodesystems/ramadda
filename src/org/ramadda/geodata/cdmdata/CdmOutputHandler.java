26b03d86dbe642832293629dd253d0fd7d0950ba
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2024-12-31 22:08:18 -0700

    update copyright

--- src/org/ramadda/geodata/cdmdata/CdmOutputHandler.java
+++ src/org/ramadda/geodata/cdmdata/CdmOutputHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2023 Geode Systems LLC
+ * Copyright (c) 2008-2025 Geode Systems LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
