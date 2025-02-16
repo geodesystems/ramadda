91726e4d873c5b0223c50d68b95fdb246ae98ee8
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-02-15 19:11:57 -0700

    update to the latest pdfbox release

--- bin/pdfbox.sh
+++ bin/pdfbox.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#Download pdfbox-app-2.0.27.jar from https://pdfbox.apache.org/download.html
+#Download pdfbox-app-3.0.4.jar from https://pdfbox.apache.org/download.html
 #and copy it to the same dir as this script
 
 
 dir=`dirname $0`
-java -jar ${dir}/pdfbox-app-2.0.27.jar "$@"
+java -jar ${dir}/pdfbox-app-3.0.4.jar "$@"
