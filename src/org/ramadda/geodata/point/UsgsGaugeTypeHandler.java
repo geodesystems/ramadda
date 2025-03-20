f33c327f54ce82a6971976e240c01f77bf15ec41
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-03-20 09:13:08 -0600

    add a use daily value. create a total_volume derived quantity

--- src/org/ramadda/geodata/point/UsgsGaugeTypeHandler.java
+++ src/org/ramadda/geodata/point/UsgsGaugeTypeHandler.java
@@ -40,6 +40,7 @@ import java.util.HashSet;
 
 
 
+@SuppressWarnings("unchecked")
 public class UsgsGaugeTypeHandler extends PointTypeHandler {
     public UsgsGaugeTypeHandler(Repository repository, Element node)
             throws Exception {
@@ -101,18 +102,49 @@ public class UsgsGaugeTypeHandler extends PointTypeHandler {
 	    
     }
 
+    //agency_cd	site_no	datetime	tz_cd	129346_00060	129346_00060_cd	283298_00065	283298_00065_cd
+    private static String CSV_HEADER_DV = "agency\\,station_id\\,date\\,discharge\\,skip1\\,gauge_height\\,skip2";
+
+    //agency_cd	site_no	datetime	tz_cd	129346_00060	129346_00060_cd	283298_00065	283298_00065_cd
+    private static String CSV_HEADER_IV = "agency\\,station_id\\,date\\,timezone\\,discharge\\,skip1\\,gauge_height\\,skip2";    
+
+    
+    private static String[] CSV_COMMANDS = {
+
+	"-tab,-header,${header}",
+	"-skip,3,-notcolumns,?skip1\\,?skip2,-change,discharge\\,gauge_height,(?i)(dis|ice|ssn|eqp|rat)",
+	"0,-integrate,discharge,date,second",
+	"volume,-scale,volume,0,0.00002295684",
+	"0,-decimals,volume,2,-runningsum",
+	"volume,-set,sum_volume,0,total_volume",
+	"-addheader, station_id.type string   discharge.type double   gauge_height.type double total_volume.unit {acre feet} total_volume.type double   volume.type double date.format {${format}}"
+    };
+
+
+
     private static final String OLD_URL_TEMPLATE_FLOW =
         "https://waterdata.usgs.gov/nwis/uv?cb_00060=on&cb_00065=on&format=rdb&site_no=${station_id}&period=${period}";
 
     private static final String URL_TEMPLATE_FLOW =
 	"https://nwis.waterdata.usgs.gov/usa/nwis/uv/?cb_00060=on&cb_00065=on&format=rdb&site_no=${station_id}&period=${period}";
 
-    private static final String URL_TEMPLATE_FLOW_DATE =
-	"https://waterservices.usgs.gov/nwis/iv/?format=rdb&site=${station_id}&startDT=${startDate}&endDT=${endDate}&parameterCd=00060,00065";
+    private static final String URL_TEMPLATE_FLOW_DATE_DV =
+	"https://waterservices.usgs.gov/nwis/dv/?format=rdb&site=${station_id}&startDT=${startDate}&endDT=${endDate}&parameterCd=00060,00065";
+    private static final String URL_TEMPLATE_FLOW_DATE_IV =
+	"https://waterservices.usgs.gov/nwis/iv/?format=rdb&site=${station_id}&startDT=${startDate}&endDT=${endDate}&parameterCd=00060,00065";    
 
     private static final String URL_TEMPLATE_PEAK =
 	"https://nwis.waterdata.usgs.gov/nwis/peak?site_no=${station_id}&agency_cd=USGS&format=rdb";
 
+
+
+    //    private static final String FIELDS_IV="agency[label=\"Agency\" type=string],station_id[label=\"Station ID\" type=string],date[type=date format=\"yyyy-MM-dd HH:mm\" label=\"Date\"],timezone[label=\"Timezone\" type=string],discharge[unit=\"cfs\" label=\"Discharge\" type=double],skip1[type=string],gauge_height[unit=\"feet\" label=\"Gauge Height\" type=double],skip2[type=string]";
+    private static final String FIELDS_IV="agency[label=\"Agency\" type=string],station_id[label=\"Station ID\" type=string],date[type=date format=\"yyyy-MM-dd HH:mm\" label=\"Date\"],timezone[label=\"Timezone\" type=string],discharge[unit=\"cfs\" label=\"Discharge\" type=double],gauge_height[unit=\"feet\" label=\"Gauge Height\" type=double]";
+
+    //agency_cd	site_no	datetime	128646_00060_00003	128646_00060_00003_cd	238320_00065_00003	238320_00065_00003_cd
+    //USGS	06447000	2024-04-01	168	A	4.71	A
+    //    private static final String FIELDS_DV="agency[label=\"Agency\" type=string],station_id[label=\"Station ID\" type=string],date[type=date format=\"yyyy-MM-dd\" label=\"Date\"],discharge[unit=\"cfs\" label=\"Discharge\" type=double],skip1[type=string],gauge_height[unit=\"feet\" label=\"Gauge Height\" type=double],skip2[type=string]";
+    private static final String FIELDS_DV="agency[label=\"Agency\" type=string],station_id[label=\"Station ID\" type=string],date[type=date format=\"yyyy-MM-dd\" label=\"Date\"],discharge[unit=\"cfs\" label=\"Discharge\" type=double],gauge_height[unit=\"feet\" label=\"Gauge Height\" type=double]";        
 
 
     @Override
@@ -120,16 +152,37 @@ public class UsgsGaugeTypeHandler extends PointTypeHandler {
                                        Hashtable properties,
                                        Hashtable requestProperties)
             throws Exception {
-        return new UsgsGaugeRecordFile(getPathForRecordEntry(request,entry,requestProperties), properties);
+	for(int i=0;i<CSV_COMMANDS.length;i++) {
+	    String command = CSV_COMMANDS[i];
+	    command = command.replace("${header}",useDailyValue(request, entry)?CSV_HEADER_DV:CSV_HEADER_IV);
+	    command = command.replace("${format}",useDailyValue(request, entry)?"yyyy-MM-dd":"yyyy-MM-dd HH:mm");
+	    System.err.println(command);
+	    properties.put("csvcommands" + (i+1),command);
+	}
+        return new UsgsGaugeRecordFile(request, entry, getPathForRecordEntry(request,entry,requestProperties), properties);
     }
 
 
     public static class UsgsGaugeRecordFile extends CsvFile {
+	Request request;
+	Entry entry;
 
-        public UsgsGaugeRecordFile(IO.Path path, Hashtable properties)
+        public UsgsGaugeRecordFile(Request request,Entry entry,IO.Path path, Hashtable properties)
                 throws IOException {
             super(path, properties);
+	    this.request= request;
+	    this.entry = entry;
         }
+
+	public String xxxgetFieldsProperty() {
+	    if(useDailyValue(request,entry)) {
+		//		System.err.println("DV:" +FIELDS_DV);
+		return FIELDS_DV;
+	    }
+	    //	    System.err.println("IV:" +FIELDS_IV);
+	    return FIELDS_IV;		
+	}
+
 
         public boolean isMissingValue(BaseRecord record, RecordField field,
                                       String s) {
@@ -144,6 +197,10 @@ public class UsgsGaugeTypeHandler extends PointTypeHandler {
 
     }
 
+    public static boolean useDailyValue(Request request, Entry entry) {
+	return Misc.equals(entry.getStringValue(request,"use_daily_value",""),"true");
+    }
+	
     @Override
     public String getPathForEntry(Request request, Entry entry, boolean forRead)
             throws Exception {
@@ -155,7 +212,7 @@ public class UsgsGaugeTypeHandler extends PointTypeHandler {
 	    url = URL_TEMPLATE_PEAK;
 	} else {
 	    if(Misc.equals(entry.getStringValue(request,"use_date_range",null),"true")) {
-		url =  URL_TEMPLATE_FLOW_DATE;
+		url =  useDailyValue(request, entry)?URL_TEMPLATE_FLOW_DATE_DV:URL_TEMPLATE_FLOW_DATE_IV;
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Date start = DateHandler.checkDate(new Date(entry.getStartDate()));
 		Date end = DateHandler.checkDate(new Date(entry.getEndDate()));		
@@ -169,7 +226,7 @@ public class UsgsGaugeTypeHandler extends PointTypeHandler {
 	    }
 	}
 	url = url.replace("${station_id}",("" + entry.getValue(request,"station_id")).trim());
-	//	System.err.println("URL: "+ url);
+	System.err.println("URL: "+ url);
         return url;
     }
 
