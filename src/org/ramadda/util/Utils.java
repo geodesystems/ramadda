766f8543824baed011979c8d170b9603dc645307
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-03-29 23:10:34 -0600

    new ansi_black_bold

--- src/org/ramadda/util/Utils.java
+++ src/org/ramadda/util/Utils.java
@@ -103,12 +103,18 @@ public class Utils extends IO {
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public static final String ANSI_RED_BOLD = "\033[1;31m";    // RED
+    public static final String ANSI_ORANGE_BOLD = "\033[1;38;5;208m";
+
+
     public static final String ANSI_GREEN_BOLD = "\033[1;32m";  // GREEN
     public static final String ANSI_YELLOW_BOLD = "\033[1;33m"; // YELLOW
     public static final String ANSI_BLUE_BOLD = "\033[1;34m";   // BLUE
     public static final String ANSI_PURPLE_BOLD = "\033[1;35m"; // PURPLE
     public static final String ANSI_CYAN_BOLD = "\033[1;36m";   // CYAN
     public static final String ANSI_WHITE_BOLD = "\033[1;37m";  // WHITE
+    public static final String ANSI_BLACK_BOLD = "\033[1;30m";
+    
+
 
     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
     public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
