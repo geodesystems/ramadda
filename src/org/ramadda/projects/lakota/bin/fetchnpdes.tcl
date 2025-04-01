9881a23b009a409cf9e9070740175908c1571626
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-04-01 06:02:31 -0600

    snapshot

--- src/org/ramadda/projects/lakota/bin/fetchnpdes.tcl
+++ src/org/ramadda/projects/lakota/bin/fetchnpdes.tcl
@@ -1,3 +1,27 @@
+if {0} {
+    #grab the html file from the epa that lists the npdes permits-
+    https://www.epa.gov/npdes-permits/south-dakota-npdes-permits
+    
+    #run the seesv script to extract the data from the html
+    #generate a tcl script that we source
+    seesv -htmltable 0 "" "" -tcl site epa.html > epasites.tcl
+
+    #the script below then runs through each site and downloads the data file from:
+    https://echo.epa.gov/trends/loading-tool/get-data/monitoring-data-download
+
+    #It then runs the seesv to strip out extraneous headers and it inserts the location and facility name
+
+    #It also combines the parameter_description, statistical_base and outfall_number columns into a single parameter column
+
+    #It then reorders the columns and drops extraneous columns
+
+    #It then cleans up the non-numeric values in the  limit_value and  dmr_value columns
+
+    #It then runs through all of the processed site csv files and merges them into a single file 
+
+}
+
+
 set ::siteArgs { location  facility_or_permit_name  permit_number  permit_status  effective_date  expiration_date }
 proc site $::siteArgs {
     set id $permit_number
@@ -11,8 +35,8 @@ proc site $::siteArgs {
 
     set processed processed_${id}.csv
     if {![file exists $processed]} {
-	puts stderr "processing $location - $id"
-	exec  sh "$::env(SEESV)" -skip 4 -add "location" "$location" -firstcolumns location -p $filename > $processed
+	puts stderr "processing $facility_or_permit_name - $id"
+	exec  sh "$::env(SEESV)" -skip 4 -combine "parameter_description,statistical_base,outfall_number" " - " parameter  -add "location" "$location" -add "facility" "$facility_or_permit_name" -firstcolumns "location,facility" -firstcolumns "location,facility,parameter"  -notcolumns "outfall_number,monitoring_location_code,limit_set_designator,parameter_code,parameter_description,statistical_base" -change limit_value Mon NaN -change dmr_value "^NODI:.*$" NaN -change dmr_value ".*<.*" NaN -p $filename > $processed
     }
 }
 
