bb95388117a60a8bdf727d01911226e6239291a0
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-04-04 13:11:49 -0600

    snapshot

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {21.87.0}
+gen::defineMacro {<%ramadda.version%>} {21.88.0}
 
 
 namespace eval wiki {}
