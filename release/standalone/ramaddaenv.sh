2d1a98ab242f395d946776bb300fc1c035733474
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-05-17 06:56:09 -0600

    more better documentation of the config options. remove the SSL port as it can be configured in the install.properties file

--- release/standalone/ramaddaenv.sh
+++ release/standalone/ramaddaenv.sh
@@ -8,23 +8,21 @@
 ##and uncomment what you want to set
 ##
 
+#which java to use
+#export JAVA=/usr/bin/java
+
 #RAMADDA home directory
 #export RAMADDA_HOME=${HOME}/.ramadda
 
-##The default ports set in ramadda.sh are 8080 and 8443 so this can be run
-##without sudo privileges
-##If you are running for real set these ports to 80 and 443
-
-
 #HTTP port RAMADDA runs on
+#the default is 8080
 #export RAMADDA_PORT=80
 
-#HTTPS port RAMADDA runs on
-#export RAMADDA_SSL_PORT=443
+#Note: the HTTPS port (443) can be configured in the install.properties file
+#in the RAMADDA home directory
 
 
-#which java to use
-#export JAVA=/usr/bin/java
+
 
 
 
