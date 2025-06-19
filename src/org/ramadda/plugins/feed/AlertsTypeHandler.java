23b2de18faa5b618e7ecfff1135913cb8fd372cb
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-06-19 08:45:26 -0600

    more work on the NWS Alerts

---
+++ src/org/ramadda/plugins/feed/AlertsTypeHandler.java
@@ -0,0 +1,140 @@
+/**
+   Copyright (c) 2008-2025 Geode Systems LLC
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.ramadda.plugins.feed;
+
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.metadata.*;
+import org.ramadda.repository.type.*;
+import org.ramadda.util.AtomUtil;
+import org.ramadda.util.HtmlUtils;
+import org.ramadda.util.IO;
+import org.ramadda.util.TTLCache;
+import org.ramadda.util.Utils;
+import org.ramadda.util.WikiUtil;
+
+import org.ramadda.data.point.text.*;
+import org.ramadda.data.record.*;
+
+import org.ramadda.data.services.PointTypeHandler;
+import org.ramadda.data.services.RecordTypeHandler;
+
+import ucar.unidata.xml.XmlUtil;
+
+
+import org.w3c.dom.*;
+
+
+import java.net.URL;
+import java.io.*;
+import java.text.SimpleDateFormat;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+
+import java.util.TimeZone;
+
+
+
+@SuppressWarnings("unchecked")
+public class AlertsTypeHandler extends GenericTypeHandler {
+
+    public AlertsTypeHandler(Repository repository, Element entryNode)
+	throws Exception {
+        super(repository, entryNode);
+    }
+
+    @Override
+    public void getWikiTags(List<String[]> tags, Entry entry) {
+	super.getWikiTags(tags, entry);
+        tags.add(new String[]{"NWS Alerts","nws_alerts"});
+    }
+
+
+    @Override
+    public String getWikiInclude(WikiUtil wikiUtil, Request request,
+                                 Entry originalEntry, Entry entry,
+                                 String tag, Hashtable props)
+	throws Exception {
+	if(!tag.equals("nws_alerts")) { 
+	    return super.getWikiInclude(wikiUtil, request, originalEntry,
+					entry, tag, props);
+	}
+
+	StringBuilder sb = new StringBuilder();
+	sb.append(HU.importJS(getRepository().getHtdocsUrl("/feed/alerts.js")));
+	sb.append(HU.cssLink(getRepository().getHtdocsUrl("/feed/alerts.css")));
+	String id = HU.getUniqueId("alerts");
+	sb.append(HU.div("",HU.attrs("id",id,"class","alerts")));
+	
+	String url = null;
+	List attrs = new ArrayList();
+	for (Object key : props.keySet()) {
+	    Object v = props.get(key);
+	    if(key.equals("urls") || key.equals("points") ||
+	       key.equals("zones") || key.equals("areas")) {
+		v = JU.list(Utils.split(v.toString(),",",true,true),true);
+	    } else {
+		v = JU.quote(v.toString());
+	    }
+	    Utils.add(attrs,key.toString(),v);
+	}
+	if(entry.isFile()) {
+	    url = entry.getTypeHandler().getEntryResourceUrl(request, entry);
+	} else if(entry.getResource().isUrl()) {
+	    url = entry.getResourcePath(request);
+	}
+	boolean hasProp = props.containsKey("urls")||
+	    props.containsKey("zones") ||
+	    props.containsKey("areas") ||
+	    props.containsKey("points");	    
+
+	String headerMessages = entry.getStringValue(request,"header_messages","");
+	if(stringDefined(headerMessages)) {
+	    for(String line:Utils.split(headerMessages,"\n",true,true)) {
+		if(line.startsWith("#")) continue;
+		List<String> toks = Utils.splitUpTo(line,":",2);
+		if(toks.size()==2) {
+		    Utils.add(attrs,"headerMessage."+ toks.get(0),JU.quote(toks.get(1)));
+		}
+	    }
+	}
+
+
+	if(!hasProp) {
+	    if(stringDefined(url)) {
+		Utils.add(attrs,"urls",JU.quote(url));
+	    }
+	    String zones = entry.getStringValue(request,"zones","");
+	    if(stringDefined(zones)) {
+		Utils.add(attrs,"zones",JU.quoteAll(Utils.split(zones,"\n",true,true)));
+	    }
+	    String areas = entry.getStringValue(request,"areas","");
+	    if(stringDefined(areas)) {
+		Utils.add(attrs,"areas",JU.quoteAll(Utils.split(areas,"\n",true,true)));
+	    }	
+	    List points = new ArrayList();
+	    if(entry.hasLocationDefined(request)) {
+		double[]point = entry.getLocation(request);
+		points.add(point[0]+","+point[1]);
+	    }
+	    if(points.size()>0) {
+		Utils.add(attrs,"points",JU.quoteAll(points));
+	    }
+	}
+	HU.script(sb,"new NwsAlerts(" + HU.squote(id)+"," + JU.map(attrs)+");\n");
+	return sb.toString();
+
+    }
+
+
+}
--- src/org/ramadda/plugins/feed/htdocs/feed/alerts.js
+++ src/org/ramadda/plugins/feed/htdocs/feed/alerts.js
@@ -1,20 +1,475 @@
-function NwsAlerts(url,div,opts) {
-    $.extend(this,{
-	url:url,
-	div:div
+function NwsAlerts(div,opts) {
+    this.opts={};
+    this.div = div;
+    $.extend(this.opts,{
+	debug:true,
+	all:false,
+	urls:[],
+	zones:[],
+	areas:[],
+	points:[],
+	title:'NWS Alerts',
+	noAlertsMessage:'There are currently no alerts'
     });
-    if(opts) $.extend(this,opts);
+    if(opts) $.extend(this.opts,opts);
     this.init();
-  
 }
 
 NwsAlerts.prototype = {
-    init:function() {
-        $.getJSON(this.url), data=>{
-	}).fail(data=>{
-	    console.error('NWS Alerts: failed to read alert:' + this.url);
-	    console.dir(data);
+    base:'https://api.weather.gov/alerts/active',
+    getProperty:function(key,dflt) {
+	if(!Utils.isDefined(this.opts[key])) return dflt;
+	return this.opts[key];
+    },
+    getFullProperty:function(key,type,value,dflt) {
+	//key.value key.type key
+	return this.getProperty(key+'.'+value,
+				this.getProperty(key+'.'+type,
+						 this.getProperty(key,dflt)));
+    },
+    addHeader:function(msg)  {
+	if(this.seenHeader[msg]) return;
+	this.seenHeader[msg] = true;
+	msg = HU.div([ATTR_CLASS,'alerts-header'],msg);
+	jqid(this.headerID).append(msg);
+    },
+    debug:function(msg) {
+	if(this.opts.debug) console.log(msg);
+    },
+    writeEvents:function() {
+	let txt='<table>\n';
+	txt+=HU.tr([],HU.td([],HU.b('ID'))+
+		   HU.td([ATTR_STYLE,HU.css('padding-left','1em','padding-right','1em')],HU.b('Event Type'))+HU.td([],HU.b('Description'))) +'\n';
+	Object.keys(this.phenomena).forEach(key=>{
+	    let info = this.phenomena[key];
+	    txt+=HU.tr(['class','search-component'],
+		       HU.td([ATTR_CLASS,'eventtype'],key)+
+		       HU.td([ATTR_CLASS,'eventtype',ATTR_STYLE,HU.css('padding-left','1em','padding-right','1em')],info.type??'')+HU.td(['align','left'],info.label)) +'\n';
 	});
+	txt+='</table>';
+	Utils.makeDownloadFile('wx.html',txt);
+
+    },
+    init:function() {
+//	this.writeEvents();
+	this.headerID =HU.getUniqueId();
+	this.getDiv().append(HU.div([ATTR_ID,this.headerID],''));
+	this.seenHeader={};
+	this.allUrls = []
+	if(this.opts.all) this.allUrls.push({url:this.base});
+	let addUrl=(type,value) =>{
+	    if(!Utils.stringDefined(value) || value.startsWith('#')) return;
+	    this.allUrls.push({url:this.base+'?' + type+'='+ value,type:type,value:value});
+	}
+
+	this.allUrls = Utils.mergeLists(this.allUrls,this.opts.urls);
+	this.opts.zones.forEach(zone=>{
+	    addUrl('zone',zone);
+	});
+	this.opts.areas.forEach(area=>{
+	    addUrl('area',area);
+	});
+	this.opts.points.forEach(point=>{
+	    addUrl('point',point);
+	});	
+	this.loadedCount=0;
+	this.alertCount=0;	
+	if(this.allUrls.length==0) {
+	    this.finish();
+	}
+	this.allUrls.forEach(item=>{
+	    if(Utils.stringDefined(item.url)) {
+		this.loadAlert(item.url,item.type,item.value,this.allUrls.length>1);
+	    }
+	});
+    },
+    loadAlert:function(url,type,value,multiples) {    
+	if(!Utils.stringDefined(url)) return;
+        $.getJSON(url, data=>{
+	    this.loadedCount++;
+	    let now = new Date();
+	    let innerContents = '';
+	    if(data.features.length==0) {
+		this.finish();
+		console.log('no events for:',type,value,url)
+		return;
+		//		innerContents+=this.noAlertsMessage;
+	    }
+	    let warnings = [];
+	    let watches = [];
+	    let other = [];
+	    data.features.forEach((alert,idx)=>{
+		let contents = '';
+		let props = alert.properties;
+		let pre = props.description;
+		if(Utils.stringDefined(props.instruction)) {
+		    pre+='\n\n' + HU.b('Instructions')+'\n';
+		    pre+=props.instruction;
+		}
+		if(Utils.stringDefined(props.areaDesc)) {
+		    pre+='\n\n' + HU.b('Area')+'\n';
+		    pre+=props.areaDesc;
+		}		
+		let title = props.headline;
+		let significance='';
+		let phenon='';
+		let code='';
+		if(props?.eventCode?.NationalWeatherService?.length) {
+		    code = props.eventCode.NationalWeatherService[0];
+		    phenon = code.substring(0,2);
+		    significance = code.substring(2,3);		    
+		    let info = this.phenomena[phenon]??{};
+		    let icon = info.emoji;
+		    if(icon) {
+			if(icon.startsWith('/'))
+			    icon = HU.image(RamaddaUtils.getUrl(icon));
+			else if(icon.startsWith('fa'))
+			    icon = HU.getIconImage(icon);
+			title = HU.span([ATTR_CLASS,'alert-icon'], icon)+' '+ title;
+		    } else {
+			//console.log(code,phenon,significance);
+			title = phenon +' '+ title;
+		    }
+
+		    this.debug('alert: type='+ type +' value:' + value +' phenomena:' + phenon +(info.label?' ' + info.label:'') +' event:'+ props.event);
+		    let headerMessage=this.getProperty('headerMessage.'+phenon,
+						       this.getProperty('headerMessage.'+info.type,null));
+		    if(headerMessage) this.addHeader(headerMessage);
+		    let expires = props.expires?new Date(props.expires):null;
+		    let sent = new Date(props.sent);
+		    //		    console.log(Utils.formatDate(sent) +" " + sent);
+		}
+		contents += HU.pre([],pre);
+		let l = (significance=='W'?warnings:(significance=='A'?watches:other));
+		if(code=='SPS') l=warnings;
+
+		l.push({significance:significance,
+			label:title,contents:contents});
+		this.alertCount++;
+		//		console.log(props.severity,props.certainty);
+	    });
+	    let list = Utils.mergeLists(warnings,watches,other);
+	    let accordion;
+	    let label = this.getFullProperty('alertLabel',type,value,data.title);
+	    if(list.length>0) {
+		accordion = HU.makeAccordionHtml(list);
+		innerContents+=accordion.contents;
+		if(list.length>1)
+		    label +=  ' - ' + list.length+' alerts';
+		else if(list.length==1)
+		    label +=  ' - ' + list.length+' alert';
+	    }
+	    let html;
+	    let showToggle = this.getFullProperty('showToggle',type,value,multiples);
+	    
+	    if(showToggle) {
+		let toggleOpen = this.getFullProperty('toggleOpen',type,value,multiples);
+		label = HU.span([ATTR_CLASS,'alert-header'],label);
+		html = HU.toggleBlock(label,innerContents,toggleOpen);
+	    }     else {
+		html = HU.div([ATTR_CLASS,'alert-header'],label)+innerContents;
+	    }
+	    this.getDiv().append(html);
+	    if(accordion) {
+		accordion.init({active:-1,xdecorate:true});
+	    }
+	    this.finish();
+	}).fail(data=>{
+	    this.loadedCount++;
+	    this.getDiv().append(HU.div('NWS Alerts: failed to read alert: ' + url));
+	    console.error('NWS Alerts: failed to read alert: ' +url);
+	    console.error(data.responseText);
+	    this.finish();
+	});
+    },
+
+    getDiv:function() {
+	return jqid(this.div);
+    },
+    finish:function() {
+	if(this.allUrls.length!=this.loadedCount) return;
+	if(this.alertCount==0) {
+	    this.getDiv().html(this.opts.noAlertsMessage);
+	}
+    },
+    significance: {
+	"W": "Warning",
+	"A": "Watch",
+	"Y": "Advisory",
+	"S": "Statement",
+	"F": "Forecast",
+	"O": "Outlook",
+	"N": "Synopsis",
+	"R": "Record"
+    },
+    phenomena: {
+	"AQ": {
+	    "label": "Air Quality",
+	    "emoji": "\ud83c\udfed"
+	},
+	"AV": {
+	    "label": "Avalanche",
+	    "emoji": "\ud83c\udfd4\ufe0f"
+	},
+	"BH": {
+	    "label": "Beach Hazards",
+	    "emoji": "\ud83c\udfd6\ufe0f"
+	},
+	"BZ": {
+	    "label": "Blizzard",
+	    "emoji": "\ud83c\udf28\ufe0f"
+	},
+	"SP": {
+	    "label":"Special Weather",
+	    "emoji": "\u26a0\ufe0f"
+	},
+
+	"DS": {
+	    "label": "Dust Storm",
+	    "emoji": "\ud83c\udf2a\ufe0f"
+	},
+	"DU": {
+	    "label": "Blowing Dust",
+	    "emoji": "\ud83c\udf2b\ufe0f"
+	},
+	"EC": {
+	    type:'cold',
+	    "label": "Extreme Cold",
+	    "emoji": "\u2744\ufe0f"
+	},
+	"EH": {
+	    type:'heat',
+	    "label": "Excessive Heat",
+	    "emoji": "\ud83c\udf21\ufe0f"
+	},
+	"XH": {
+	    type:'heat',
+	    "label": "Extreme Heat",
+	    "emoji": "\ud83c\udf21\ufe0f"
+	},
+	"HT": {
+	    type:'heat',
+	    "label": "Heat",
+	    "emoji": "\ud83d\udd25"
+	},
+	"CF": {
+	    type:'flood',
+	    "label": "Coastal Flood",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"FA": {
+	    type:'flood',
+	    "label": "Areal Flood",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"FF": {
+	    type:'flood',
+	    "label": "Flash Flood",
+	    "emoji": "\ud83d\udea8\ud83c\udf0a"
+	},
+	"FL": {
+	    type:'flood',
+	    "label": "Flood",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"LS": {
+	    type:'flood',
+	    "label": "Lakeshore Flood",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"UW": {
+	    type:'flood',
+	    "label": "Urban and Small Stream Flood",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"FG": {
+	    "label": "Dense Fog",
+	    "emoji": "\ud83c\udf2b\ufe0f"
+	},
+	"FR": {
+	    "label": "Frost",
+	    "emoji": "\ud83c\udf2b\ufe0f"
+	},
+	"FW": {
+	    "label": "Fire Weather",
+	    "emoji": "\ud83d\udd25"
+	},
+	"AF": {
+	    "label": "Ashfall",
+	    "emoji": "\ud83c\udf0b"
+	},
+
+	"FZ": {
+	    "label": "Freeze",
+	    "emoji": "\ud83e\uddca"
+	},
+	"GL": {
+	    "label": "Gale",
+	    "emoji": "\ud83c\udf2c\ufe0f"
+	},
+
+	"HF": {
+	    type:'wind',
+	    "label": "Hurricane Force Wind",
+	    "emoji": "\ud83c\udf00"
+	},
+	"HU": {
+	    "label": "Hurricane",
+	    "emoji": "\ud83c\udf00"
+	},
+
+	"HZ": {
+	    "label": "Hard Freeze",
+	    "emoji": "\ud83e\udd76"
+	},
+	"IP": {
+	    "label": "Sleet",
+	    "emoji": "\ud83c\udf27\ufe0f"
+	},
+	"IS": {
+	    "label": "Ice Storm",
+	    "emoji": "\ud83e\uddca"
+	},
+	"LB": {
+	    "label": "Lake Effect Snow and Blowing Snow",
+	    "emoji": "\ud83c\udf28\ufe0f\ud83d\udca8"
+	},
+	"LE": {
+	    "label": "Lake Effect Snow",
+	    "emoji": "\ud83c\udf28\ufe0f"
+	},
+	"LO": {
+	    "label": "Low Water",
+	    "emoji": "\ud83d\udca7"
+	},
+	"MA": {
+	    "label": "Marine",
+	    "emoji": "\ud83d\udea2"
+	},
+	"RB": {
+	    "label": "Small Craft for Rough Bar",
+	    "emoji": "\u26f5"
+	},
+	"RD": {
+	    "label": "Radiological Hazard",
+	    "emoji": "\u2622\ufe0f"
+	},
+	"RF": {
+	    "label": "Small Craft for Rough Waters",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"SC": {
+	    "label": "Small Craft",
+	    "emoji": "\u26f5"
+	},
+	"SE": {
+	    "label": "Hazardous Seas",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"SI": {
+	    type:'wind',
+	    "label": "Small Craft for Winds",
+	    "emoji": "\ud83d\udca8"
+	},
+	"SM": {
+	    "label": "Dense Smoke",
+	    "emoji": "\ud83c\udf2b\ufe0f"
+	},
+	"SN": {
+	    "label": "Snow",
+	    "emoji": "\u2744\ufe0f"
+	},
+	"SQ": {
+	    "label": "Snow Squall",
+	    "emoji": "\ud83c\udf28\ufe0f"
+	},
+	"SR": {
+	    "label": "Storm Surge",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"SS": {
+	    "label": "Storm Surge",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"SU": {
+	    "label": "Blowing Snow",
+	    "emoji": "\ud83d\udca8"
+	},
+	"SV": {
+	    "label": "Severe Thunderstorm",
+	    "emoji": "\u26c8\ufe0f"
+	},
+	"SW": {
+	    "label": "Small Craft for Hazardous Seas",
+	    "emoji": "\ud83d\udea4"
+	},
+	"TO": {
+	    "label": "Tornado",
+	    "emoji": "\ud83c\udf2a\ufe0f"
+	},
+	"TR": {
+	    "label": "Tropical Storm",
+	    "emoji": "\ud83c\udf00"
+	},
+	"TS": {
+	    "label": "Tsunami",
+	    "emoji": "\ud83c\udf0a"
+	},
+	"TY": {
+	    "label": "Typhoon",
+	    "emoji": "\ud83c\udf00"
+	},
+	"UP": {
+	    "label": "Unknown Precipitation",
+	    "emoji": "\u2753"
+	},
+	"VO": {
+	    "label": "Volcano",
+	    "emoji": "\ud83c\udf0b"
+	},
+	"WC": {
+	    "label": "Wind Chill",
+	    "emoji": "\ud83e\udd76"
+	},
+	"WI": {
+	    type:'wind',
+	    "label": "Wind",
+	    "emoji": "\ud83d\udca8"
+	},
+	"HW": {
+	    type:'wind',
+	    "label": "High Wind",
+	    "emoji": "\ud83c\udf2c\ufe0f"
+	},
+	"BW": {
+	    type:'wind',
+	    "label": "Brisk Wind",
+	    "emoji": "\ud83d\udca8"
+	},
+	"EW": {
+	    type:'wind',
+	    "label": "Extreme Wind",
+	    "emoji": "\ud83c\udf2c\ufe0f"
+	},
+
+	"WS": {
+	    type:'cold',
+	    "label": "Winter Storm",
+	    "emoji": "\ud83c\udf28\ufe0f"
+	},
+	"WW": {
+	    type:'cold',
+	    "label": "Winter Weather",
+	    "emoji": "\u2744\ufe0f"
+	},
+	"ZF": {
+	    "label": "Freezing Fog",
+	    "emoji": "\ud83c\udf2b\ufe0f\u2744\ufe0f"
+	},
+	"ZR": {
+	    "label": "Freezing Rain",
+	    "emoji": "\ud83c\udf27\ufe0f\u2744\ufe0f"
+	}
     }
 }
 
