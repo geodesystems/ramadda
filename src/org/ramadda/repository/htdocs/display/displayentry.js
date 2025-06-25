76a680d422cfb0640ee65ba4ed3659655cbd64d0
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-06-24 20:53:11 -0600

    better type search widget

--- src/org/ramadda/repository/htdocs/display/displayentry.js
+++ src/org/ramadda/repository/htdocs/display/displayentry.js
@@ -696,7 +696,7 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
 		if(opts.addSimpleToggle) {
 		    w  = HU.toggleBlock(HU.b(label),widget);
 		} else {
-		    w=w+HU.span([ATTR_ID,widgetId,ATTR_STYLE,HU.css('width','95%','display',opts.toggleClose?'none':'inline-block')],widget);
+		    w=w+HU.span([ATTR_ID,widgetId,ATTR_STYLE,HU.css('width','100%','display',opts.toggleClose?'none':'inline-block')],widget);
 		}
 		return HU.div([ATTR_CLASS,"display-search-block"], w);
 	    }
@@ -1887,7 +1887,12 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
                 return;
             }
 
+
             this.entryTypes = newTypes;
+	    /*
+            this.entryTypes = newTypes.sort((type1,type2)=>{
+		return type2.entryCount-type1.entryCount;
+	    });*/
 
             if (this.getEntryTypes()) {
                 let showType = {};
@@ -1963,12 +1968,14 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
                 let option = HU.tag(TAG_OPTION, optionAttrs, label);
                 let map = catMap[type.getCategory()];
                 if (map == null) {
-		    if(addTypeCategory) 
-			catMap[type.getCategory()] = HU.tag(TAG_OPTION, [ATTR_CLASS, "display-typelist-category", ATTR_TITLE, "", ATTR_VALUE, ""], type.getCategory());
+		    if(true||addTypeCategory)  {
+			catMap[type.getCategory()] =
+			    HU.tag(TAG_OPTION, [ATTR_CLASS, "display-typelist-category", ATTR_TITLE, "", ATTR_VALUE, ""], type.getCategory());
+		    }
                     cats.push(type.getCategory());
                 }
+//		select+= option;
                 catMap[type.getCategory()] += option;
-
             }
             for (let i in cats) {
                 select += catMap[cats[i]];
@@ -1980,11 +1987,20 @@ function RamaddaSearcherDisplay(displayManager, id,  type, properties) {
 		if(this.entryTypes[0].getId()!='any')
 		    this.writeHtml(ID_TYPE_DIV, HU.hidden(ID_TYPE_FIELD,this.entryTypes[0].getId()));
 	    } else {
-		this.writeHtml(ID_TYPE_DIV, this.addWidget(this.getProperty('typesLabel','Types'),select));
+		this.writeHtml(ID_TYPE_DIV,
+			       this.addWidget(this.getProperty('typesLabel','Types'),select));
 	    }
 	    
             HtmlUtils.initSelect(this.jq(ID_TYPE_FIELD),
 				 { autoWidth: false,  "max-height":"100px"});
+
+	    HU.makeSelectTagPopup(this.jq(ID_TYPE_FIELD),{
+		icon:true,
+		after:true,
+		single:true,
+		hide:false,
+		label:$(this).attr('data-label')});	    
+
             this.addExtraForm();
 	    this.typesPending=false;
 	    this.submitSearchForm();
