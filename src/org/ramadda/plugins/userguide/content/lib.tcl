7a66718d084bfdd25c5399282447ceb6da1fc31c
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-07-22 11:41:14 -0600

    add grid subset monitor and ext edit sections

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {22.93.0}
+gen::defineMacro {<%ramadda.version%>} {22.98.0}
 
 
 namespace eval wiki {}
