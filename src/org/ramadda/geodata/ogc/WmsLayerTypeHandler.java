827f8e147b2d00d41ccff158bdfa122323d04263
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-07-23 05:41:35 -0600

    supper wms and wmts layer ingest and addToMap

--- src/org/ramadda/geodata/ogc/WmsLayerTypeHandler.java
+++ src/org/ramadda/geodata/ogc/WmsLayerTypeHandler.java
@@ -7,6 +7,7 @@ package org.ramadda.geodata.ogc;
 
 
 import org.ramadda.repository.*;
+import org.ramadda.repository.map.*;
 import org.ramadda.repository.metadata.Metadata;
 import org.ramadda.repository.output.*;
 import org.ramadda.repository.type.*;
@@ -55,15 +56,69 @@ public class WmsLayerTypeHandler extends ExtensibleGroupTypeHandler {
 
 
 
+    @Override
+    public boolean addToMap(Request request, Entry entry, MapInfo map)
+            throws Exception {
+
+
+
+	if(isType("type_wms_layer")) {
+	    String baseUrl = entry.getStringValue(request,"base_url",null);
+	    String layer = entry.getStringValue(request,"layer_name",null);	    
+	    if(stringDefined(baseUrl) && stringDefined(layer)) {
+		map.addJS(HtmlUtils.call("theMap.addWMSLayer",
+					 HtmlUtils.squote(entry.getName()),
+					 HtmlUtils.squote(baseUrl),
+					 HtmlUtils.squote(layer),
+					 "false","false","{visible:true}"));
+
+		map.addJS("\n");
+		if (entry.hasAreaDefined(request)) {
+		    map.centerOn(request, entry);
+		}
+	    }
+	    return false;
+	}
+	    
+
+	if(isType("type_wmts_layer")) {
+	    String url = entry.getResource().getPath();
+	    if(!stringDefined(url)) return false;
+	    map.addJS(HtmlUtils.call("theMap.createXYZLayer",
+				     HtmlUtils.squote(entry.getName()),
+				     HtmlUtils.squote(url),
+				     "null","true","true","true"));
+	    map.addJS("\n");
+	    if (entry.hasAreaDefined(request)) {
+		map.centerOn(request, entry);
+	    }
+	}
+	return false;
+    }
+
+
+
 
     @Override
     public void initializeNewEntry(Request request, Entry entry,NewType newType)
             throws Exception {
         super.initializeNewEntry(request, entry, newType);
-        //Read the xml
+	if(!isNew(newType)) return;
+	if(isType("type_wms_layer")) return;
+
         String url = entry.getResource().getPath();
 	if(stringDefined(url)) {
-	    url = url.replace("/[0-9]+/[0-9]+/[0-9]+","${z}/${x}/${y}");
+	    if(!Utils.stringDefined(request.getString(ARG_NAME,""))) {
+		//https://storage.googleapis.com/noaa-nidis-drought-gov-data/current-conditions/tile/v1/precip-prcntnorm-60d/4/2/5.png?time=1753230600000
+		//https://storage.googleapis.com/noaa-nidis-drought-gov-data/current-conditions/tile/v1/precip-prcntnorm-30d/${z}/${x}/${y}.png?time=1753221600000
+		String name = StringUtil.findPattern(url,"/([^/]+)/\\$\\{z");
+		if(name==null)
+		    name = StringUtil.findPattern(url,"/([^/]+)/[0-9]+/[0-9]+/[0-9]+");		    
+		if(name!=null) entry.setName(Utils.makeLabel(name));
+	    }
+
+	    url = url.replaceAll("/[0-9]+/[0-9]+/[0-9]+","/\\${z}/\\${x}/\\${y}");
+	    url = url.replaceAll("time=[0-9]+","time=\\${timestamp\\}");
 	    entry.getResource().setPath(url);
 	}
     }
