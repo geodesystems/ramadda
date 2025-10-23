6bade54fc4a5c44ad444332be1ae06edb3472b92
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-10-23 10:23:45 -0600

    cleanup

--- src/org/ramadda/plugins/media/htdocs/media/animatedgif.js
+++ src/org/ramadda/plugins/media/htdocs/media/animatedgif.js
@@ -2,13 +2,13 @@
 var AnimatedGif =  {
     init:function(control, div,addButtons) {
 	if(addButtons) {
-	    $("#" + div).html("Loading");
+	    jqid(div).html("Loading");
 	}
 	control.load((gif)=>{
 	    if(addButtons) {
 		AnimatedGif.addButtons(control,div);
 		//Listen for left/right arrow
-		jqid(div).attr('tabindex','1');
+		jqid(div).attr(ATTR_TABINDEX,1);
 		jqid(div).keydown((evt)=>{
 		    if(evt.keyCode==39) {
 			control.move_relative(1);
@@ -24,14 +24,21 @@ var AnimatedGif =  {
 	let html = "";
 	let cnt=0;
 	for(let i=0;i<count;i++) {
-	    html+=HU.div(["data-index",i,"style",HU.css("xfont-family","monospace","font-size","8pt", "display","inline-block","padding-right","1px","padding-left","1px","margin-right","1px"), "class","ramadda-hoverable ramadda-clickable gif-index"],(i+1));
+	    html+=HU.div(["data-index",i,
+			  ATTR_STYLE,
+			  HU.css(CSS_FONT_SIZE,HU.pt(8),
+				 CSS_DISPLAY,DISPLAY_INLINE_BLOCK,
+				 CSS_PADDING_RIGHT,HU.px(1),
+				 CSS_PADDING_LEFT,HU.px(1),
+				 CSS_MARGIN_RIGHT,HU.px(1)),
+			  ATTR_CLASS,HU.classes(CLASS_HOVERABLE,CLASS_CLICKABLE,'gif-index')],(i+1));
 	    if(cnt++>=30) {
-		html+="<br>";
+		html+=HU.br();
 		cnt=0;
 	    }
 	}
 
-	let buttons = 	$("#" + div).html(html).find(".gif-index");
+	let buttons = 	jqid(div).html(html).find(HU.dotClass('gif-index'));
 	buttons.click(function() {
 	    let idx = +$(this).attr("data-index");
 	    control.move_to(idx);
@@ -41,9 +48,9 @@ var AnimatedGif =  {
 	    buttons.each(function() {
 		let idx = +$(this).attr("data-index");
 		if(idx==index)
-		    $(this).css("background","#ccc");
+		    $(this).css(CSS_BACKGROUND,"#ccc");
 		else
-		    $(this).css("background","transparent");
+		    $(this).css(CSS_BACKGROUND,COLOR_TRANSPARENT);
 	    });
 	});
 	
