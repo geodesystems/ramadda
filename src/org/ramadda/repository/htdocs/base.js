c23f7e44b28d3b8efa4b00fea2a620754797b729
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-10-27 06:56:51 -0600

    add ramaddathisentryname to the base.js

--- src/org/ramadda/repository/htdocs/base.js
+++ src/org/ramadda/repository/htdocs/base.js
@@ -8,6 +8,7 @@ var ramaddaUser = "${ramadda.user}";
 var ramaddaTreeSearchEnabled = ${ramadda.search.tree};
 var ramaddaState={};
 var ramaddaThisEntry = null;
+var ramaddaThisEntryName = null;
 var ramaddaLanguage = ${ramadda.user.language};
 var ramaddaLanguages = ${ramadda.languages};
 var ramaddaLanguagesEnabled = ${ramadda.languages.enabled};
