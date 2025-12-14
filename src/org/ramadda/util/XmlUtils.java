48c7cc4c30dd3a64001458b33bd7b5d977c2256a
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-12-14 07:54:52 -0700

    add elementToString

--- src/org/ramadda/util/XmlUtils.java
+++ src/org/ramadda/util/XmlUtils.java
@@ -1,6 +1,6 @@
 /**
-Copyright (c) 2008-2026 Geode Systems LLC
-SPDX-License-Identifier: Apache-2.0
+   Copyright (c) 2008-2026 Geode Systems LLC
+   SPDX-License-Identifier: Apache-2.0
 */
 
 package org.ramadda.util;
@@ -24,6 +24,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
+
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -58,7 +63,7 @@ public abstract class XmlUtils {
     }
 
     public static void appendCdataBytes(Appendable sb, byte[] bytes)
-            throws Exception {
+	throws Exception {
         sb.append("<![CDATA[");
         sb.append(XmlUtil.encodeBase64(bytes));
         sb.append("]]>");
@@ -68,6 +73,39 @@ public abstract class XmlUtils {
         sb.append("<![CDATA[");
         sb.append(s);
         sb.append("]]>");
+    }
+
+    public static String elementToString(Element element) {
+	try { 
+	    removeWhitespaceTextNodes(element);
+	    Transformer t =TransformerFactory.newInstance().newTransformer();
+ 	    t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	    t.setOutputProperty(OutputKeys.INDENT, "yes");
+	    t.setOutputProperty(OutputKeys.METHOD, "xml");
+	    t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	    t.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "2");
+	    StringWriter writer = new StringWriter();
+	    t.transform(new DOMSource(element), new StreamResult(writer));
+	    return writer.toString();
+	} catch (TransformerException e) {
+	    throw new RuntimeException(e);
+	}
+    }
+
+
+
+    public static void removeWhitespaceTextNodes(Node node) {
+	NodeList children = node.getChildNodes();
+	for (int i = children.getLength() - 1; i >= 0; i--) {
+	    Node child = children.item(i);
+	    if (child.getNodeType() == Node.TEXT_NODE) {
+		if (child.getTextContent().trim().isEmpty()) {
+		    node.removeChild(child);
+		}
+	    } else if (child.getNodeType() == Node.ELEMENT_NODE) {
+		removeWhitespaceTextNodes(child);
+	    }
+	}
     }
 
 }
