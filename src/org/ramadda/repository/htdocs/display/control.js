86aa63ed12b26b8550152317039f6967d89338a9
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-01-28 08:30:58 -0700

    add a makeSelectTagPopup to the menu display

--- src/org/ramadda/repository/htdocs/display/control.js
+++ src/org/ramadda/repository/htdocs/display/control.js
@@ -1341,6 +1341,8 @@ function RamaddaMenuDisplay(displayManager, id, properties) {
 		this.recordToIdx[record.getId()] = idx;
 	    });
 
+
+
 	    let menu =  HU.select("",[ATTR_ID, this.getDomId(ID_MENU)],options);
 	    if(this.getShowArrows(false)) {
 		let noun = this.getProperty("noun", "Data");
@@ -1358,6 +1360,16 @@ function RamaddaMenuDisplay(displayManager, id, properties) {
 	    let label = this.getMenuLabel();
 	    if(label) menu = label+SPACE + menu;
 	    this.setContents(menu);
+
+	    HU.makeSelectTagPopup(this.jq(ID_MENU),{
+		sticky:true,
+		after:true,
+		icon:true,
+		single:true,
+		hide:false,
+		label:'Select'});	    
+
+
 	    if(this.getShowArrows(false)) {
 		this.jq(ID_PREV).click(e=>{
 		    let index = +this.jq(ID_MENU).val()-1;
