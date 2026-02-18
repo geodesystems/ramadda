7a9e90dbaf188a1dc1d46493e677efbd0fa9879f
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-02-17 20:16:05 -0700

    document the debug connections

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {24.108.0}
+gen::defineMacro {<%ramadda.version%>} {25.26.0}
 
 
 namespace eval wiki {}
