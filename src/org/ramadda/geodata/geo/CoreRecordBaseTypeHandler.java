a807d3d6f5d7ddf88c91bce6e36f948861146bb6
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-02-21 04:15:21 -0700

    type_borehole_las.xml

---
+++ src/org/ramadda/geodata/geo/CoreRecordBaseTypeHandler.java
@@ -0,0 +1,205 @@
+/**
+Copyright (c) 2008-2026 Geode Systems LLC
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.ramadda.geodata.geo;
+
+import org.ramadda.data.point.text.*;
+import org.ramadda.data.record.*;
+import org.ramadda.data.services.PointTypeHandler;
+import org.ramadda.data.point.PointMetadataHarvester;
+import org.ramadda.data.services.RecordEntry;
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.metadata.*;
+import org.ramadda.repository.type.*;
+import org.ramadda.util.WikiUtil;
+import org.ramadda.util.IO;
+import org.ramadda.util.Utils;
+import org.ramadda.util.geo.GeoUtils;
+
+import ucar.unidata.util.StringUtil;
+
+
+
+import org.w3c.dom.*;
+import org.json.*;
+import java.net.URL;
+import java.io.*;
+import org.json.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.HashSet;
+import java.util.List;
+
+
+@SuppressWarnings("unchecked")
+public class BoreholeTypeHandler extends PointTypeHandler {
+
+
+    private JSONArray holes;
+
+    public BoreholeTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+
+    }
+
+
+    protected void handleHarvestedMetadata(Request request, RecordEntry recordEntry,
+                                           PointMetadataHarvester metadata)
+            throws Exception {
+	super.handleHarvestedMetadata(request, recordEntry, metadata);
+	int depthIndex=-1;
+	int index=0;
+	//	System.err.println("harvest metadata");
+	RecordField depthField=null;
+	for(RecordField field: metadata.getFields()) {
+	    String id = field.getName().toLowerCase();
+	    if(id.equals("depth")) {
+		depthField = field;
+		depthIndex=index;
+		break;
+	    }
+	    index++;
+	}
+	Entry entry = recordEntry.getEntry();
+	if(depthIndex>=0) {
+	    double[]range =metadata.getRange(depthIndex);
+	    if(range!=null) {
+		double top  =range[0];
+		double bottom  =range[1];		
+		String unit = depthField.getUnit();
+		//		System.err.println("init:" + top +" " + bottom);
+
+		if(unit!=null) {
+		    unit  = unit.toLowerCase();
+		    if(unit.equals("f") || unit.equals("feet") ||
+		       unit.equals("ft")) {
+			top = GeoUtils.feetToMeters(top);
+			bottom = GeoUtils.feetToMeters(bottom);
+		    } else if(unit.equals("mm") || unit.equals("millimeters")) {
+			top = GeoUtils.mmToMeters(top);
+			bottom = GeoUtils.mmToMeters(bottom);
+		    }
+		}
+		//		System.err.println("range:" + depthField.getUnit()+" " +range[0] +" " + range[1]);
+		entry.setValue("top_depth",top);
+		entry.setValue("bottom_depth",bottom);
+	    }
+	}
+    }
+
+
+    @Override
+    public void initializeNewEntry(Request request, Entry entry,NewType newType)
+            throws Exception {
+	super.initializeNewEntry(request, entry,newType);
+	if(!isNew(newType)) return;
+
+	if(isType("type_borehole_xrf")) {
+	    Double d=(Double)entry.getValue(request, "xrf_voltage");
+	    if(d!=null) {	    
+		double voltage = d;
+		if(voltage==15) {
+		    entry.setValue("fields_to_show","k_pct,si_pct,fe_pct,s_pct");
+		} else if (voltage==40) {
+		    entry.setValue("fields_to_show","fe_pct,sr_pct,zr_pct,ba_pct");
+		} else {
+		    entry.setValue("fields_to_show","k_pct,si_pct,fe_pct,s_pct");
+		}
+	    }
+	}
+
+
+	String holeId = (String) entry.getValue(request,"hole");
+	if(!stringDefined(holeId)) return;
+	if(holes==null) {
+	    holes = new JSONArray(getStorageManager().readUncheckedSystemResource("/org/ramadda/geodata/geo/resources/holes.json"));
+	}
+	holeId=holeId.trim();
+	for(int i=0;i<holes.length();i++) {
+	    JSONObject hole=holes.getJSONObject(i);
+	    String id = hole.getString("id").trim();
+	    if(id.equals(holeId)) {
+		entry.setLatitude(hole.getDouble("latitude"));
+		entry.setLongitude(hole.getDouble("longitude"));		
+	    }
+	}
+
+    }
+
+    @Override
+    public void handleHeaderPatternValue(Request request, Entry entry,Hashtable state, String field, String value) throws Exception {
+	super.handleHeaderPatternValue(request, entry, state,  field, value);
+    }
+
+    @Override
+    public String getWikiInclude(WikiUtil wikiUtil, Request request,
+                                 Entry originalEntry, Entry entry,
+                                 String tag, Hashtable props)
+	throws Exception {
+        if (tag.equals("borehole_profiles")) {
+	    StringBuilder sb = new StringBuilder();
+	    String fields= Utils.getProperty(props,"fields","");
+	    String indexField= Utils.getProperty(props,"indexField",".*depth.*");
+						 
+	    if(!stringDefined(fields)) {
+		fields = (String) entry.getValue(request,"fields_to_show");
+	    }
+
+	    if(!stringDefined(fields)) {
+		fields = getTypeProperty("profile.fields","");
+	    }
+
+	    if(!stringDefined(fields)) {
+		//Hacky way to get the number of fields since the metadata can be edited/changed
+		List<Metadata> metadata= getMetadataManager().findMetadata(request, entry, "thredds.variable",false);
+		if(metadata!=null) {
+		    fields="";
+		    HashSet<String> seen = new HashSet<String>();
+		    for(int i=1;i<metadata.size();i++) {
+			Metadata mtd =metadata.get(i);
+			if(Utils.equals(Utils.toLowerCase(mtd.getAttr1()),"depth")) continue;
+			if(seen.contains(mtd.getAttr1())) continue;
+			seen.add(mtd.getAttr1());
+			if(fields.length()>0) fields+=",";
+			fields+="#"+ (i+1);
+		    }
+		}		    
+	    }
+	    if(!stringDefined(fields)) {
+		fields="#2,#3,#4";
+	    }
+	    List<String> ids = Utils.split(fields,",",true,true);
+	    sb.append("+row tight=true\n");
+	    String template = "{{display_profile loadingMessage=\"\" width=100% height=500px displayInnerStyle=\"border-right:1px solid #000;\" showLegend=false marginRight=0 {extra} max=10000 showMenu=true yAxisReverse=true marginTop=0  profileMode=lines indexField=\"${indexField}\"  fields=\"{field}\"}}\n";
+	    template = template.replace("${indexField}",indexField);
+	    String height=Utils.getProperty(props,"height",null);
+	    for(int i=0;i<ids.size();i++) {
+		String id = ids.get(i);
+		String extra = i>0?" marginLeft=0 yAxisTitle=\"\" ":"";
+		if(i % 2 != 0)
+		    extra += " lineColor1=\"#FF7F0E\" ";
+		if(height!=null)
+		    extra+= " height=\"" + height+"\" ";
+		sb.append("+col-"+ Math.round((12/ids.size()))+"\n");
+		sb.append(template.replace("{field}",id).replace("{extra}",extra));
+		sb.append("-col-"+ Math.round((12/ids.size()))+"\n");
+	    }
+	    sb.append("-row\n");
+	    //	    System.err.println(sb);
+	    return getWikiManager().wikifyEntry(request, entry,sb.toString());
+	}
+
+	return super.getWikiInclude(wikiUtil,request, originalEntry, entry,tag,props);
+
+    }
+
+
+
+}
