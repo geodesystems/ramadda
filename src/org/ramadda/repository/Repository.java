3851844d0caeda92d2d859bc54cb33a2c462ed28
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-05-23 08:34:15 -0400

    dedebug

--- src/org/ramadda/repository/Repository.java
+++ src/org/ramadda/repository/Repository.java
@@ -4205,7 +4205,6 @@ public class Repository extends RepositoryBase implements RequestHandler,
 	checkHuman            = getProperty(PROP_ISHUMAN_CHECK,false);
 	alwaysHttps           = getProperty(PROP_ALWAYS_HTTPS, false);
         allSsl                = getProperty(PROP_ACCESS_ALLSSL, false);
-	System.err.println("all ssl:" + allSsl);
         sslIgnore             = getProperty(PROP_SSL_IGNORE, false);
         cacheResources        = getProperty(PROP_CACHE_RESOURCES, false);
 	cacheHtdocs           = getProperty(PROP_CACHE_HTDOCS, true);
