0599febf3a0e2d361fa407f8af56f89d76ae86b5
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-06-16 06:07:02 -0400

    debug

--- src/org/ramadda/plugins/media/ZoomifyTypeHandler.java
+++ src/org/ramadda/plugins/media/ZoomifyTypeHandler.java
@@ -58,18 +58,16 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
         Process     process = pb.start();
         InputStream is      = process.getInputStream();
 
-
-	getLogManager().logSpecial("Zoomify: done creating image tiles for:" + entry.getName()+"\n"+	getRepository().getAdmin().appendMemory());
+	getLogManager().logSpecial("Zoomify: after process call for:" + entry.getName()+"\n"+	getRepository().getAdmin().appendMemory());
 	byte[] bytes = IO.readBytes(is,100_000);
-	getLogManager().logSpecial("Zoomify: after reading bytes\n"+	getRepository().getAdmin().appendMemory());
+	getLogManager().logSpecial("Zoomify: after process completion: "+	getRepository().getAdmin().appendMemory());
         String      result  = new String(bytes);
-	getLogManager().logSpecial("Zoomify: after making string\n"+
+	getLogManager().logSpecial("Zoomify: after reading results: "+
 				   getRepository().getAdmin().appendMemory());
         if (result.indexOf("unable to open image")<0 && result.trim().length() > 0) {
             throw new IllegalArgumentException("Error running image slicer:"
 					       + result);
         }
-	getLogManager().logSpecial("Zoomify: OK");
     }
 
     @Override
