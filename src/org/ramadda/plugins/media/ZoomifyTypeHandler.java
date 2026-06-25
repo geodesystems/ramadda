e471c87550f4ba63d65cb78c09eb3c7e1c10120b
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2026-06-25 03:29:45 -0600

    support new RamaddaActionManager JS

--- src/org/ramadda/plugins/media/ZoomifyTypeHandler.java
+++ src/org/ramadda/plugins/media/ZoomifyTypeHandler.java
@@ -42,29 +42,34 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
         }
 
 	ActionManager.Action action = new ActionManager.Action() {
+		@Override
 		public void run(Object actionId) throws Exception {
-		    processImage(request, theEntry, actionId);
+		    processImage(request, theEntry, actionId,this);
 		};
+		@Override
+		public void setRunning(boolean running) {
+		    super.setRunning(running);
+		}
 	    };
 	Object actionId = getActionManager().runAction(action,"Process Zoom Images","",theEntry);
 	StringBuilder sb = new StringBuilder();
 	String url = getRepository().getUrlBase() +"/status?actionid=" + actionId +"&output=json";
         getPageHandler().entrySectionOpen(request, theEntry, sb, "Zoomable Image Processing");
-	sb.append(HU.span("Results",HU.attrs("id",actionId+"_heading")));
+	boolean canCancel = true;
+	String cancelUrl = getRepository().getUrlBase() +"/status?actionid=" + actionId +"&" + ARG_CANCEL+"=true";
+	sb.append(HU.div("",HU.attrs("id",actionId+"_cancel")));
 	HU.div(sb,"",HU.attrs("class","ramadda-action-results", "id",actionId.toString()));
-	boolean canCancel = false;
-	if(canCancel) {
-	    //		String cancelUrl = getRepository().getUrlBase() +"/status?actionid=" + actionId +"&" + ARG_CANCEL+"=true";
-	    //	    String cancelUrl = HU.url(extEditUrl,ARG_CANCEL,actionId.toString());
-	    //	    sb.append(HU.button(HU.href(cancelUrl,LABEL_CANCEL)));
-	}
-	HU.script(sb,"Utils.handleActionResults('" + actionId +"','" + url+"',"+ canCancel+");\n");
+	sb.append(HU.importJS(getHtdocsUrl("/actions.js")));
+	HU.script(sb,HU.call("new RamaddaActionManager",
+			     HU.squote(actionId),
+			     HU.squote(url),
+			     JU.map("cancelUrl",JU.quote(cancelUrl))));
         getPageHandler().entrySectionClose(request, theEntry, sb);
 	Result result = getEntryManager().makeEntryEditResult(request, theEntry, "Zoom Image Processing", sb);
 	request.setOverrideResult(result);
     }
 
-    private void processImage(Request request, Entry entry, Object actionId) throws Exception {
+    private void processImage(Request request, Entry entry, Object actionId,ActionManager.Action action) throws Exception {
         String slicer = getRepository().getScriptPath("ramadda.image.slicer");
 	getRepository().addScriptPath("sh");
         File entryDir  = getStorageManager().getEntryDir(entry.getId(), true);
@@ -82,38 +87,60 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
 		  "-s", "200",
 		  "-p",
 		  getRepository().getProperty("ramadda.zoomify.arguments",
-					      "-limit memory 512MiB -limit map 1GiB -limit disk 20GiB -limit thread 1 -strip -quality 85")
+					      "-limit memory 512MiB -limit map 1GiB -limit disk 20GiB -auto-orient  -limit thread 1 -strip -quality 85")
 		  );
 
-	long startTime = System.currentTimeMillis();
+
 	getLogManager().logSpecial("Zoomify: calling: " + Utils.join(Utils.quoteSpace(commands)," "));
 	ProcessBuilder pb = getRepository().makeProcessBuilder(commands);
 	pb.redirectErrorStream(true);
 	String tmpDir = getStorageManager().makeTempDir("imagemagic",false).toString();
-	pb.environment().put("MAGICK_TEMPORARY_PATH", tmpDir);
 	pb.environment().put("TMPDIR", tmpDir);
+	pb.environment().put("MAGICK_TEMPORARY_PATH", tmpDir);
 	pb.environment().put("MAGICK_THREAD_LIMIT", "1");
+	pb.environment().put("MAGICK_SLICER_VMEM_KB","2500000");
         pb.redirectErrorStream(true);
 	Process process = pb.start();
+	action.setProcess(process);
+	processImage(request, entry, actionId,action,process);
+    }
+
+    private void processImage(Request request, Entry entry, Object actionId,ActionManager.Action action,
+			      Process process) throws Exception {
+	long startTime = System.currentTimeMillis();
 	StringBuilder output = new StringBuilder();
 	List<String> lines = new ArrayList<String>();
 	getLogManager().logSpecial("Zoomify: creating image tiles for:" + entry.getName());
-	lines.add("Creating image tiles for:" + entry.getName());
+	lines.add("Creating image tiles for: " + entry.getName());
 	getActionManager().setActionMessage(actionId,Utils.join(lines,""));
 	String line="";
 	boolean inError = false;
 	try (BufferedReader br = new BufferedReader(
 						    new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
 	    while ((line = br.readLine()) != null) {
+		//		System.err.println("line:" + line);
+		if ( !getActionManager().getActionOk(actionId)) {
+		    if(process!=null) {
+			Utils.destroy(process);
+			action.setProcess(null);
+			process=null;
+		    }
+		    break;
+		}
+
 		if(!stringDefined(line)) {
 		    continue;
 		}
-		//		System.err.println("line:" + line);
 		line = formatProgress(line);
+		//		ucar.unidata.util.Misc.sleepSeconds(5);
 		if(line==null) continue;
 		if(line.matches(".*(error/|no images defined|unable to open).*")) {
 		    inError=true;
-		    process.destroy();
+		    if(process!=null) {
+			Utils.destroy(process);
+			action.setProcess(null);
+			process=null;
+		    }
 		    break;
 		}
 
@@ -145,37 +172,46 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
 	    }
 	}
 	
-	int exitCode = process.waitFor();
+	action.setProcess(null);
+	if(process!=null) {
+	    int exitCode= process.waitFor();
+	    if (exitCode != 0 ) inError=true;
+	}
+	if ( !getActionManager().getActionOk(actionId)) {
+	    return;
+	}
+
 	String result = output.toString();
-	if (exitCode != 0 || inError) {
-	    getActionManager().setContinueHtml(actionId,
-					       "Error:" + line);
+	if (inError) {
+	    getActionManager().setContinueHtml(actionId,"Error:" + line);
 	    getLogManager().logSpecial("Zoomify: error processing:" + entry+"\n"+line);
 	    return;
 	}
+
 	getActionManager().setContinueHtml(actionId,
 					   "Finished processing. "+
 					   HU.href(getEntryManager().getEntryURL(request, entry),
 						   "View " + entry.getName()));
-
-	getLogManager().logSpecial("Zoomify: after reading results: "+
-				   getRepository().getAdmin().appendMemory());
-	/*
-	  if (result.indexOf("unable to open image")<0 && result.trim().length() > 0) {
-	  throw new IllegalArgumentException("Error running image slicer:"
-	  + result);
-	  }
-	*/
-
+	//	getLogManager().logSpecial("Zoomify: after reading results: "+   getRepository().getAdmin().appendMemory());
     }
 
-    private static final Pattern PROGRESS_PATTERN =
-	Pattern.compile("PROGRESS\\s+(\\w+)\\s+level=(\\d+)\\s+total=(\\d+)\\s+size=(\\d+)");
+    private static final Pattern PROGRESS_PATTERN1 =
+	Pattern.compile("PROGRESS\\s+(\\w+)\\s+level=(\\d+)\\s+total=(\\d+)\\s+width=(\\d+)\\s+height=(\\d+)");
+
+    private static final Pattern PROGRESS_PATTERN2 =
+	Pattern.compile("PROGRESS\\s+(\\w+)\\s+.*");
 
     private String formatProgress(String line) {
 	try {
-	    Matcher m = PROGRESS_PATTERN.matcher(line);
+	    Matcher m = PROGRESS_PATTERN1.matcher(line);
 	    if (!m.find()) {
+		m = PROGRESS_PATTERN2.matcher(line);
+		if (m.find()) {
+		    String phase = m.group(1);
+		    if (phase.equals("normalize")) {
+			return  "Normalizing the source image";
+		    }
+		}
 		if(line.indexOf("PROGRESS")<0) {
 		    return line;
 		}
@@ -185,21 +221,24 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
 	    String phase = m.group(1);
 	    int level = Integer.parseInt(m.group(2));
 	    int total = Integer.parseInt(m.group(3));
-	    int size = Integer.parseInt(m.group(4));
+	    int width = Integer.parseInt(m.group(4));
+	    int height = Integer.parseInt(m.group(5));	    
 
 	    // Convert to 1-based numbering for display
 	    int displayLevel = level + 1;
 
+
+
 	    if (phase.equals("resize")) {
 		return String.format(
-				     "Preparing zoom level %d of %d (%d pixels)",
-				     displayLevel, total, size);
+				     "Preparing zoom level %d of %d W:%d H:%d",
+				     displayLevel, total, width,height);
 	    }
 
 	    if (phase.equals("slice")) {
 		return String.format(
-				     "Creating tiles for zoom level %d of %d (%d pixels)",
-				     displayLevel, total, size);
+				     "Creating tiles for zoom level %d of %d W:%d  H:%d",
+				     displayLevel, total, width,height);
 	    }
 
 	    if (phase.equals("sliced")) {
@@ -207,10 +246,10 @@ public class ZoomifyTypeHandler extends GenericTypeHandler implements WikiTagHan
 				     "Finished zoom level %d of %d",
 				     displayLevel, total);
 	    }
-
 	    return null;
 	} catch(Exception exc){
 	    getLogManager().logError("processing magickslicer line:" + line,exc);
+	    System.err.println("Error:" +line+" " + exc);
 	    return line;
 	}
     }
